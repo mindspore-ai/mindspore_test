0c29a89010ae6cdf3a96c88b8ebe9df780db29da
Margaret_wangrui <wangrui178@huawei.com>
2024-08-01 10:23:43 +0800

    When a parameter is used as input to the top graph, need check if the parameter object has changed and check if need recompile.

---
+++ tests/st/parameter/test_parameter_compile_cache.py
@@ -0,0 +1,114 @@
+# Copyright 2024 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+import pytest
+from mindspore.common import Tensor, Parameter, jit
+from mindspore.nn import Cell
+from mindspore import ops
+import mindspore as ms
+
+
+ms.context.set_context(mode=ms.GRAPH_MODE)
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.env_onecard
+def test_param_compile_cache():
+    """
+    Feature: Support compile cache when exist parameter in the top graph inputs.
+    Description: Support compile cache.
+    Expectation: Run success.
+    """
+    class AddNet(Cell):
+        def __init__(self):
+            super().__init__()
+            self.op = ops.add
+
+        def construct(self, weight):
+            out = self.op(1, weight)
+            return out
+
+    weight1 = Parameter(Tensor([2]), name='weight1')
+    weight2 = Parameter(Tensor([3]), name='weight2')
+    net = AddNet()
+    out1 = net(weight1)
+    assert weight1.asnumpy() == 2
+    assert weight2.asnumpy() == 3
+    assert out1 == 3
+    out2 = net(weight2)
+    assert weight1.asnumpy() == 2
+    assert weight2.asnumpy() == 3
+    assert out2 == 4
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.env_onecard
+def test_param_compile_cache_kwargs():
+    """
+    Feature: Support compile cache when exist parameter in the top graph inputs.
+    Description: Support compile cache.
+    Expectation: Run success.
+    """
+    class AddNet(Cell):
+        def __init__(self):
+            super().__init__()
+            self.op = ops.add
+
+        def construct(self, **kwargs):
+            out = self.op(kwargs['a'], 3)
+            return out
+
+    input_a = Parameter(Tensor([4]), name='input_a')
+    input_b = Parameter(Tensor([5]), name='input_b')
+    net = AddNet()
+    out1 = net(a=input_a)
+    assert input_a.asnumpy() == 4
+    assert input_b.asnumpy() == 5
+    assert out1 == 7
+    out2 = net(a=input_b)
+    assert input_a.asnumpy() == 4
+    assert input_b.asnumpy() == 5
+    assert out2 == 8
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.env_onecard
+def test_param_compile_cache_jit():
+    """
+    Feature: Support compile cache when exist parameter in the top graph inputs.
+    Description: Support compile cache.
+    Expectation: Run success.
+    """
+    @jit
+    def func(param, **kwargs):
+        return ops.add(param, kwargs['a'])
+
+    param_1 = Parameter(Tensor([1]), name='param_1')
+    param_2 = Parameter(Tensor([2]), name='param_2')
+    param_3 = Parameter(Tensor([3]), name='param_3')
+    res1 = func(param_1, a=param_1)
+    res2 = func(param_2, a=param_1)
+    assert res1 == 2
+    assert res2 == 3
+    assert param_1.asnumpy() == 1
+    assert param_2.asnumpy() == 2
+    res3 = func(param_1, a=param_2)
+    res4 = func(param_1, a=param_3)
+    assert res3 == 3
+    assert res4 == 4
+    assert param_1.asnumpy() == 1
+    assert param_2.asnumpy() == 2
+    assert param_3.asnumpy() == 3
