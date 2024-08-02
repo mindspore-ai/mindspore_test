mindspore-ai/mindspore

modify simple infer ut

mindspore/core/ops/ops_func_impl/fill_scalar.h
@@ -26,7 +26,12 @@
 
 namespace mindspore {
 namespace ops {
-class MIND_API FillScalarFuncImpl : public FillFuncImpl {};
+class MIND_API FillScalarFuncImpl : public FillFuncImpl {
+ public:
+  TypePtr InferType(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) const override;
+  // simply infer
+  TypePtrList InferType(const PrimitivePtr &primitive, const ValuePtrList &input_values) const override;
+};
 }  // namespace ops
 }  // namespace mindspore
 

mindspore/core/ops/ops_func_impl/fill_tensor.h
@@ -26,7 +26,14 @@
 
 namespace mindspore {
 namespace ops {
-class MIND_API FillTensorFuncImpl : public FillFuncImpl {};
+class MIND_API FillTensorFuncImpl : public FillFuncImpl {
+ public:
+  BaseShapePtr InferShape(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) const override;
+  TypePtr InferType(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) const override;
+  // simply infer
+  ShapeArray InferShape(const PrimitivePtr &primitive, const ValuePtrList &input_values) const override;
+  TypePtrList InferType(const PrimitivePtr &primitive, const ValuePtrList &input_values) const override;
+};
 }  // namespace ops
 }  // namespace mindspore
 

mindspore-ai/mindspore_test

mindspore/ops/infer/ops_func_impl/fill_scalar.h
@@ -26,7 +26,12 @@
 
 namespace mindspore {
 namespace ops {
-class OPS_API FillScalarFuncImpl : public FillFuncImpl {};
+class OPS_API FillScalarFuncImpl : public FillFuncImpl {
+ public:
+  TypePtr InferType(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) const override;
+  // simply infer
+  TypePtrList InferType(const PrimitivePtr &primitive, const ValuePtrList &input_values) const override;
+};
 }  // namespace ops
 }  // namespace mindspore
 

mindspore/ops/infer/ops_func_impl/fill_tensor.h
@@ -26,7 +26,14 @@
 
 namespace mindspore {
 namespace ops {
-class OPS_API FillTensorFuncImpl : public FillFuncImpl {};
+class OPS_API FillTensorFuncImpl : public FillFuncImpl {
+ public:
+  BaseShapePtr InferShape(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) const override;
+  TypePtr InferType(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) const override;
+  // simply infer
+  ShapeArray InferShape(const PrimitivePtr &primitive, const ValuePtrList &input_values) const override;
+  TypePtrList InferType(const PrimitivePtr &primitive, const ValuePtrList &input_values) const override;
+};
 }  // namespace ops
 }  // namespace mindspore
 