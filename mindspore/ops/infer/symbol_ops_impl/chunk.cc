mindspore-ai/mindspore

add symbol ops for opensora / moe network

mindspore/core/ops/symbol_ops_impl/chunk.cc
@@ -1,0 +1,61 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "mindspore/core/ops/symbol_ops_impl/common.h"
+
+namespace mindspore {
+namespace symshape {
+namespace ops {
+class MS_CORE_API Chunk : public InferShapeOp {
+ public:
+  using InferShapeOp::InferShapeOp;
+  ~Chunk() override = default;
+  MS_DECLARE_PARENT(Chunk, InferShapeOp)
+ protected:
+  SymbolPtr Eval() override;
+};
+
+SymbolPtr Chunk::Eval() {
+  auto input_shape = input_as<ListSymbol>(kIndex0);
+  if (!input_shape->HasData()) {
+    return nullptr;
+  }
+  DoNotEvalOnRun();
+  auto chunks = input_as<IntSymbol>(kIndex1)->value();
+  auto rank = input_shape->size();
+  auto dim = NormAxis(input_as<IntSymbol>(kIndex2)->value(), rank);
+  // chunk does not support dynamic dim.
+  auto dim_size = input_shape->item_as<IntSymbol>(LongToSize(dim))->value();
+  int64_t each_size = (dim_size + chunks - 1) / chunks;
+  if (each_size == 0 && dim_size == 0) {
+    return GenList(SymbolPtrList(LongToSize(chunks), kSym0));
+  }
+  auto actual_chunks = std::max<int64_t>((dim_size + each_size - 1) / each_size, 1);
+  SymbolPtrList each_shape = input_shape->symbols();
+  each_shape[dim] = GenInt(each_size);
+  SymbolPtrList result(LongToSize(actual_chunks), GenList(std::move(each_shape)));
+  int64_t last_split_size = each_size - (each_size * actual_chunks - dim_size);
+  auto last_shape = input_shape->symbols();
+  last_shape[dim] = GenInt(last_split_size);
+  result.back() = GenList(std::move(last_shape));
+  return GenList(std::move(result));
+}
+
+REG_SYMBOL_OP_BUILDER("Chunk")
+  .SetShapeDepend({DependOn::kShape, DependOn::kValue, DependOn::kValue})
+  .SetShapeFuncWith<Chunk>();
+}  // namespace ops
+}  // namespace symshape
+}  // namespace mindspore

mindspore/core/ops/symbol_ops_impl/elemwise_op.cc
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "mindspore/core/ops/symbol_ops_impl/elemwise_binop.h"
+#include "mindspore/core/ops/symbol_ops_impl/elemwise_op.h"
 #include <algorithm>
 #include <memory>
 #include <utility>
@@ -83,6 +83,7 @@ SymbolPtrList ElemwiseBinop::Process(const SymbolPtrList &lhs, const SymbolPtrLi
   return result;
 }
 
+// binary op
 REG_SYMBOL_OP_BUILDER("Add").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
 REG_SYMBOL_OP_BUILDER("AddExt").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
 REG_SYMBOL_OP_BUILDER("Div").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
@@ -101,6 +102,30 @@ REG_SYMBOL_OP_BUILDER("Pow").SetShapeDependN<DependOn::kShape, 2>().SetShapeFunc
 REG_SYMBOL_OP_BUILDER("RealDiv").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
 REG_SYMBOL_OP_BUILDER("Sub").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
 REG_SYMBOL_OP_BUILDER("SubExt").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
+
+SymbolPtr AddnBuildShape(OperationBuilder *b, const SymbolPtrList &symbols) {
+  if (symbols.empty()) {
+    return nullptr;
+  }
+  auto result = symbols[0];
+  for (size_t i = 1; i < symbols.size(); i++) {
+    result = b->Emit(std::make_shared<ElemwiseBinop>(result, symbols[i]));
+  }
+  return result;
+}
+SymbolPtr GeneralBuildShape(OperationBuilder *b) { return AddnBuildShape(b, b->GetSymbolsOfDepend()); }
+
+// ops of more inputs
+REG_SYMBOL_OP_BUILDER("AddN").SetShapeDependN<DependOn::kShape>().SetShapeFunc([](OperationBuilder *b) {
+  if (b->input_num() > kDim1) {
+    return AddnBuildShape(b, b->GetSymbolsOfDepend());
+  }
+  auto inputs = b->GetInputShape(kIndex0)->as_sptr<ListSymbol>();
+  return AddnBuildShape(b, inputs->symbols());
+});
+REG_SYMBOL_OP_BUILDER("Addcdiv").SetShapeDependN<DependOn::kShape, 4>().SetShapeFunc(GeneralBuildShape);
+REG_SYMBOL_OP_BUILDER("Addcmul").SetShapeDependN<DependOn::kShape, 4>().SetShapeFunc(GeneralBuildShape);
+REG_SYMBOL_OP_BUILDER("Select").SetShapeDependN<DependOn::kShape, 3>().SetShapeFunc(GeneralBuildShape);
 }  // namespace ops
 }  // namespace symshape
 }  // namespace mindspore

mindspore/core/ops/symbol_ops_impl/range.cc
@@ -1,0 +1,48 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "mindspore/core/ops/symbol_ops_impl/common.h"
+#include "mindspore/core/ops/symbol_ops_impl/scalar_sub.h"
+#include "mindspore/core/ops/symbol_ops_impl/scalar_div.h"
+#include "mindspore/core/ops/symbol_ops_impl/scalar_min.h"
+
+namespace mindspore {
+namespace symshape {
+namespace ops {
+class MS_CORE_API Range : public InferShapeOp {
+ public:
+  using InferShapeOp::InferShapeOp;
+  ~Range() override = default;
+  MS_DECLARE_PARENT(Range, InferShapeOp)
+ protected:
+  SymbolPtr Eval() override;
+};
+
+SymbolPtr Range::Eval() {
+  auto start = input(kIndex0);
+  auto end = input(kIndex1);
+  auto step = input(kIndex2);
+  auto maxlen = input(kIndex3);
+  DoNotEvalOnRun();
+  // range length = (end - start) / step.  (to ceil)
+  auto len = Emit(std::make_shared<ScalarSub>(end, start));
+  len = Emit(std::make_shared<ScalarCeilDiv>(len, step));
+  return Emit(std::make_shared<ScalarMin>(len, maxlen));
+}
+
+REG_SYMBOL_OP_BUILDER("Range").SetShapeDependN<DependOn::kValue, 4>().SetShapeFuncWith<Range>();
+}  // namespace ops
+}  // namespace symshape
+}  // namespace mindspore

mindspore/core/ops/symbol_ops_impl/scalar_cmp.h
@@ -1,0 +1,80 @@
+/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MINDSPORE_CORE_OPS_SYMBOL_OPS_IMPL_SCALAR_CMP_H_
+#define MINDSPORE_CORE_OPS_SYMBOL_OPS_IMPL_SCALAR_CMP_H_
+
+#include "mindspore/core/ops/symbol_ops_impl/common.h"
+
+namespace mindspore {
+namespace symshape {
+namespace ops {
+class MS_CORE_API ScalarCmpOp : public InferValueOp {
+ public:
+  using InferValueOp::InferValueOp;
+  ScalarCmpOp(const SymbolPtr &a, const SymbolPtr &b) : InferValueOp({a, b}) {}
+  ~ScalarCmpOp() override = default;
+  MS_DECLARE_PARENT(ScalarCmpOp, InferValueOp)
+ protected:
+  SymbolPtr Eval() override;
+  void EvalOnRun() override {
+    output_as<BoolSymbol>()->SetValue(Compare(input_as<IntSymbol>(0), input_as<IntSymbol>(1)));
+  }
+  virtual bool Compare(const IntSymbol *a, const IntSymbol *b) const = 0;
+};
+
+class MS_CORE_API ScalarEq final : public ScalarCmpOp {
+ public:
+  using ScalarCmpOp::ScalarCmpOp;
+  MS_DECLARE_PARENT(ScalarEq, ScalarCmpOp)
+ protected:
+  bool Compare(const IntSymbol *a, const IntSymbol *b) const override { return *a == *b; }
+};
+
+class MS_CORE_API ScalarGt final : public ScalarCmpOp {
+ public:
+  using ScalarCmpOp::ScalarCmpOp;
+  MS_DECLARE_PARENT(ScalarGt, ScalarCmpOp)
+ protected:
+  bool Compare(const IntSymbol *a, const IntSymbol *b) const override { return *a > *b; }
+};
+
+class MS_CORE_API ScalarGe final : public ScalarCmpOp {
+ public:
+  using ScalarCmpOp::ScalarCmpOp;
+  MS_DECLARE_PARENT(ScalarGe, ScalarCmpOp)
+ protected:
+  bool Compare(const IntSymbol *a, const IntSymbol *b) const override { return *a >= *b; }
+};
+
+class MS_CORE_API ScalarLt final : public ScalarCmpOp {
+ public:
+  using ScalarCmpOp::ScalarCmpOp;
+  MS_DECLARE_PARENT(ScalarLt, ScalarCmpOp)
+ protected:
+  bool Compare(const IntSymbol *a, const IntSymbol *b) const override { return *a < *b; }
+};
+
+class MS_CORE_API ScalarLe final : public ScalarCmpOp {
+ public:
+  using ScalarCmpOp::ScalarCmpOp;
+  MS_DECLARE_PARENT(ScalarLe, ScalarCmpOp)
+ protected:
+  bool Compare(const IntSymbol *a, const IntSymbol *b) const override { return *a <= *b; }
+};
+}  // namespace ops
+}  // namespace symshape
+}  // namespace mindspore
+#endif  // MINDSPORE_CORE_OPS_SYMBOL_OPS_IMPL_SCALAR_CMP_H_

mindspore-ai/mindspore_test

mindspore/ops/infer/symbol_ops_impl/chunk.cc
@@ -1,0 +1,61 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "mindspore/ops/infer/symbol_ops_impl/common.h"
+
+namespace mindspore {
+namespace symshape {
+namespace ops {
+class OPS_API Chunk : public InferShapeOp {
+ public:
+  using InferShapeOp::InferShapeOp;
+  ~Chunk() override = default;
+  MS_DECLARE_PARENT(Chunk, InferShapeOp)
+ protected:
+  SymbolPtr Eval() override;
+};
+
+SymbolPtr Chunk::Eval() {
+  auto input_shape = input_as<ListSymbol>(kIndex0);
+  if (!input_shape->HasData()) {
+    return nullptr;
+  }
+  DoNotEvalOnRun();
+  auto chunks = input_as<IntSymbol>(kIndex1)->value();
+  auto rank = input_shape->size();
+  auto dim = NormAxis(input_as<IntSymbol>(kIndex2)->value(), rank);
+  // chunk does not support dynamic dim.
+  auto dim_size = input_shape->item_as<IntSymbol>(LongToSize(dim))->value();
+  int64_t each_size = (dim_size + chunks - 1) / chunks;
+  if (each_size == 0 && dim_size == 0) {
+    return GenList(SymbolPtrList(LongToSize(chunks), kSym0));
+  }
+  auto actual_chunks = std::max<int64_t>((dim_size + each_size - 1) / each_size, 1);
+  SymbolPtrList each_shape = input_shape->symbols();
+  each_shape[dim] = GenInt(each_size);
+  SymbolPtrList result(LongToSize(actual_chunks), GenList(std::move(each_shape)));
+  int64_t last_split_size = each_size - (each_size * actual_chunks - dim_size);
+  auto last_shape = input_shape->symbols();
+  last_shape[dim] = GenInt(last_split_size);
+  result.back() = GenList(std::move(last_shape));
+  return GenList(std::move(result));
+}
+
+REG_SYMBOL_OP_BUILDER("Chunk")
+  .SetShapeDepend({DependOn::kShape, DependOn::kValue, DependOn::kValue})
+  .SetShapeFuncWith<Chunk>();
+}  // namespace ops
+}  // namespace symshape
+}  // namespace mindspore

mindspore/ops/infer/symbol_ops_impl/elemwise_op.cc
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "mindspore/ops/infer/symbol_ops_impl/elemwise_binop.h"
+#include "mindspore/ops/infer/symbol_ops_impl/elemwise_op.h"
 #include <algorithm>
 #include <memory>
 #include <utility>
@@ -83,6 +83,7 @@ SymbolPtrList ElemwiseBinop::Process(const SymbolPtrList &lhs, const SymbolPtrLi
   return result;
 }
 
+// binary op
 REG_SYMBOL_OP_BUILDER("Add").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
 REG_SYMBOL_OP_BUILDER("AddExt").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
 REG_SYMBOL_OP_BUILDER("Div").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
@@ -101,6 +102,30 @@ REG_SYMBOL_OP_BUILDER("Pow").SetShapeDependN<DependOn::kShape, 2>().SetShapeFunc
 REG_SYMBOL_OP_BUILDER("RealDiv").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
 REG_SYMBOL_OP_BUILDER("Sub").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
 REG_SYMBOL_OP_BUILDER("SubExt").SetShapeDependN<DependOn::kShape, 2>().SetShapeFuncWith<ElemwiseBinop>();
+
+SymbolPtr AddnBuildShape(OperationBuilder *b, const SymbolPtrList &symbols) {
+  if (symbols.empty()) {
+    return nullptr;
+  }
+  auto result = symbols[0];
+  for (size_t i = 1; i < symbols.size(); i++) {
+    result = b->Emit(std::make_shared<ElemwiseBinop>(result, symbols[i]));
+  }
+  return result;
+}
+SymbolPtr GeneralBuildShape(OperationBuilder *b) { return AddnBuildShape(b, b->GetSymbolsOfDepend()); }
+
+// ops of more inputs
+REG_SYMBOL_OP_BUILDER("AddN").SetShapeDependN<DependOn::kShape>().SetShapeFunc([](OperationBuilder *b) {
+  if (b->input_num() > kDim1) {
+    return AddnBuildShape(b, b->GetSymbolsOfDepend());
+  }
+  auto inputs = b->GetInputShape(kIndex0)->as_sptr<ListSymbol>();
+  return AddnBuildShape(b, inputs->symbols());
+});
+REG_SYMBOL_OP_BUILDER("Addcdiv").SetShapeDependN<DependOn::kShape, 4>().SetShapeFunc(GeneralBuildShape);
+REG_SYMBOL_OP_BUILDER("Addcmul").SetShapeDependN<DependOn::kShape, 4>().SetShapeFunc(GeneralBuildShape);
+REG_SYMBOL_OP_BUILDER("Select").SetShapeDependN<DependOn::kShape, 3>().SetShapeFunc(GeneralBuildShape);
 }  // namespace ops
 }  // namespace symshape
 }  // namespace mindspore

mindspore/ops/infer/symbol_ops_impl/range.cc
@@ -1,0 +1,48 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "mindspore/ops/infer/symbol_ops_impl/common.h"
+#include "mindspore/ops/infer/symbol_ops_impl/scalar_sub.h"
+#include "mindspore/ops/infer/symbol_ops_impl/scalar_div.h"
+#include "mindspore/ops/infer/symbol_ops_impl/scalar_min.h"
+
+namespace mindspore {
+namespace symshape {
+namespace ops {
+class OPS_API Range : public InferShapeOp {
+ public:
+  using InferShapeOp::InferShapeOp;
+  ~Range() override = default;
+  MS_DECLARE_PARENT(Range, InferShapeOp)
+ protected:
+  SymbolPtr Eval() override;
+};
+
+SymbolPtr Range::Eval() {
+  auto start = input(kIndex0);
+  auto end = input(kIndex1);
+  auto step = input(kIndex2);
+  auto maxlen = input(kIndex3);
+  DoNotEvalOnRun();
+  // range length = (end - start) / step.  (to ceil)
+  auto len = Emit(std::make_shared<ScalarSub>(end, start));
+  len = Emit(std::make_shared<ScalarCeilDiv>(len, step));
+  return Emit(std::make_shared<ScalarMin>(len, maxlen));
+}
+
+REG_SYMBOL_OP_BUILDER("Range").SetShapeDependN<DependOn::kValue, 4>().SetShapeFuncWith<Range>();
+}  // namespace ops
+}  // namespace symshape
+}  // namespace mindspore

mindspore/ops/infer/symbol_ops_impl/scalar_cmp.h
@@ -1,0 +1,80 @@
+/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MINDSPORE_CORE_OPS_SYMBOL_OPS_IMPL_SCALAR_CMP_H_
+#define MINDSPORE_CORE_OPS_SYMBOL_OPS_IMPL_SCALAR_CMP_H_
+
+#include "mindspore/ops/infer/symbol_ops_impl/common.h"
+
+namespace mindspore {
+namespace symshape {
+namespace ops {
+class OPS_API ScalarCmpOp : public InferValueOp {
+ public:
+  using InferValueOp::InferValueOp;
+  ScalarCmpOp(const SymbolPtr &a, const SymbolPtr &b) : InferValueOp({a, b}) {}
+  ~ScalarCmpOp() override = default;
+  MS_DECLARE_PARENT(ScalarCmpOp, InferValueOp)
+ protected:
+  SymbolPtr Eval() override;
+  void EvalOnRun() override {
+    output_as<BoolSymbol>()->SetValue(Compare(input_as<IntSymbol>(0), input_as<IntSymbol>(1)));
+  }
+  virtual bool Compare(const IntSymbol *a, const IntSymbol *b) const = 0;
+};
+
+class OPS_API ScalarEq final : public ScalarCmpOp {
+ public:
+  using ScalarCmpOp::ScalarCmpOp;
+  MS_DECLARE_PARENT(ScalarEq, ScalarCmpOp)
+ protected:
+  bool Compare(const IntSymbol *a, const IntSymbol *b) const override { return *a == *b; }
+};
+
+class OPS_API ScalarGt final : public ScalarCmpOp {
+ public:
+  using ScalarCmpOp::ScalarCmpOp;
+  MS_DECLARE_PARENT(ScalarGt, ScalarCmpOp)
+ protected:
+  bool Compare(const IntSymbol *a, const IntSymbol *b) const override { return *a > *b; }
+};
+
+class OPS_API ScalarGe final : public ScalarCmpOp {
+ public:
+  using ScalarCmpOp::ScalarCmpOp;
+  MS_DECLARE_PARENT(ScalarGe, ScalarCmpOp)
+ protected:
+  bool Compare(const IntSymbol *a, const IntSymbol *b) const override { return *a >= *b; }
+};
+
+class OPS_API ScalarLt final : public ScalarCmpOp {
+ public:
+  using ScalarCmpOp::ScalarCmpOp;
+  MS_DECLARE_PARENT(ScalarLt, ScalarCmpOp)
+ protected:
+  bool Compare(const IntSymbol *a, const IntSymbol *b) const override { return *a < *b; }
+};
+
+class OPS_API ScalarLe final : public ScalarCmpOp {
+ public:
+  using ScalarCmpOp::ScalarCmpOp;
+  MS_DECLARE_PARENT(ScalarLe, ScalarCmpOp)
+ protected:
+  bool Compare(const IntSymbol *a, const IntSymbol *b) const override { return *a <= *b; }
+};
+}  // namespace ops
+}  // namespace symshape
+}  // namespace mindspore
+#endif  // MINDSPORE_CORE_OPS_SYMBOL_OPS_IMPL_SCALAR_CMP_H_