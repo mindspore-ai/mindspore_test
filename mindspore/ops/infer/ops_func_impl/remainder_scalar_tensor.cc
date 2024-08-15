mindspore-ai/mindspore

add mint.remainder

mindspore/core/ops/ops_func_impl/remainder_scalar_tensor.cc
@@ -1,0 +1,58 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+#include "ops/ops_func_impl/remainder_scalar_tensor.h"
+#include "ops/op_utils.h"
+#include "ops/ops_func_impl/simple_infer.h"
+
+namespace mindspore::ops {
+BaseShapePtr RemainderScalarTensorFuncImpl::InferShape(const PrimitivePtr &primitive,
+                                                       const std::vector<AbstractBasePtr> &input_args) const {
+  return input_args[kInputIndex1]->GetShape();
+}
+
+TypePtr RemainderScalarTensorFuncImpl::InferType(const PrimitivePtr &primitive,
+                                                 const std::vector<AbstractBasePtr> &input_args) const {
+  auto input_type = input_args[kInputIndex0]->GetType();
+  auto other_type = input_args[kInputIndex1]->GetType();
+  MS_EXCEPTION_IF_NULL(input_type);
+  MS_EXCEPTION_IF_NULL(other_type);
+  auto out_type = PromoteType(input_type, other_type, primitive->name());
+  return std::make_shared<TensorType>(out_type);
+}
+
+// simple infer
+TypePtrList RemainderScalarTensorFuncImpl::InferType(const PrimitivePtr &primitive,
+                                                     const ValuePtrList &input_values) const {
+  const auto &input_value = input_values[kInputIndex0];
+  const auto &other_tensor = input_values[kInputIndex1]->cast<tensor::BaseTensorPtr>();
+  MS_EXCEPTION_IF_NULL(input_value);
+  MS_EXCEPTION_IF_NULL(other_tensor);
+  const auto &input_type = input_value->type();
+  const auto &other_type = other_tensor->Dtype();
+  return {PromoteType(input_type, other_type, primitive->name())};
+}
+
+ShapeArray RemainderScalarTensorFuncImpl::InferShape(const PrimitivePtr &primitive,
+                                                     const ValuePtrList &input_values) const {
+  const auto &other_tensor = input_values[kInputIndex1]->cast<tensor::BaseTensorPtr>();
+  MS_EXCEPTION_IF_NULL(other_tensor);
+  return {other_tensor->shape()};
+}
+
+REGISTER_SIMPLE_INFER(kNameRemainderScalarTensor, RemainderScalarTensorFuncImpl)
+}  // namespace mindspore::ops

mindspore/core/ops/ops_func_impl/remainder_tensor_scalar.cc
@@ -1,0 +1,58 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+#include "ops/ops_func_impl/remainder_tensor_scalar.h"
+#include "ops/op_utils.h"
+#include "ops/ops_func_impl/simple_infer.h"
+
+namespace mindspore::ops {
+BaseShapePtr RemainderTensorScalarFuncImpl::InferShape(const PrimitivePtr &primitive,
+                                                       const std::vector<AbstractBasePtr> &input_args) const {
+  return input_args[kInputIndex0]->GetShape();
+}
+
+TypePtr RemainderTensorScalarFuncImpl::InferType(const PrimitivePtr &primitive,
+                                                 const std::vector<AbstractBasePtr> &input_args) const {
+  auto input_type = input_args[kInputIndex0]->GetType();
+  auto other_type = input_args[kInputIndex1]->GetType();
+  MS_EXCEPTION_IF_NULL(input_type);
+  MS_EXCEPTION_IF_NULL(other_type);
+  auto out_type = PromoteType(input_type, other_type, primitive->name());
+  return std::make_shared<TensorType>(out_type);
+}
+
+// simple infer
+TypePtrList RemainderTensorScalarFuncImpl::InferType(const PrimitivePtr &primitive,
+                                                     const ValuePtrList &input_values) const {
+  const auto &input_tensor = input_values[kInputIndex0]->cast<tensor::BaseTensorPtr>();
+  const auto &other_value = input_values[kInputIndex1];
+  MS_EXCEPTION_IF_NULL(input_tensor);
+  MS_EXCEPTION_IF_NULL(other_value);
+  const auto &input_type = input_tensor->Dtype();
+  const auto &other_type = other_value->type();
+  return {PromoteType(input_type, other_type, primitive->name())};
+}
+
+ShapeArray RemainderTensorScalarFuncImpl::InferShape(const PrimitivePtr &primitive,
+                                                     const ValuePtrList &input_values) const {
+  const auto &input_tensor = input_values[kInputIndex0]->cast<tensor::BaseTensorPtr>();
+  MS_EXCEPTION_IF_NULL(input_tensor);
+  return {input_tensor->shape()};
+}
+
+REGISTER_SIMPLE_INFER(kNameRemainderTensorScalar, RemainderTensorScalarFuncImpl)
+}  // namespace mindspore::ops

tests/ut/cpp/ops/test_ops_remainder_tensor_scalar.cc
@@ -1,0 +1,116 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vector>
+#include <memory>
+#include "common/common_test.h"
+#include "ir/dtype/type.h"
+#include "abstract/dshape.h"
+#include "utils/tensor_construct_utils.h"
+#include "ir/primitive.h"
+#include "abstract/abstract_value.h"
+#include "include/backend/optimizer/helper.h"
+#include "ops/test_ops.h"
+#include "ops/ops_func_impl/remainder_tensor_scalar.h"
+#include "ops/test_value_utils.h"
+#include "ops/test_ops_cmp_utils.h"
+
+namespace mindspore {
+namespace ops {
+
+struct RemainderTensorScalarParam {
+  ValuePtr y;
+  std::vector<int64_t> x_shape;
+  TypePtr x_type;
+  std::vector<int64_t> out_shape;
+  TypePtr out_type;
+};
+
+class TestRemainderTensorScalar :
+  public TestOps,
+  public testing::WithParamInterface<std::tuple<RemainderTensorScalarParam>> {};
+
+TEST_P(TestRemainderTensorScalar, dyn_shape) {
+  const auto &param = std::get<0>(GetParam());
+
+  RemainderTensorScalarFuncImpl remainder_tensor_scalar_func_impl;
+  auto prim = std::make_shared<Primitive>("RemainderTensorScalar");
+  auto x = std::make_shared<abstract::AbstractTensor>(param.x_type, param.x_shape);
+  auto y = param.y->ToAbstract();
+  auto expect_shape = std::make_shared<abstract::TensorShape>(param.out_shape);
+  auto expect_dtype = std::make_shared<TensorType>(param.out_type);
+
+  auto out_shape = remainder_tensor_scalar_func_impl.InferShape(prim, {x, y});
+  ASSERT_TRUE(*out_shape == *expect_shape);
+  auto out_dtype = remainder_tensor_scalar_func_impl.InferType(prim, {x, y});
+  ASSERT_TRUE(*out_dtype == *expect_dtype);
+}
+
+class TestRemainderTensorScalarSimpleInfer :
+  public TestOps,
+  public testing::WithParamInterface<std::tuple<RemainderTensorScalarParam>> {};
+
+TEST_P(TestRemainderTensorScalarSimpleInfer, simple_infer) {
+  const auto &param = std::get<0>(GetParam());
+  RemainderTensorScalarFuncImpl remainder_tensor_scalar_func_impl;
+
+  auto prim = std::make_shared<Primitive>("RemainderTensorScalar");
+  ASSERT_NE(prim, nullptr);
+
+  auto x = std::make_shared<tensor::BaseTensor>(param.x_type->type_id(), param.x_shape);
+  ASSERT_NE(x, nullptr);
+  ValuePtrList input_values;
+  input_values.push_back(std::move(x));
+  input_values.push_back(std::move(param.y));
+
+  auto expect_shape = ShapeArray{param.out_shape};
+  auto expect_type = TypePtrList{param.out_type};
+
+  auto output_shape = remainder_tensor_scalar_func_impl.InferShape(prim, input_values);
+  auto output_type = remainder_tensor_scalar_func_impl.InferType(prim, input_values);
+
+  ShapeCompare(output_shape, expect_shape);
+  TypeCompare(output_type, expect_type);
+}
+
+auto RemainderTensorScalarOpTestCases =
+  testing::ValuesIn({RemainderTensorScalarParam{CreateScalar<bool>(true), {10}, kFloat32, {10}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<bool>(true), {10, 1, 2}, kInt64, {10, 1, 2}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<float>(2.0), {10, 4, 2}, kInt64, {10, 4, 2}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<int>(2), {10, 1, -1}, kInt64, {10, 1, -1}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<int>(2), {10, 1, -1}, kFloat32, {10, 1, -1}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<bool>(false), {-2}, kInt64, {-2}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<float>(2.0), {}, kFloat32, {}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<bool>(true), {}, kInt64, {}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<int>(2), {}, kInt64, {}, kInt64}});
+
+auto RemainderTensorScalarOpSimpleInferTestCases =
+  testing::ValuesIn({RemainderTensorScalarParam{CreateScalar<bool>(true), {10}, kFloat32, {10}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<bool>(true), {10, 1, 2}, kInt64, {10, 1, 2}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<float>(2.0), {10, 4, 2}, kInt64, {10, 4, 2}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<float>(2.0), {}, kFloat32, {}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<bool>(true), {}, kInt64, {}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<int>(2), {}, kInt64, {}, kInt64}});
+
+INSTANTIATE_TEST_CASE_P(TestRemainderTensorScalar,
+                        TestRemainderTensorScalar,
+                        testing::Combine(RemainderTensorScalarOpTestCases));
+
+INSTANTIATE_TEST_CASE_P(TestRemainderTensorScalarSimpleInfer,
+                        TestRemainderTensorScalarSimpleInfer,
+                        testing::Combine(RemainderTensorScalarOpSimpleInferTestCases));
+}  // namespace ops
+}  // namespace mindspore

tests/ut/cpp/ops/test_ops_remainder_tensor_tensor.cc
@@ -1,0 +1,40 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+#include "common/common_test.h"
+#include "ops/test_ops.h"
+#include "ops/test_ops_cmp_utils.h"
+#include "ops/test_value_utils.h"
+#include "ops/ops_func_impl/remainder_tensor_tensor.h"
+
+namespace mindspore {
+namespace ops {
+OP_FUNC_IMPL_INFER_TEST_DECLARE(RemainderTensorTensor, MultiInputOpParams);
+OP_FUNC_IMPL_INFER_TEST_CASES(
+  RemainderTensorTensor,
+  testing::Values(
+    MultiInputOpParams{{{1, 3}, {2, 1}}, {kFloat32, kFloat32}, {{2, 3}}, {kFloat32}, {}},
+    MultiInputOpParams{{{-1, 3}, {-1, 1}}, {kFloat32, kFloat64}, {{-1, 3}}, {kFloat64}, {}},
+    MultiInputOpParams{{{-2}, {2, 3}}, {kFloat16, kBFloat16}, {{-2}}, {kFloat32}, {}},
+    MultiInputOpParams{{{-1, 1, 3}, {1, -1, 3}}, {kInt64, kFloat32}, {{-1, -1, 3}}, {kFloat32}, {}},
+    MultiInputOpParams{{{-1, 2, 3}, {2, -1, 3}}, {kBFloat16, kInt32}, {{2, 2, 3}}, {kBFloat16}, {}},
+    MultiInputOpParams{{{2, 1, 4, 5, 6, 9}, {9}}, {kFloat16, kInt32}, {{2, 1, 4, 5, 6, 9}}, {kFloat16}, {}},
+    MultiInputOpParams{{{2, 1, 4, -1}, {-1, -1, 4, 5}}, {kFloat32, kInt32}, {{2, -1, 4, 5}}, {kFloat32}, {}},
+    MultiInputOpParams{{{-2}, {2, 1, 4, 5, -1, 9}}, {kBFloat16, kInt64}, {{-2}}, {kBFloat16}, {}}
+  ));
+}  // namespace ops
+}  // namespace mindspore

mindspore-ai/mindspore_test

mindspore/ops/infer/ops_func_impl/remainder_scalar_tensor.cc
@@ -1,0 +1,58 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+#include "infer/ops_func_impl/remainder_scalar_tensor.h"
+#include "mindspore/ops/ops_utils/op_utils.h"
+#include "ops/ops_func_impl/simple_infer.h"
+
+namespace mindspore::ops {
+BaseShapePtr RemainderScalarTensorFuncImpl::InferShape(const PrimitivePtr &primitive,
+                                                       const std::vector<AbstractBasePtr> &input_args) const {
+  return input_args[kInputIndex1]->GetShape();
+}
+
+TypePtr RemainderScalarTensorFuncImpl::InferType(const PrimitivePtr &primitive,
+                                                 const std::vector<AbstractBasePtr> &input_args) const {
+  auto input_type = input_args[kInputIndex0]->GetType();
+  auto other_type = input_args[kInputIndex1]->GetType();
+  MS_EXCEPTION_IF_NULL(input_type);
+  MS_EXCEPTION_IF_NULL(other_type);
+  auto out_type = PromoteType(input_type, other_type, primitive->name());
+  return std::make_shared<TensorType>(out_type);
+}
+
+// simple infer
+TypePtrList RemainderScalarTensorFuncImpl::InferType(const PrimitivePtr &primitive,
+                                                     const ValuePtrList &input_values) const {
+  const auto &input_value = input_values[kInputIndex0];
+  const auto &other_tensor = input_values[kInputIndex1]->cast<tensor::BaseTensorPtr>();
+  MS_EXCEPTION_IF_NULL(input_value);
+  MS_EXCEPTION_IF_NULL(other_tensor);
+  const auto &input_type = input_value->type();
+  const auto &other_type = other_tensor->Dtype();
+  return {PromoteType(input_type, other_type, primitive->name())};
+}
+
+ShapeArray RemainderScalarTensorFuncImpl::InferShape(const PrimitivePtr &primitive,
+                                                     const ValuePtrList &input_values) const {
+  const auto &other_tensor = input_values[kInputIndex1]->cast<tensor::BaseTensorPtr>();
+  MS_EXCEPTION_IF_NULL(other_tensor);
+  return {other_tensor->shape()};
+}
+
+REGISTER_SIMPLE_INFER(kNameRemainderScalarTensor, RemainderScalarTensorFuncImpl)
+}  // namespace mindspore::ops

mindspore/ops/infer/ops_func_impl/remainder_tensor_scalar.cc
@@ -1,0 +1,58 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+#include "infer/ops_func_impl/remainder_tensor_scalar.h"
+#include "mindspore/ops/ops_utils/op_utils.h"
+#include "ops/ops_func_impl/simple_infer.h"
+
+namespace mindspore::ops {
+BaseShapePtr RemainderTensorScalarFuncImpl::InferShape(const PrimitivePtr &primitive,
+                                                       const std::vector<AbstractBasePtr> &input_args) const {
+  return input_args[kInputIndex0]->GetShape();
+}
+
+TypePtr RemainderTensorScalarFuncImpl::InferType(const PrimitivePtr &primitive,
+                                                 const std::vector<AbstractBasePtr> &input_args) const {
+  auto input_type = input_args[kInputIndex0]->GetType();
+  auto other_type = input_args[kInputIndex1]->GetType();
+  MS_EXCEPTION_IF_NULL(input_type);
+  MS_EXCEPTION_IF_NULL(other_type);
+  auto out_type = PromoteType(input_type, other_type, primitive->name());
+  return std::make_shared<TensorType>(out_type);
+}
+
+// simple infer
+TypePtrList RemainderTensorScalarFuncImpl::InferType(const PrimitivePtr &primitive,
+                                                     const ValuePtrList &input_values) const {
+  const auto &input_tensor = input_values[kInputIndex0]->cast<tensor::BaseTensorPtr>();
+  const auto &other_value = input_values[kInputIndex1];
+  MS_EXCEPTION_IF_NULL(input_tensor);
+  MS_EXCEPTION_IF_NULL(other_value);
+  const auto &input_type = input_tensor->Dtype();
+  const auto &other_type = other_value->type();
+  return {PromoteType(input_type, other_type, primitive->name())};
+}
+
+ShapeArray RemainderTensorScalarFuncImpl::InferShape(const PrimitivePtr &primitive,
+                                                     const ValuePtrList &input_values) const {
+  const auto &input_tensor = input_values[kInputIndex0]->cast<tensor::BaseTensorPtr>();
+  MS_EXCEPTION_IF_NULL(input_tensor);
+  return {input_tensor->shape()};
+}
+
+REGISTER_SIMPLE_INFER(kNameRemainderTensorScalar, RemainderTensorScalarFuncImpl)
+}  // namespace mindspore::ops

tests/ut/cpp/ops/test_ops_remainder_tensor_scalar.cc
@@ -1,0 +1,116 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vector>
+#include <memory>
+#include "common/common_test.h"
+#include "ir/dtype/type.h"
+#include "abstract/dshape.h"
+#include "utils/tensor_construct_utils.h"
+#include "ir/primitive.h"
+#include "abstract/abstract_value.h"
+#include "include/backend/optimizer/helper.h"
+#include "ops/test_ops.h"
+#include "infer/ops_func_impl/remainder_tensor_scalar.h"
+#include "ops/test_value_utils.h"
+#include "ops/test_ops_cmp_utils.h"
+
+namespace mindspore {
+namespace ops {
+
+struct RemainderTensorScalarParam {
+  ValuePtr y;
+  std::vector<int64_t> x_shape;
+  TypePtr x_type;
+  std::vector<int64_t> out_shape;
+  TypePtr out_type;
+};
+
+class TestRemainderTensorScalar :
+  public TestOps,
+  public testing::WithParamInterface<std::tuple<RemainderTensorScalarParam>> {};
+
+TEST_P(TestRemainderTensorScalar, dyn_shape) {
+  const auto &param = std::get<0>(GetParam());
+
+  RemainderTensorScalarFuncImpl remainder_tensor_scalar_func_impl;
+  auto prim = std::make_shared<Primitive>("RemainderTensorScalar");
+  auto x = std::make_shared<abstract::AbstractTensor>(param.x_type, param.x_shape);
+  auto y = param.y->ToAbstract();
+  auto expect_shape = std::make_shared<abstract::TensorShape>(param.out_shape);
+  auto expect_dtype = std::make_shared<TensorType>(param.out_type);
+
+  auto out_shape = remainder_tensor_scalar_func_impl.InferShape(prim, {x, y});
+  ASSERT_TRUE(*out_shape == *expect_shape);
+  auto out_dtype = remainder_tensor_scalar_func_impl.InferType(prim, {x, y});
+  ASSERT_TRUE(*out_dtype == *expect_dtype);
+}
+
+class TestRemainderTensorScalarSimpleInfer :
+  public TestOps,
+  public testing::WithParamInterface<std::tuple<RemainderTensorScalarParam>> {};
+
+TEST_P(TestRemainderTensorScalarSimpleInfer, simple_infer) {
+  const auto &param = std::get<0>(GetParam());
+  RemainderTensorScalarFuncImpl remainder_tensor_scalar_func_impl;
+
+  auto prim = std::make_shared<Primitive>("RemainderTensorScalar");
+  ASSERT_NE(prim, nullptr);
+
+  auto x = std::make_shared<tensor::BaseTensor>(param.x_type->type_id(), param.x_shape);
+  ASSERT_NE(x, nullptr);
+  ValuePtrList input_values;
+  input_values.push_back(std::move(x));
+  input_values.push_back(std::move(param.y));
+
+  auto expect_shape = ShapeArray{param.out_shape};
+  auto expect_type = TypePtrList{param.out_type};
+
+  auto output_shape = remainder_tensor_scalar_func_impl.InferShape(prim, input_values);
+  auto output_type = remainder_tensor_scalar_func_impl.InferType(prim, input_values);
+
+  ShapeCompare(output_shape, expect_shape);
+  TypeCompare(output_type, expect_type);
+}
+
+auto RemainderTensorScalarOpTestCases =
+  testing::ValuesIn({RemainderTensorScalarParam{CreateScalar<bool>(true), {10}, kFloat32, {10}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<bool>(true), {10, 1, 2}, kInt64, {10, 1, 2}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<float>(2.0), {10, 4, 2}, kInt64, {10, 4, 2}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<int>(2), {10, 1, -1}, kInt64, {10, 1, -1}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<int>(2), {10, 1, -1}, kFloat32, {10, 1, -1}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<bool>(false), {-2}, kInt64, {-2}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<float>(2.0), {}, kFloat32, {}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<bool>(true), {}, kInt64, {}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<int>(2), {}, kInt64, {}, kInt64}});
+
+auto RemainderTensorScalarOpSimpleInferTestCases =
+  testing::ValuesIn({RemainderTensorScalarParam{CreateScalar<bool>(true), {10}, kFloat32, {10}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<bool>(true), {10, 1, 2}, kInt64, {10, 1, 2}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<float>(2.0), {10, 4, 2}, kInt64, {10, 4, 2}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<float>(2.0), {}, kFloat32, {}, kFloat32},
+                     RemainderTensorScalarParam{CreateScalar<bool>(true), {}, kInt64, {}, kInt64},
+                     RemainderTensorScalarParam{CreateScalar<int>(2), {}, kInt64, {}, kInt64}});
+
+INSTANTIATE_TEST_CASE_P(TestRemainderTensorScalar,
+                        TestRemainderTensorScalar,
+                        testing::Combine(RemainderTensorScalarOpTestCases));
+
+INSTANTIATE_TEST_CASE_P(TestRemainderTensorScalarSimpleInfer,
+                        TestRemainderTensorScalarSimpleInfer,
+                        testing::Combine(RemainderTensorScalarOpSimpleInferTestCases));
+}  // namespace ops
+}  // namespace mindspore

tests/ut/cpp/ops/test_ops_remainder_tensor_tensor.cc
@@ -1,0 +1,40 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+#include "common/common_test.h"
+#include "ops/test_ops.h"
+#include "ops/test_ops_cmp_utils.h"
+#include "ops/test_value_utils.h"
+#include "infer/ops_func_impl/remainder_tensor_tensor.h"
+
+namespace mindspore {
+namespace ops {
+OP_FUNC_IMPL_INFER_TEST_DECLARE(RemainderTensorTensor, MultiInputOpParams);
+OP_FUNC_IMPL_INFER_TEST_CASES(
+  RemainderTensorTensor,
+  testing::Values(
+    MultiInputOpParams{{{1, 3}, {2, 1}}, {kFloat32, kFloat32}, {{2, 3}}, {kFloat32}, {}},
+    MultiInputOpParams{{{-1, 3}, {-1, 1}}, {kFloat32, kFloat64}, {{-1, 3}}, {kFloat64}, {}},
+    MultiInputOpParams{{{-2}, {2, 3}}, {kFloat16, kBFloat16}, {{-2}}, {kFloat32}, {}},
+    MultiInputOpParams{{{-1, 1, 3}, {1, -1, 3}}, {kInt64, kFloat32}, {{-1, -1, 3}}, {kFloat32}, {}},
+    MultiInputOpParams{{{-1, 2, 3}, {2, -1, 3}}, {kBFloat16, kInt32}, {{2, 2, 3}}, {kBFloat16}, {}},
+    MultiInputOpParams{{{2, 1, 4, 5, 6, 9}, {9}}, {kFloat16, kInt32}, {{2, 1, 4, 5, 6, 9}}, {kFloat16}, {}},
+    MultiInputOpParams{{{2, 1, 4, -1}, {-1, -1, 4, 5}}, {kFloat32, kInt32}, {{2, -1, 4, 5}}, {kFloat32}, {}},
+    MultiInputOpParams{{{-2}, {2, 1, 4, 5, -1, 9}}, {kBFloat16, kInt64}, {{-2}}, {kBFloat16}, {}}
+  ));
+}  // namespace ops
+}  // namespace mindspore