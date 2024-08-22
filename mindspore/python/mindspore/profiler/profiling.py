062d93aeb54af12a08121f7811cc43b46ef1f76a
臧庆香 <zangqingxiang@huawei.com>
2024-08-22 16:13:26 +0800

    with_stack doc

--- mindspore/python/mindspore/profiler/profiling.py
+++ mindspore/python/mindspore/profiler/profiling.py
@@ -414,7 +414,8 @@ class Profiler:
             directory are retained to save disk space.
             Default value: ``True`` .
         with_stack (bool, optional): (Ascend) Whether to collect frame host call stack data on the Python side. This
-            data is presented in the form of a flame graph in the timeline. Default value: ``False`` .
+            data is presented in the form of a flame graph in the timeline. When using this parameter, the parameter
+            op_time must be set to True. Default value: ``False`` .
 
     Raises:
         RuntimeError: When the version of CANN does not match the version of MindSpore,
