mindspore-ai/mindspore

add stress detect

mindspore/ccsrc/plugin/device/ascend/hal/hardware/ge_device_res_manager.cc
@@ -29,6 +29,7 @@
 #include "plugin/device/ascend/hal/device/ascend_event.h"
 #include "plugin/device/ascend/hal/device/ascend_pin_mem_pool.h"
 #include "plugin/device/cpu/hal/device/cpu_device_synchronizer.h"
+#include "plugin/device/ascend/kernel/pyboost/customize/stress_detect.h"
 #include "include/transform/graph_ir/utils.h"
 #include "graph/types.h"
 #include "transform/symbol/acl_rt_symbol.h"
@@ -497,6 +498,15 @@ size_t GeDeviceResManager::DefaultStream() const {
     return SIZE_MAX;
   }
   return AscendStreamMng::GetInstance().default_stream_id();
+}
+
+int GeDeviceResManager::StressDetect() const {
+  if (!BindDeviceToCurrentThread(false)) {
+    MS_LOG(ERROR) << "Bind context to current thread failed";
+    return 0;
+  }
+  MS_EXCEPTION_IF_NULL(device_context_);
+  return kernel::pyboost::StressDetectKernel(device_context_, AscendStreamMng::GetInstance().default_stream_id());
 }
 
 // ACL_EVENT_TIME_LINE: indicates that the number of created events is not limited, and the created events can be used

mindspore/ccsrc/pybind_api/utils/stress_detect_py.cc
@@ -1,0 +1,47 @@
+/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "pybind_api/utils/stress_detect_py.h"
+#include <utility>
+#include "runtime/pynative/op_executor.h"
+#include "runtime/hardware/device_context_manager.h"
+#include "utils/ms_context.h"
+#include "include/common/pybind_api/api_register.h"
+#include "runtime/device/multi_stream_controller.h"
+
+namespace mindspore {
+namespace {
+DeviceContext *GetDeviceCtx() {
+  const auto &device_name = MsContext::GetInstance()->get_param<std::string>(MS_CTX_DEVICE_TARGET);
+  auto device_ctx = device::DeviceContextManager::GetInstance().GetOrCreateDeviceContext(
+    {device_name, MsContext::GetInstance()->get_param<uint32_t>(MS_CTX_DEVICE_ID)});
+  MS_EXCEPTION_IF_NULL(device_ctx);
+
+  device_ctx->Initialize();
+  return device_ctx;
+}
+}  // namespace
+
+int StressDetect() {
+  auto device_ctx = GetDeviceCtx();
+  runtime::OpExecutor::GetInstance().WaitAll();
+  device::MultiStreamController::GetInstance()->Refresh(device_ctx);
+  (void)device::MultiStreamController::GetInstance()->SyncAllStreams(device_ctx);
+  device_ctx->device_res_manager_->StressDetect();
+  return 0;
+}
+
+void RegStress(py::module *m) { (void)m->def("stress_detect", &mindspore::StressDetect, "Detect stress"); }
+}  // namespace mindspore

mindspore-ai/mindspore_test

mindspore/ccsrc/plugin/device/ascend/hal/hardware/ge_device_res_manager.cc
@@ -31,6 +31,7 @@
 #include "plugin/device/ascend/hal/device/ascend_event.h"
 #include "plugin/device/ascend/hal/device/ascend_pin_mem_pool.h"
 #include "plugin/device/cpu/hal/device/cpu_device_synchronizer.h"
+#include "mindspore/ops/kernel/ascend/pyboost/customize/stress_detect.h"
 #include "include/transform/graph_ir/utils.h"
 #include "transform/symbol/acl_rt_symbol.h"
 #include "transform/symbol/symbol_utils.h"
@@ -656,6 +657,15 @@ TensorPtr GeDeviceResManager::GetSliceByPaddingShapeHandle(const tensor::TensorP
                 << ", shape:" << shape << ", data_type:" << TypeIdToString(type_id);
   tensor->set_device_address(device_address);
   return tensor;
+}
+
+int GeDeviceResManager::StressDetect() const {
+  if (!BindDeviceToCurrentThread(false)) {
+    MS_LOG(ERROR) << "Bind context to current thread failed";
+    return 0;
+  }
+  MS_EXCEPTION_IF_NULL(device_context_);
+  return kernel::pyboost::StressDetectKernel(device_context_);
 }
 
 // ACL_EVENT_TIME_LINE: indicates that the number of created events is not limited, and the created events can be used

mindspore/ccsrc/pybind_api/utils/stress_detect_py.cc
@@ -1,0 +1,49 @@
+/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "pybind_api/utils/stress_detect_py.h"
+#include <utility>
+#include "runtime/pynative/op_executor.h"
+#include "runtime/pipeline/pipeline.h"
+#include "runtime/hardware/device_context_manager.h"
+#include "utils/ms_context.h"
+#include "include/common/pybind_api/api_register.h"
+#include "runtime/device/multi_stream_controller.h"
+
+namespace mindspore {
+namespace {
+DeviceContext *GetDeviceCtx() {
+  const auto &device_name = MsContext::GetInstance()->get_param<std::string>(MS_CTX_DEVICE_TARGET);
+  auto device_ctx = device::DeviceContextManager::GetInstance().GetOrCreateDeviceContext(
+    {device_name, MsContext::GetInstance()->get_param<uint32_t>(MS_CTX_DEVICE_ID)});
+  MS_EXCEPTION_IF_NULL(device_ctx);
+
+  device_ctx->Initialize();
+  return device_ctx;
+}
+}  // namespace
+
+int StressDetect() {
+  auto device_ctx = GetDeviceCtx();
+  runtime::Pipeline::Get().WaitAll();
+  device::MultiStreamController::GetInstance()->Refresh(device_ctx);
+  (void)device::MultiStreamController::GetInstance()->SyncAllStreams(device_ctx);
+  MS_EXCEPTION_IF_NULL(device_ctx);
+  MS_EXCEPTION_IF_NULL(device_ctx->device_res_manager_);
+  return device_ctx->device_res_manager_->StressDetect();
+}
+
+void RegStress(py::module *m) { (void)m->def("stress_detect", &mindspore::StressDetect, "Detect stress"); }
+}  // namespace mindspore