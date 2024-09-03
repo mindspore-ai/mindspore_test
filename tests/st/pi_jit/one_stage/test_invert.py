5f09c11b128ac454947a994b87cf53594278fca6
luoxuewei <luoxuewei@huawei.com>
2024-09-03 15:16:35 +0800

    重载invert函数

--- tests/st/pi_jit/one_stage/test_invert.py
+++ tests/st/pi_jit/one_stage/test_invert.py
@@ -19,6 +19,7 @@ import mindspore as ms
 import mindspore.nn as nn
 from mindspore import Tensor, context
 from mindspore.common.api import jit
+from mindspore._c_expression import jit_mode_pi_enable, jit_mode_pi_disable, get_code_extra
 from tests.mark_utils import arg_mark
 
 context.set_context(mode=context.GRAPH_MODE)
@@ -31,13 +32,21 @@ def test_invert_operation_int():
     Expectation: No exception.
     """
     class InvertNet(nn.Cell):
-        @jit
+        @jit(mode="PIJit")
         def construct(self, x):
             return ~x
+
+    context.set_context(mode=context.PYNATIVE_MODE)
     invert = InvertNet()
     x = 1
-    jit_x = invert(x)
-    assert jit_x == -2
+    jit_mode_pi_disable()
+    pynative_x = invert(x)
+    jit_mode_pi_enable()
+    pijit_x = invert(x)
+    jcr = get_code_extra(InvertNet.construct.__wrapped__)
+    assert jcr["break_count_"] == 0
+    assert pynative_x == pijit_x
+    jit_mode_pi_disable()
 
 
 @arg_mark(plat_marks=['cpu_linux'], level_mark='level0', card_mark='onecard', essential_mark='essential')
@@ -48,16 +57,25 @@ def test_invert_operation_bool():
     Expectation: No exception.
     """
     class InvertNet(nn.Cell):
-        @jit
+        @jit(mode="PIJit")
         def construct(self, x):
             return ~x
+
+    context.set_context(mode=context.PYNATIVE_MODE)
     invert = InvertNet()
     x = True
     y = False
-    jit_x = invert(x)
-    jit_y = invert(y)
-    assert jit_x == -2
-    assert jit_y == -1
+    jit_mode_pi_disable()
+    pynative_x = invert(x)
+    pynative_y = invert(y)
+    jit_mode_pi_enable()
+    pijit_x = invert(x)
+    pijit_y = invert(y)
+    jcr = get_code_extra(InvertNet.construct.__wrapped__)
+    assert jcr["break_count_"] == 0
+    assert pynative_x == pijit_x
+    assert pynative_y == pijit_y
+    jit_mode_pi_disable()
 
 @arg_mark(plat_marks=['cpu_linux'], level_mark='level0', card_mark='onecard', essential_mark='essential')
 def test_invert_operation_tensor():
@@ -67,15 +85,22 @@ def test_invert_operation_tensor():
     Expectation: No exception.
     """
     class InvertNet(nn.Cell):
-        @jit
+        @jit(mode="PIJit")
         def construct(self, x):
             return ~x
+
+    context.set_context(mode=context.PYNATIVE_MODE)
     invert = InvertNet()
-    x = Tensor([1, 2, 3, 4])
-    jit_x = invert(x)
-    ret = Tensor([False, False, False, False])
-    ret = ret == jit_x
+    x = Tensor([1,2,3,4])
+    jit_mode_pi_disable()
+    pynative_x = invert(x)
+    jit_mode_pi_enable()
+    pijit_x = invert(x)
+    jcr = get_code_extra(InvertNet.construct.__wrapped__)
+    assert jcr["break_count_"] == 0
+    ret = pynative_x == pijit_x
     assert ms.ops.all(ret).item()
+    jit_mode_pi_disable()
 
 @arg_mark(plat_marks=['cpu_linux'], level_mark='level0', card_mark='onecard', essential_mark='essential')
 def test_invert_operation_tensor_bool():
@@ -85,16 +110,22 @@ def test_invert_operation_tensor_bool():
     Expectation: No exception.
     """
     class InvertNet(nn.Cell):
-        @jit
+        @jit(mode="PIJit")
         def construct(self, x):
             return ~x
+
+    context.set_context(mode=context.PYNATIVE_MODE)
     invert = InvertNet()
     x = Tensor([True, False])
-    jit_x = invert(x)
-    ret = Tensor([False, True])
-    ret = ret == jit_x
+    jit_mode_pi_disable()
+    pynative_x = invert(x)
+    jit_mode_pi_enable()
+    pijit_x = invert(x)
+    jcr = get_code_extra(InvertNet.construct.__wrapped__)
+    assert jcr["break_count_"] == 0
+    ret = pynative_x == pijit_x
     assert ms.ops.all(ret).item()
-
+    jit_mode_pi_disable()
 
 @arg_mark(plat_marks=['cpu_linux'], level_mark='level0', card_mark='onecard', essential_mark='essential')
 def test_invert_operation_float():
@@ -104,11 +135,17 @@ def test_invert_operation_float():
     Expectation: No exception.
     """
     class InvertNet(nn.Cell):
-        @jit
+        @jit(mode="PIJit")
         def construct(self, x):
             return ~x
+
+    context.set_context(mode=context.PYNATIVE_MODE)
     invert = InvertNet()
     x = 1.1
+    jit_mode_pi_disable()
     with pytest.raises(TypeError) as err:
         invert(x)
+    jcr = get_code_extra(InvertNet.construct.__wrapped__)
+    assert jcr["break_count_"] == 0
     assert "bad operand type for unary ~:" in str(err)
+    jit_mode_pi_disable()
