0afd11c8a4a85e7a6e4a5c6a2e29f894d244be57
wujueying <wujueying1@huawei.com>
2024-09-04 21:16:48 +0800

    fix cholesky case

--- tests/st/tensor/test_cholesky.py
+++ tests/st/tensor/test_cholesky.py
@@ -27,7 +27,7 @@ class CholeskyTensorNet(nn.Cell):
         return x.cholesky(upper=False)
 
 
-@arg_mark(plat_marks=['cpu_linux', 'cpu_windows', 'cpu_macos', 'platform_ascend'],
+@arg_mark(plat_marks=['cpu_linux', 'cpu_windows', 'cpu_macos'],
           level_mark='level2',
           card_mark='onecard',
           essential_mark='unessential')
