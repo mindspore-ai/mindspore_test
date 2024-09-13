a5ce1f47edd218d54ea0f8fe9e6787cb83ebc3ea
PingqiLi <lipingqi2018@gmail.com>
2024-09-13 19:44:39 +0800

    Refactor: change pyboost code generation in OOP style

--- mindspore/python/mindspore/ops_generate/gen_constants.py
+++ mindspore/python/mindspore/ops_generate/gen_constants.py
@@ -13,9 +13,50 @@
 # limitations under the License.
 # ============================================================================
 """
-Generate constants
+This module contains the constant strings used in generating ops files.
+
+Constants:
+    PY_LICENSE: License strings used for .py files
+    CC_LICENSE: License strings used for .h/.cc files
+    ......
+    Other constant strings in the module are used for generation paths
 """
+
 import os
+
+# py license
+PY_LICENSE = f"""# Copyright 2024 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+
+# cc license
+CC_LICENSE = f"""/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */"""
 
 # op_def
 OP_DEF_AUTO_GENERATE_PATH = "op_def/auto_generate"
@@ -24,6 +65,9 @@ YAML_PATH = "op_def/yaml"
 MS_YAML_PATH = "mindspore/ops/" + YAML_PATH
 PY_AUTO_GEN_PATH = "mindspore/python/mindspore/ops/auto_generate"
 PY_OPS_GEN_PATH = "mindspore/python/mindspore/ops_generate"
+PYBOOST_NATIVE_GRAD_FUNC_GEN_PATH = "mindspore/ccsrc/pipeline/pynative/grad/function/auto_generate"
+PIPELINE_PYBOOST_FUNC_GEN_PATH = "mindspore/ccsrc/pipeline/pynative/op_function/auto_generate"
+RUNTIME_PYBOOST_FUNC_GEN_PATH = "mindspore/ccsrc/runtime/pynative/op_function/auto_generate"
 
 # infer
 MS_OPS_FUNC_IMPL_PATH = "mindspore/ops/infer/ops_func_impl"
