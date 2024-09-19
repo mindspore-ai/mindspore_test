32eb1e925fe08e449fb0eda510c06eb3c0d4c498
qiuleilei <qiuleilei1@huawei.com>
2024-09-19 10:13:55 +0800

    【问题单：IAOSU4】bidense算子的fp16精度误差问题

--- tests/st/ops/test_func_bidense.py
+++ tests/st/ops/test_func_bidense.py
@@ -60,8 +60,8 @@ def test_ops_bidense_two_dim(mode):
     assert np.allclose(output.asnumpy(), expect_output, rtol, atol)
 
 
-@arg_mark(plat_marks=['platform_ascend', 'platform_gpu', 'cpu_linux', 'cpu_windows', 'cpu_macos'], level_mark='level2',
-          card_mark='onecard', essential_mark='unessential')
+@arg_mark(plat_marks=['platform_ascend910b', 'platform_gpu', 'cpu_linux', 'cpu_windows', 'cpu_macos'],
+          level_mark='level2', card_mark='onecard', essential_mark='unessential')
 @pytest.mark.parametrize('mode', [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
 def test_ops_bidense_three_dim(mode):
     """
