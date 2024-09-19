40cfeed7e1a77d25e088ba5a97057173dad81293
panzhihui <panzhihui3@huawei.com>
2024-09-19 09:44:23 +0800

    Inferinfo add GetScalarValueWithCheck

--- mindspore/core/include/ops/infer_info/infer_info.h
+++ mindspore/core/include/ops/infer_info/infer_info.h
@@ -62,6 +62,15 @@ class MS_CORE_API InferInfo {
   }
 
   template <class T>
+  T GetScalarValueWithCheck() {
+    const auto &opt = GetScalarValue<T>();
+    if (!opt.has_value()) {
+      MS_LOG(EXCEPTION) << "Unable to get scalar value, " << BaseDebugInfo();
+    }
+    return opt.value();
+  }
+
+  template <class T>
   std::optional<ArrayValue<T>> GetArrayValue() {
     if (MS_UNLIKELY(IsNone())) {
       MS_LOG(EXCEPTION) << "Calling GetArrayValue on a None object, " << BaseDebugInfo();
