7d645ef2a947340c5615a15a402565496ea336a2
张清华 <zhangqinghua3@huawei.com>
2024-09-25 17:16:03 +0800

    Add jit block and trace function.

--- mindspore/ccsrc/pipeline/jit/ps/pipeline.cc
+++ mindspore/ccsrc/pipeline/jit/ps/pipeline.cc
@@ -663,8 +663,9 @@ py::object GraphExecutorPy::GenerateArgumentsKey(const py::object &obj, const py
 
   static uint64_t key_counter = 0;
   kArgsCache[args_abs] = key_counter;
-  kCellArgsMap[obj.ptr()] = args_abs;
-
+  if (!py::isinstance<py::none>(obj)) {
+    kCellArgsMap[obj.ptr()] = args_abs;
+  }
   MS_LOG(INFO) << "Generate a new compile key for new args, key: " << key_counter;
   if (IS_OUTPUT_ON(mindspore::kInfo)) {
     std::ostringstream buffer;
