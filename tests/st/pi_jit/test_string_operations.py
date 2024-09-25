aba6c67eed47cbc28f41b1d313c95819dc0e1689
shaoshengqi <shaoshengqi@huawei.com>
2024-09-25 10:56:47 +0800

    fix build f-string core dump caused by nullptr

--- tests/st/pi_jit/test_string_operations.py
+++ tests/st/pi_jit/test_string_operations.py
@@ -16,13 +16,20 @@
 from mindspore import Tensor
 from mindspore import context
 from mindspore.common.api import jit
-from .share.utils import match_array, assert_executed_by_graph_mode
+from mindspore._c_expression import get_code_extra
+
+from .share.utils import assert_executed_by_graph_mode, match_array
 from tests.mark_utils import arg_mark
 
 
-cfg = {
-    "compile_by_trace": True,
-}
+cfg = {"compile_with_try": False}
+
+
+def assert_graph_break_count(func, break_count: int):
+    jcr = get_code_extra(getattr(func, "__wrapped__", func))
+    assert jcr is not None
+    assert jcr['stat'] == 'GRAPH_CALLABLE'
+    assert jcr['break_count_'] == break_count
 
 
 @arg_mark(plat_marks=['cpu_linux'], level_mark='level0', card_mark='onecard', essential_mark='essential')
@@ -61,6 +68,50 @@ def test_build_fstring_case_2():
     s2 = fn('hello', ' world')
 
     assert s1 == s2
+
+
+@arg_mark(plat_marks=['cpu_linux'], level_mark='level0', card_mark='onecard', essential_mark='essential')
+def test_build_fstring_case_3():
+    """
+    Feature: Test build f-string.
+    Description: Use int variable to build f-string.
+    Expectation: No exception, but has graph break.
+    """
+    def fn(x: int):
+        return f'x={x}'
+
+    context.set_context(mode=context.PYNATIVE_MODE)
+    s1 = fn(1)
+
+    fn = jit(fn, mode="PIJit", jit_config=cfg)
+    s2 = fn(1)
+
+    assert s1 == s2
+    assert_graph_break_count(fn, 0)
+
+
+@arg_mark(plat_marks=['cpu_linux'], level_mark='level0', card_mark='onecard', essential_mark='essential')
+def test_build_fstring_case_4():
+    """
+    Feature: Test build f-string.
+    Description: Use int variable to build f-string, and use this string to do dict getitem.
+    Expectation: No exception, but has graph break.
+    """
+    def fn(x: int, d: dict):
+        i = x * 2
+        k = f'a{i}'
+        v = d[k]
+        return v + x
+
+    context.set_context(mode=context.PYNATIVE_MODE)
+    x = 1
+    d = {'a1': Tensor([1, 2, 3]), 'a2': Tensor([2, 4, 6])}
+    o1 = fn(x, d)
+
+    fn = jit(fn, mode="PIJit", jit_config=cfg)
+    o2 = fn(x, d)
+
+    match_array(o1, o2)
 
 
 @arg_mark(plat_marks=['cpu_linux'], level_mark='level0', card_mark='onecard', essential_mark='essential')
