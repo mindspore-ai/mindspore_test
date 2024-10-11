35db374e0d5d18d278430d4e24e38e0358673a94
chujinjin <chujinjin52@huawei.com>
2024-10-11 19:58:18 +0800

    fix nllloss to online compile

--- mindspore/ccsrc/transform/acl_ir/acl_declare/other_ops.cc
+++ mindspore/ccsrc/transform/acl_ir/acl_declare/other_ops.cc
@@ -30,6 +30,7 @@ REGISTER_ACL_OP(UpsampleNearest3d).set_run_mode(false);
 REGISTER_ACL_OP(CheckValid).set_run_mode(false);
 REGISTER_ACL_OP(SpaceToDepth).set_run_mode(false);
 REGISTER_ACL_OP(Renorm).set_run_mode(false);
+REGISTER_ACL_OP(NLLLoss).set_run_mode(false);
 
 REGISTER_ACL_OP(HistogramFixedWidth).set_extra_supported_datatype({ge::DT_DOUBLE});
 
