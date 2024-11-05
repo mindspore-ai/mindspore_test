6bf900502a60499c031e26a62cca6067a2770255
yide12 <wangyide10@huawei.com>
2024-11-05 16:52:14 +0800

    fix_unified_name_map_bug

--- mindspore/python/mindspore/parallel/transform_safetensors.py
+++ mindspore/python/mindspore/parallel/transform_safetensors.py
@@ -915,12 +915,16 @@ def unified_safetensors(src_dir, src_strategy_file, dst_dir, merge_with_redundan
     layout_map = _convert_to_list(src_strategy_dict)
 
     total_size = 0
+    actual_params = set()
     for _, file_name in all_safetensor_files_map.items():
         total_size += os.path.getsize(file_name) / 1024 / 1024 / 1024
+        with safe_open(file_name, framework="np") as f:
+            actual_params.update(f.keys())
     split_num = math.ceil(total_size / 3)
+    params_to_store = actual_params & set(layout_map.keys())
 
     name_list = []
-    for name in list(layout_map.keys()):
+    for name in list(params_to_store):
         if name.startswith("accu_grads"):
             continue
         name_list.append(name)
