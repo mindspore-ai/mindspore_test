eaf10c9025ce1e16cca8ccad51a6a5ca301ffd2f
kangwei <kangwei13@huawei.com>
2024-11-08 10:42:15 +0800

    pow

---
+++ mindspore/ops/infer/ops_func_impl/pow_tensor_scalar.cc
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "infer/ops_func_impl/pow_tensor_scalar.h"
+#include <set>
+#include "ops/ops_func_impl/simple_infer.h"
+#include "mindspore/ops/ops_utils/op_utils.h"
+
+namespace mindspore::ops {
+BaseShapePtr PowTensorScalarFuncImpl::InferShape(const PrimitivePtr &primitive,
+                                                 const std::vector<AbstractBasePtr> &input_args) const {
+  auto input_shape = input_args[kInputIndex0]->GetShape();
+  return input_shape->Clone();
+}
+
+TypePtr PowCheckAndInferType(const TypeId input_type_id, const TypeId exp_type_id, const TypePtr input_type,
+                             const ValuePtr exp_value) {
+  static const std::vector<TypeId> int_or_bool = {kNumberTypeUInt8, kNumberTypeInt8,  kNumberTypeInt16,
+                                                  kNumberTypeInt32, kNumberTypeInt64, kNumberTypeBool};
+  static const std::set<TypeId> valid_types = {
+    kNumberTypeUInt8, kNumberTypeInt8,    kNumberTypeInt16,   kNumberTypeInt32,   kNumberTypeInt64,
+    kNumberTypeBool,  kNumberTypeFloat16, kNumberTypeFloat32, kNumberTypeFloat64, kNumberTypeBFloat16};
+  if (valid_types.find(input_type_id) == valid_types.end()) {
+    MS_EXCEPTION(TypeError) << "For primitive [PowTensorScalar], 'input' type is not supported, 'input' type: "
+                            << input_type->ToString();
+  }
+  bool is_int_or_bool = std::any_of(int_or_bool.begin(), int_or_bool.end(),
+                                    [&input_type_id](const TypeId &type_id) { return input_type_id == type_id; });
+  if (exp_type_id == kNumberTypeFloat32 && is_int_or_bool) {
+    return kFloat32;
+  }
+
+  if (exp_type_id == kNumberTypeInt64 && input_type_id == kNumberTypeBool) {
+    return kInt64;
+  }
+
+  if (exp_type_id == kNumberTypeBool && input_type_id == kNumberTypeBool) {
+    MS_EXCEPTION(TypeError)
+      << "For primitive [PowTensorScalar], 'input' and 'exponent' cannot be bool at the same time.";
+  }
+
+  if (exp_type_id == kNumberTypeInt64 && input_type_id != kNumberTypeBool && is_int_or_bool) {
+    auto exp_opt = GetScalarValue<int64_t>(exp_value);
+    if (MS_UNLIKELY(exp_opt.has_value() && exp_opt.value() < 0)) {
+      MS_EXCEPTION(RuntimeError)
+        << "For primitive [PowTensorScalar], Integers to negative integer powers are not allowed.";
+    }
+  }
+
+  return input_type;
+}
+
+TypePtr PowTensorScalarFuncImpl::InferType(const PrimitivePtr &primitive,
+                                           const std::vector<AbstractBasePtr> &input_args) const {
+  const auto &input_type = input_args[kInputIndex0]->GetType();
+  auto input_type_id = input_type->cast<TensorTypePtr>()->element()->type_id();
+  const auto &exp_type = input_args[kInputIndex1]->GetType();
+  auto exp_type_id = exp_type->type_id();
+
+  return PowCheckAndInferType(input_type_id, exp_type_id, input_type, input_args[kInputIndex1]->GetValue());
+}
+
+TypePtrList PowTensorScalarFuncImpl::InferType(const PrimitivePtr &primitive, const ValuePtrList &input_values) const {
+  const auto &x_tensor = input_values[kInputIndex0]->cast<tensor::BaseTensorPtr>();
+  auto input_type_id = x_tensor->Dtype()->type_id();
+  const auto &exp_type = input_values[kInputIndex1]->type();
+  auto exp_type_id = exp_type->type_id();
+
+  return {PowCheckAndInferType(input_type_id, exp_type_id, x_tensor->Dtype(), input_values[kInputIndex1])};
+}
+ShapeArray PowTensorScalarFuncImpl::InferShape(const PrimitivePtr &primitive, const ValuePtrList &input_values) const {
+  const auto &x_tensor = input_values[kInputIndex0]->cast<tensor::BaseTensorPtr>();
+  MS_EXCEPTION_IF_NULL(x_tensor);
+  return {x_tensor->shape()};
+}
+REGISTER_SIMPLE_INFER(kNamePowTensorScalar, PowTensorScalarFuncImpl)
+}  // namespace mindspore::ops
---
+++ tests/st/ops/test_ops_pow_tensor_scalar.py
@@ -0,0 +1,117 @@
+# Copyright 2024 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+import numpy as np
+import pytest
+from tests.st.utils import test_utils
+from tests.st.ops.dynamic_shape.test_op_utils import TEST_OP
+from tests.mark_utils import arg_mark
+from mindspore import mint, ops
+import mindspore as ms
+
+
+def generate_random_input(shape, dtype):
+    return np.random.uniform(0.9, 1.0, size=shape).astype(dtype), np.random.uniform(0.9, 1.0, size=shape).astype(dtype)
+
+
+@test_utils.run_with_cell
+def pow_forward_func(x, y):
+    return mint.pow(x, y)
+
+
+@test_utils.run_with_cell
+def pow_backward_func(x, y):
+    return ops.grad(pow_forward_func, (0, 1))(x, y) # pylint: disable=not-callable
+
+
+@arg_mark(plat_marks=['platform_ascend', 'platform_ascend910b'], level_mark='level0', card_mark='onecard',
+          essential_mark='essential')
+@pytest.mark.parametrize("context_mode", [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
+@pytest.mark.parametrize("data_type", [np.float32])
+@test_utils.run_test_with_On
+def test_pow_op_forward(context_mode, data_type):
+    """
+    Feature: Ops.
+    Description: test op pow forward.
+    Expectation: expect correct result.
+    """
+    ms.context.set_context(mode=context_mode)
+    if context_mode == ms.GRAPH_MODE:
+        ms.set_context(jit_config={'jit_level': 'O0'})
+    x_np, _ = generate_random_input((2, 3, 4, 5), dtype=data_type)
+    y_np = 0
+
+    x = ms.Tensor(x_np)
+    y = y_np
+    out = pow_forward_func(x, y)
+    expect_out = np.power(x_np, y_np)
+    np.testing.assert_allclose(out.asnumpy(), expect_out, rtol=1e-3)
+
+    x_np, _ = generate_random_input((3, 4, 5), dtype=data_type)
+    y_np = 2.3
+
+    x = ms.Tensor(x_np)
+    y = y_np
+    out = pow_forward_func(x, y)
+    expect_out = np.power(x_np, y_np)
+    np.testing.assert_allclose(out.asnumpy(), expect_out, rtol=1e-3)
+
+
+@arg_mark(plat_marks=['platform_ascend', 'platform_ascend910b'], level_mark='level0', card_mark='onecard',
+          essential_mark='essential')
+@pytest.mark.parametrize("context_mode", [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
+@pytest.mark.parametrize("data_type", [np.float32])
+@test_utils.run_test_with_On
+def test_pow_op_backward(context_mode, data_type):
+    """
+    Feature: Auto grad.
+    Description: test auto grad of op pow.
+    Expectation: expect correct result.
+    """
+    ms.context.set_context(mode=context_mode)
+    if context_mode == ms.GRAPH_MODE:
+        ms.set_context(jit_config={'jit_level': 'O0'})
+    x_np, _ = generate_random_input((2, 3, 4, 5), dtype=data_type)
+    y_np = 2.3
+
+    x = ms.Tensor(x_np)
+    y = y_np
+    grads = pow_backward_func(x, y)
+    expect_out = np.power(x_np, y_np - 1) * y_np
+    np.testing.assert_allclose(grads.asnumpy(), expect_out, rtol=1e-3)
+
+    x_np, _ = generate_random_input((2, 3, 4, 5), dtype=data_type)
+    y_np = 0
+
+    x = ms.Tensor(x_np)
+    y = y_np
+    grads = pow_backward_func(x, y)
+    expect_out = np.zeros_like(x_np)
+    np.testing.assert_allclose(grads.asnumpy(), expect_out, rtol=1e-3)
+
+
+@arg_mark(plat_marks=['platform_ascend', 'platform_ascend910b'], level_mark='level1', card_mark='onecard',
+          essential_mark='unessential')
+def test_pow_dynamic_shape():
+    """
+    Feature: Test pow with dynamic shape in graph mode.
+    Description: call ops.pow with valid input and index.
+    Expectation: return the correct value.
+    """
+    x1, _ = generate_random_input((2, 3, 4), np.float32)
+    x2, _ = generate_random_input((3, 4, 5, 6), np.float32)
+
+    TEST_OP(pow_forward_func, [[ms.Tensor(x1), 2.3], [ms.Tensor(x2), 0]], 'pow_scalar_tensor',
+            disable_input_check=True, disable_mode=['GRAPH_MODE'], disable_resize=True)
