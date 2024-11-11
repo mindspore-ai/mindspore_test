0716f21457a1fe52bf5437f655204fab7990ce74
Mrtutu <zhangwei983@huawei.com>
2024-11-11 20:56:50 +0800

    refact

--- mindspore/python/mindspore/profiler/platform/cpu_profiler.py
+++ mindspore/python/mindspore/profiler/platform/cpu_profiler.py
@@ -13,10 +13,11 @@
 # limitations under the License.
 # ============================================================================
 """CPU platform profiler."""
-import mindspore._c_expression as c_expression
 from mindspore import log as logger
 from mindspore.profiler.common.registry import PROFILERS
 from mindspore.profiler.common.constant import DeviceTarget, ProfilerActivity
+import mindspore._c_expression as c_expression
+
 from mindspore.profiler.common.profiler_context import ProfilerContext
 from mindspore.profiler.platform.base_profiler import BaseProfiler
 
@@ -36,8 +37,8 @@ class CpuProfiler(BaseProfiler):
         """Start profiling."""
         logger.info("CpuProfiler start.")
         self._profiler.init(self._prof_ctx.framework_path)
+        logger.info("CpuProfiler framework_path: %s", self._prof_ctx.framework_path)
         self._profiler.step_profiling_enable(True)
-        logger.info("CpuProfiler init framework_path: %s", self._prof_ctx.framework_path)
 
         if ProfilerActivity.CPU in self._prof_ctx.activities:
             self._profiler.enable_op_time()
--- mindspore/python/mindspore/profiler/platform/npu_profiler.py
+++ mindspore/python/mindspore/profiler/platform/npu_profiler.py
@@ -1,4 +1,4 @@
-# Copyright 2020-2024 Huawei Technologies Co., Ltd
+# Copyright 2024 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,45 +13,231 @@
 # limitations under the License.
 # ============================================================================
 """NPU platform profiler."""
+import os
+import re
+import glob
+import json
+
+from mindspore import context
 from mindspore import log as logger
+import mindspore._c_dataengine as cde
+import mindspore._c_expression as c_expression
 from mindspore.profiler.common.registry import PROFILERS
-from mindspore.profiler.common.constant import DeviceTarget
-from mindspore.profiler.platform_profiler.profiler_interface import ProfilerInterface
+from mindspore.profiler.common.constant import DeviceTarget, ProfilerActivity
+
+from mindspore._c_expression import _framework_profiler_enable_mi
+from mindspore.profiler.common.profiler_context import ProfilerContext
+from mindspore.profiler.platform.base_profiler import BaseProfiler
+from mindspore.profiler.common.profiler_path_manager import ProfilerPathManager
+from mindspore.profiler.analysis.task_manager import TaskManager
+from mindspore.profiler.common.profiler_info import ProfilerInfo
+from mindspore.profiler.analysis.parser.ascend_cann_parser import AscendMsprofParser
+from mindspore.profiler.analysis.parser.ms_framework_parser import FrameworkParser
+from mindspore.profiler.analysis.viewer.ms_dataset_viewer import MsDatasetViewer
+from mindspore.profiler.analysis.time_converter import TimeConverter
+from mindspore.profiler.analysis.parser.framework_cann_relation_parser import FrameworkCannRelationParser
+from mindspore.profiler.analysis.viewer.ascend_timeline_viewer import AscendTimelineViewer
+from mindspore.profiler.analysis.viewer.ascend_kernel_details_viewer import AscendKernelDetailsViewer
+from mindspore.profiler.analysis.viewer.ascend_step_trace_time_viewer import AscendStepTraceTimeViewer
+from mindspore.profiler.analysis.parser.ms_minddata_parser import MindDataParser
+from mindspore.profiler.analysis.viewer.ms_minddata_viewer import (
+    MindDataPipelineRawViewer,
+    MindDataPiplineSummaryViewer,
+)
 
 
-@PROFILERS.register_module(DeviceTarget.ASCEND.value)
-class NpuProfiler(ProfilerInterface):
+@PROFILERS.register_module(DeviceTarget.NPU.value)
+class NpuProfiler(BaseProfiler):
     """
     NPU platform profiler
     """
 
-    def __init__(
-            self,
-            output_path: str = "./data",
-            op_time: bool = True,
-            profile_communication: bool = False,
-            profile_memory: bool = False,
-            parallel_strategy: bool = False,
-            start_profile: bool = True,
-            aicore_metrics: int = 0,
-            l2_cache: bool = False,
-            hbm_ddr: bool = False,
-            pcie: bool = False,
-            data_process: bool = False,
-            timeline_limit: int = 500,
-            profile_framework: str = None,
-            **kwargs
-    ) -> None:
+    def __init__(self) -> None:
         super().__init__()
+        self._prof_ctx = ProfilerContext()
+        self._prof_info = ProfilerInfo()
+        self._prof_path_mgr = ProfilerPathManager()
+
+        self._profiler = None
+        if ProfilerActivity.NPU in self._prof_ctx.activities:
+            self._profiler = c_expression.Profiler.get_instance(
+                DeviceTarget.NPU.value
+            )
+            # initialize profiler backend
+            self._profiler.init(
+                self._prof_ctx.ascend_ms_dir,
+                int(self._prof_ctx.device_id),
+                json.dumps(self._prof_ctx.npu_profiler_params),
+            )
+
+        # record original profiler params
+        self._prof_info.profiler_parameters = self._prof_ctx.original_params
+        self._prof_info.ms_profiler_info = {
+            "context_mode": context.get_context("mode"),
+            "rank_id": self._prof_ctx.rank_id,
+        }
+
+        # initialize minddata profiler
+        if self._prof_ctx.data_process:
+            self._md_profiler = cde.GlobalContext.profiling_manager()
+            self._md_profiler.init()
+            logger.info("NpuProfiler init minddata profiler")
+
+        self._prof_mgr = c_expression.ProfilerManager.get_instance()
 
     def start(self) -> None:
         """Start profiling."""
         logger.info("NpuProfiler start.")
+        self._prof_path_mgr.create_profiler_paths()
+
+        if ProfilerActivity.CPU in self._prof_ctx.activities:
+            _framework_profiler_enable_mi()
+            self._prof_mgr.set_profile_framework("time")
+            logger.info("NpuProfiler start enable framework")
+
+        if self._profiler:
+            self._profiler.start()
+
+        if self._prof_ctx.data_process:
+            self._md_profiler.start()
 
     def stop(self) -> None:
         """Stop profiling."""
         logger.info("NpuProfiler stop.")
+        if self._profiler:
+            self._profiler.stop()
 
-    def analyse(self, pretty=False, step_list=None, mode="sync", rank_id=None) -> None:
+        if ProfilerActivity.CPU in self._prof_ctx.activities:
+            self._prof_mgr.set_profile_framework("NULL")
+            logger.info("NpuProfiler stop disable framework")
+
+        if self._prof_ctx.data_process:
+            self._md_profiler.stop()
+            self._md_profiler.save(self._prof_ctx.framework_path)
+
+        self._prof_info.save(self._prof_ctx.ascend_ms_dir, self._prof_ctx.rank_id)
+
+    def analyse(self, **kwargs) -> None:
         """Analyse the profiling data."""
         logger.info("NpuProfiler analyse.")
+
+        NPUProfilerAnalysis.online_analyse()
+        ProfilerPathManager.reset()
+
+    def finalize(self) -> None:
+        """Finalize profiling data."""
+        logger.info("NpuProfiler finalize.")
+        if self._profiler:
+            self._profiler.finalize()
+
+
+class NPUProfilerAnalysis:
+    """
+    NPU profiler analysis interface
+    """
+    @classmethod
+    def online_analyse(cls):
+        """
+        Online analysis for NPU
+        """
+        cls._pre_analyse_online()
+        cls._run_tasks(**ProfilerContext().to_dict())
+
+    @classmethod
+    def _pre_analyse_online(cls):
+        """
+        Pre-process for online analysis
+        """
+        prof_ctx = ProfilerContext()
+        prof_dir = glob.glob(os.path.join(prof_ctx.ascend_ms_dir, "PROF_*"))
+        if not prof_dir:
+            logger.error(f"No PROF_* directory found in {prof_ctx.ascend_ms_dir}")
+            return
+
+        prof_ctx.msprof_profile_path = prof_dir[0]
+        ProfilerPathManager().clean_analysis_cache()
+        ProfilerPathManager().create_output_path()
+        ProfilerInfo().load_time_parameters(
+            prof_ctx.msprof_profile_path,
+            prof_ctx.msprof_profile_host_path
+        )
+        TimeConverter.init_parameters(**ProfilerInfo().time_parameters)
+
+    @classmethod
+    def _run_tasks(cls, **kwargs) -> None:
+        """
+        Run tasks for online analysis
+        """
+        task_mgr = TaskManager()
+        task_mgr.create_flow(
+            AscendMsprofParser(**kwargs),
+            FrameworkParser(**kwargs).register_post_hook(
+                MsDatasetViewer(**kwargs).save
+            ),
+            FrameworkCannRelationParser()
+            .register_post_hook(AscendTimelineViewer(**kwargs).save)
+            .register_post_hook(AscendKernelDetailsViewer(**kwargs).save)
+            .register_post_hook(AscendStepTraceTimeViewer(**kwargs).save),
+            flow_name="cann_flow",
+            show_process=True,
+        )
+
+        enable_data_process = kwargs.get("data_process", False)
+        if enable_data_process:
+            task_mgr.create_flow(
+                MindDataParser(**kwargs)
+                .register_post_hook(MindDataPipelineRawViewer(**kwargs).save)
+                .register_post_hook(MindDataPiplineSummaryViewer(**kwargs).save),
+                flow_name="minddata_flow",
+                show_process=False,
+            )
+
+        task_mgr.run()
+
+    @staticmethod
+    def get_rank_id(ascend_ms_dir: str) -> str:
+        """
+        Function Description:
+            Get rank id from profiler_info_*.json
+        Parameter:
+            ascend_ms_dir: the directory path of profiler data, eg: xxx_ascend_ms
+        Return:
+            str type rank id
+        """
+        prof_info_path = os.path.join(ascend_ms_dir, "profiler_info_*.json")
+        prof_info_path = glob.glob(prof_info_path)
+        if not prof_info_path:
+            raise ValueError(f"Cannot find profiler_info.json in the {ascend_ms_dir}")
+
+        pattern = r"profiler_info_(\d+)\.json$"
+        match = re.search(pattern, prof_info_path[0])
+        rank_id = match.group(1) if match else None
+
+        if rank_id is None:
+            raise ValueError(f"Cannot find rank id in {prof_info_path[0]}")
+
+        return rank_id
+
+    @staticmethod
+    def get_device_id(prof_dir: str) -> str:
+        """
+        Function Description:
+            Get device id from PROF_XXX dir
+        Parameter:
+            prof_dir: the directory path of PROF_XXX
+        Return:
+            str type device id
+        """
+        device_dir = os.path.join(prof_dir, "device_*")
+        device_dir = glob.glob(device_dir)
+        if not device_dir:
+            raise ValueError(f"Cannot find device_* directory in the {prof_dir}")
+
+        pattern = r"device_(\d+)$"
+        match = re.search(pattern, device_dir[0])
+        device_id = match.group(1) if match else None
+
+        if device_id is None:
+            raise ValueError(f"Cannot find device id in {device_dir[0]}")
+
+        return device_id
