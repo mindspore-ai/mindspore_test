38a1c720dff47ee202da42d76c022e42a4071238
zhengzuohe <zhengzuohe@huawei.com>
2024-11-22 16:49:41 +0800

    fix PagedAttention Resize cost time, ms_kernels_internal: [commit] ee39400b,[branch] r2.4.0-bugfix

--- mindspore/ccsrc/plugin/device/ascend/kernel/internal/acme_kernel_mod.h
+++ mindspore/ccsrc/plugin/device/ascend/kernel/internal/acme_kernel_mod.h
@@ -55,6 +55,11 @@ class AcmeKernelMod : public KernelMod {
 
  protected:
   virtual bool IsNeedRecreate(const std::vector<KernelTensor *> &inputs, const std::vector<KernelTensor *> &outputs);
+  virtual void CreateOpParam(const std::vector<KernelTensor *> &inputs, const std::vector<KernelTensor *> &outputs) {
+    return;
+  }
+  virtual bool IsParamChanged() { return false; }
+  virtual void *GetParam() { return nullptr; }
   virtual acme::AcmeOpPtr CreateKernel(const acme::InputsImmutableInfoList &inputs,
                                        const acme::OutputsImmutableInfoList &outputs,
                                        const std::vector<KernelTensor *> &ms_inputs,
