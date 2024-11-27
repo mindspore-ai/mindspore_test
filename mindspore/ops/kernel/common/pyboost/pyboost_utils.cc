239bfe728dacd072999987043228d99f670c8537
wutengda <wutengda2@huawei.com>
2024-11-27 08:56:20 +0800

    op: Add CPU/GPU support for pow_tensor_scalar and pow_scalar_tensor
    Add CPU/GPU support for pow_tensor_scalar and pow_scalar_tensor by using
    expander fallback in graph mode and implementing pyboost customize in
    pynative mode. Both of them are routing to the pow operation.

--- mindspore/ops/kernel/common/pyboost/pyboost_utils.cc
+++ mindspore/ops/kernel/common/pyboost/pyboost_utils.cc
@@ -95,32 +95,39 @@ tensor::BaseTensorPtr PyBoostUtils::ScalarToTensor(const ScalarPtr &scalar) {
   if (scalar == nullptr) {
     MS_EXCEPTION(ArgumentError) << "Nullptr Error!";
   }
+  return ScalarToTensor(scalar, scalar->type());
+}
+
+tensor::BaseTensorPtr PyBoostUtils::ScalarToTensor(const ScalarPtr &scalar, const TypePtr &tensor_dtype) {
+  MS_EXCEPTION_IF_NULL(scalar);
+  MS_EXCEPTION_IF_NULL(tensor_dtype);
+
   TypePtr data_type = scalar->type();
   MS_EXCEPTION_IF_NULL(data_type);
   TypeId type_id = data_type->type_id();
   switch (type_id) {
     case kNumberTypeBool:
-      return std::make_shared<tensor::BaseTensor>(GetValue<bool>(scalar), data_type);
+      return std::make_shared<tensor::BaseTensor>(GetValue<bool>(scalar), tensor_dtype);
     case kNumberTypeInt8:
-      return std::make_shared<tensor::BaseTensor>(static_cast<int64_t>(GetValue<int8_t>(scalar)), data_type);
+      return std::make_shared<tensor::BaseTensor>(static_cast<int64_t>(GetValue<int8_t>(scalar)), tensor_dtype);
     case kNumberTypeInt16:
-      return std::make_shared<tensor::BaseTensor>(static_cast<int64_t>(GetValue<int16_t>(scalar)), data_type);
+      return std::make_shared<tensor::BaseTensor>(static_cast<int64_t>(GetValue<int16_t>(scalar)), tensor_dtype);
     case kNumberTypeInt32:
-      return std::make_shared<tensor::BaseTensor>(static_cast<int64_t>(GetValue<int32_t>(scalar)), data_type);
+      return std::make_shared<tensor::BaseTensor>(static_cast<int64_t>(GetValue<int32_t>(scalar)), tensor_dtype);
     case kNumberTypeInt64:
-      return std::make_shared<tensor::BaseTensor>(GetValue<int64_t>(scalar), data_type);
+      return std::make_shared<tensor::BaseTensor>(GetValue<int64_t>(scalar), tensor_dtype);
     case kNumberTypeUInt8:
-      return std::make_shared<tensor::BaseTensor>(static_cast<uint64_t>(GetValue<uint8_t>(scalar)), data_type);
+      return std::make_shared<tensor::BaseTensor>(static_cast<uint64_t>(GetValue<uint8_t>(scalar)), tensor_dtype);
     case kNumberTypeUInt16:
-      return std::make_shared<tensor::BaseTensor>(static_cast<uint64_t>(GetValue<uint16_t>(scalar)), data_type);
+      return std::make_shared<tensor::BaseTensor>(static_cast<uint64_t>(GetValue<uint16_t>(scalar)), tensor_dtype);
     case kNumberTypeUInt32:
-      return std::make_shared<tensor::BaseTensor>(static_cast<uint64_t>(GetValue<uint32_t>(scalar)), data_type);
+      return std::make_shared<tensor::BaseTensor>(static_cast<uint64_t>(GetValue<uint32_t>(scalar)), tensor_dtype);
     case kNumberTypeUInt64:
-      return std::make_shared<tensor::BaseTensor>(GetValue<uint64_t>(scalar), data_type);
+      return std::make_shared<tensor::BaseTensor>(GetValue<uint64_t>(scalar), tensor_dtype);
     case kNumberTypeFloat32:
-      return std::make_shared<tensor::BaseTensor>(GetValue<float>(scalar), data_type);
+      return std::make_shared<tensor::BaseTensor>(GetValue<float>(scalar), tensor_dtype);
     case kNumberTypeFloat64:
-      return std::make_shared<tensor::BaseTensor>(GetValue<double>(scalar), data_type);
+      return std::make_shared<tensor::BaseTensor>(GetValue<double>(scalar), tensor_dtype);
     default:
       MS_LOG(EXCEPTION) << "When convert scalar to tensor, the scalar type: " << data_type << " is invalid.";
   }
---
+++ mindspore/ops/kernel/cpu/pyboost/customize/pow_tensor_scalar.cc
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "kernel/cpu/pyboost/customize/pow_tensor_scalar.h"
+#include "kernel/cpu/pyboost/auto_generate/pow.h"
+
+namespace mindspore {
+namespace kernel {
+namespace pyboost {
+void PowTensorScalarCPUCustomize(const std::shared_ptr<OpRunner> &op, const BaseTensorPtr &input_tensor,
+                                 const ScalarPtr &exponent) {
+  MS_LOG(DEBUG) << "PowTensorScalar Call start";
+  OpRunner::InferOpOutput(op, input_tensor, exponent);
+
+  const auto device_context = op->device_context();
+  const auto &device_name = device_context->device_context_key_.device_name_;
+
+  // the PowTensorScalar primitive does not support CPU, so use Pow instead.
+  const auto pow_op = CREATE_PYBOOST_OP(Pow, device_name);
+
+  // handle type promotion manually since the CPU kernelmod Pow does not support it
+  const auto out_dtype = op->output(0)->Dtype();
+  auto input_tensor_cast = input_tensor;
+  if (input_tensor->Dtype()->type_id() != out_dtype->type_id()) {
+    input_tensor_cast = PyBoostUtils::CastTensor(input_tensor, out_dtype->type_id(), device_name);
+  }
+  const auto exponent_tensor = PyBoostUtils::ScalarToTensor(exponent, out_dtype);
+
+  const auto pow_out = pow_op->Call(input_tensor_cast, exponent_tensor);
+  op->set_outputs({pow_out});
+
+  MS_LOG(DEBUG) << "PowTensorScalar Call end";
+}
+}  // namespace pyboost
+}  // namespace kernel
+}  // namespace mindspore
