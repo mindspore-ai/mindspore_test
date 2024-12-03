e5ff0b2de5d1bdcf09c4bd68465f0ebd04ec2d3f
baishanyang <yangbaifeng1@huawei.com>
2024-12-03 20:38:09 +0800

    add floor_ grad and kbk
    Signed-off-by: baishanyang <yangbaifeng1@huawei.com>

--- mindspore/ops/grad/grad_math_ops.cc
+++ mindspore/ops/grad/grad_math_ops.cc
@@ -2303,7 +2303,7 @@ REG_BPROP_BUILDER("Floor").SetUnusedInputs({i0, i1, i2}).SetBody(ReturnZeros);
 REG_BPROP_BUILDER("InplaceFloor").SetUnusedInputs({i0, i1, i2}).SetBody(BODYFUNC(ib) {
   auto x = ib->GetInput(kIndex0);
   auto dout = ib->GetInput(kIndex2);
-  auto zeros = ib->Emit("ZerosLikeExt", {dout, ib->Value(static_cast<int64_t>(ib->GetDtypeId(x)))});
+  auto zeros = ib->ZerosLikeExt(dout, ib->Value(static_cast<int64_t>(ib->GetDtypeId(x))));
   return {zeros};
 });
 
