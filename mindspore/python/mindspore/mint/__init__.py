7418780bd2e4b37bf435aca1ba72dceaa8245697
PingqiLi <lipingqi2018@gmail.com>
2024-12-04 18:07:15 +0800

    Enable mint.repeat_interleave overload

--- mindspore/python/mindspore/mint/__init__.py
+++ mindspore/python/mindspore/mint/__init__.py
@@ -40,7 +40,7 @@ from mindspore.ops.auto_generate import abs
 from mindspore.ops.auto_generate import clone
 from mindspore.ops.function.array_func import full_like_ext as full_like
 # 1
-from mindspore.ops.function.math_func import divide, div
+from mindspore.mint.functional_overload import divide, div
 from mindspore.ops.auto_generate import topk_ext as topk
 from mindspore.ops.function.math_func import roll
 # 2
@@ -74,7 +74,7 @@ from mindspore.ops.function.math_func import ne
 # 11
 
 # 12
-from mindspore.ops.function.array_func import repeat_interleave_ext as repeat_interleave
+from mindspore.mint.functional_overload import repeat_interleave
 # 13
 from mindspore.ops.functional import flip
 # 14
