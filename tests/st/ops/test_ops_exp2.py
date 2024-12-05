1fbff797c6e6de6a5a71e87aa4a55c699d45c3c8
Zhang QR <zhangqirui4@huawei.com>
2024-12-05 14:24:09 +0800

    add Tensor.normal_() kbk / fix testcases bug

--- tests/st/ops/test_ops_exp2.py
+++ tests/st/ops/test_ops_exp2.py
@@ -116,12 +116,14 @@ def test_ops_exp2_dtype(context_mode):
     card_mark="onecard",
     essential_mark="unessential",
 )
-def test_exp2_dynamic_shape():
+@pytest.mark.parametrize("mode", [ms.PYNATIVE_MODE])
+def test_exp2_dynamic_shape(mode):
     """
     Feature: Test dynamic shape.
     Description: test function exp2 dynamic feature.
     Expectation: expect correct result.
     """
+    ms.context.set_context(mode=mode)
     ms_data1 = generate_random_input((2, 3, 4, 5), np.float32)
     ms_data2 = generate_random_input((3, 4, 5, 6, 7), np.float32)
     TEST_OP(exp2_forward_func, [[ms.Tensor(ms_data1)], [ms.Tensor(ms_data2)]], "exp2")
