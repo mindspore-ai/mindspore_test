cfe16ea75c15b9397f1a3c818c90a73402f973e2
wtobill <tongwu@huawei.com>
2024-12-09 10:53:28 +0800

    tensor refactor __eq__ api

--- mindspore/python/mindspore/ops/tensor_method.py
+++ mindspore/python/mindspore/ops/tensor_method.py
@@ -538,6 +538,12 @@ def tensor_eq(input, other):
     return eq(input, other)
 
 
+def deprecated_tensor_eq(input, other):
+    if not isinstance(other, (int, float, Tensor)):
+        return False
+    return eq(input, other)
+
+
 # 30 erf
 def tensor_erf(input):
     return erf(input)
