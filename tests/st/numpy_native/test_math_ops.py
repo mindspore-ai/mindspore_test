36e459b495c9ce923cde1db2fcce56c2d6f7ceed
wtobill <tongwu@huawei.com>
2024-12-10 11:15:55 +0800

    fix cumsum use case after tensor cumsum api refactor

--- tests/st/numpy_native/test_math_ops.py
+++ tests/st/numpy_native/test_math_ops.py
@@ -1172,7 +1172,7 @@ def test_negative():
     match_array(mnp_neg.asnumpy(), onp_neg, 1e-5)
 
 
-@arg_mark(plat_marks=['platform_ascend', 'platform_gpu', 'cpu_linux', 'cpu_windows', 'cpu_macos'], level_mark='level1',
+@arg_mark(plat_marks=['platform_ascend', 'platform_gpu', 'cpu_linux', 'cpu_windows', 'cpu_macos'], level_mark='level0',
           card_mark='onecard', essential_mark='unessential')
 def test_cumsum():
     x = mnp.ones((16, 16), dtype="bool")
@@ -1182,7 +1182,7 @@ def test_cumsum():
     match_meta(mnp.cumsum(x).asnumpy(), onp.cumsum(x.asnumpy()))
 
     x = rand_int(3, 4, 5)
-    match_array(mnp.cumsum(to_tensor(x), dtype="bool").asnumpy(),
+    match_array(mnp.cumsum(to_tensor(x), dtype=ms.bool_).asnumpy(),
                 onp.cumsum(x, dtype="bool"))
     match_array(mnp.cumsum(to_tensor(x), axis=-1).asnumpy(),
                 onp.cumsum(x, axis=-1))
