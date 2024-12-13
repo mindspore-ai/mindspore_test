bcaf7bdd56218e8cc29e95ef5ac7d04e35b37258
caifubi <caifubi1@huawei.com>
2024-12-13 15:36:49 +0800

    Set tuple output flag for auto grad.

--- mindspore/ops/kernel/ascend/pyboost/customize/inner_comm_isend.cc
+++ mindspore/ops/kernel/ascend/pyboost/customize/inner_comm_isend.cc
@@ -32,7 +32,7 @@ void InnerCommIsendAscendCustomize(const std::shared_ptr<OpRunner> &op, const Ba
   PyBoostUtils::PrepareOpInputs(op->device_context(), kDefaultStreamIndex, input_tensor);
 
   op->set_outputs({input_tensor});
-  op->CreateOutputSimpleInfoForView();
+  op->CreateOutputSimpleInfo();
 
   auto run_func = [op, input_tensor, dst, group]() {
     auto device_context = op->device_context();
