3aa01696b1504292fd85fa6ef1b2aaccc3c0ee21
zhangminli <zhangminli8@huawei.com>
2024-12-13 11:41:51 +0800

    llm_boost adapt to latest atb

--- mindspore/python/mindspore/experimental/llm_boost/atb/llama_boost.py
+++ mindspore/python/mindspore/experimental/llm_boost/atb/llama_boost.py
@@ -23,7 +23,7 @@ from mindspore.experimental.llm_boost.atb.boost_base import (
 from mindspore._c_expression import LlmBoostBinder
 from mindspore.experimental.llm_boost.register import LlmBoostRegister, LlmBoostType
 
-CPP_LLAMA_MODEL_CLASS_NAME = "llama_parallel_LlamaDecoderModel"
+CPP_LLAMA_MODEL_CLASS_NAME = "llama_LlamaDecoderModel"
 
 
 @LlmBoostRegister.register(LlmBoostType.BUILDIN, "Llama")
@@ -99,7 +99,6 @@ class LlamaBoost(AtbBoostBase):
         self.atb_decoder_operation.init(json.dumps({**decoder_param}))
         return True
 
-    # pylint: disable=C0330
     def _prepare_inputs(
             self,
             prefill=None,
