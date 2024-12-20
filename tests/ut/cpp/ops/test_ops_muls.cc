536ee2397bfe177b3a33bf8fa24b4ce3c5fe1968
wangyuqiao1 <wangyuqiao1@huawei.com>
2024-12-20 14:21:10 +0800

    fix doc

--- tests/ut/cpp/ops/test_ops_muls.cc
+++ tests/ut/cpp/ops/test_ops_muls.cc
@@ -13,28 +13,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <memory>
-#include "common/common_test.h"
-#include "infer/ops_func_impl/muls.h"
-#include "ops/test_ops.h"
-#include "ops/test_ops_cmp_utils.h"
-#include "ops/test_value_utils.h"
 
-namespace mindspore {
-namespace ops {
-OP_FUNC_IMPL_INFER_TEST_DECLARE(Muls, MultiInputOpParams);
+#include "ops/utils/general_infer_utils.h"
 
-OP_FUNC_IMPL_INFER_TEST_CASES(
-  Muls,
-  testing::Values(MultiInputOpParams{{{1}}, {kFloat16}, {{1}}, {kFloat16}, {}},
-                  MultiInputOpParams{{{2, 3}}, {kFloat16}, {{2, 3}}, {kFloat16}, {}},
-                  MultiInputOpParams{{{2, 3}}, {kFloat32}, {{2, 3}}, {kFloat32}, {}},
-                  MultiInputOpParams{{{2, 3, 4}}, {kFloat64}, {{2, 3, 4}}, {kFloat64}, {}},
-                  MultiInputOpParams{{{2, 3}}, {kInt8}, {{2, 3}}, {kInt8}, {}},
-                  MultiInputOpParams{{{2, 3}}, {kInt16}, {{2, 3}}, {kInt16}, {}},
-                  MultiInputOpParams{{{2, 3}}, {kInt32}, {{2, 3}}, {kInt32}, {}},
-                  MultiInputOpParams{{{2, 3}}, {kInt64}, {{2, 3}}, {kInt64}, {}},
-                  MultiInputOpParams{{{2, 3}}, {kUInt8}, {{2, 3}}, {kUInt8}, {}},
-                  MultiInputOpParams{{{-1, -1}}, {kBool}, {{-1, -1}}, {kBool}, {}}));
-}  // namespace ops
-}  // namespace mindspore
+namespace mindspore::ops {
+namespace {
+std::vector<GeneralInferParam> prepare_params() {
+  GeneralInferParamGenerator generator;
+  generator
+    .FeedInputArgs({InferInfoParam{ShapeVector{2, 3}, kNumberTypeFloat32},
+                    InferInfoParam{ShapeVector{}, kNumberTypeFloat32, CreateScalar<float>(1.)}})
+    .FeedExpectedOutput({{2, 3}}, {kNumberTypeFloat32});
+  generator
+    .FeedInputArgs({InferInfoParam{ShapeVector{2, -1}, kNumberTypeFloat16},
+                    InferInfoParam{ShapeVector{}, kNumberTypeFloat32, CreateScalar<float>(1.)}})
+    .FeedExpectedOutput({{2, -1}}, {kNumberTypeFloat16});
+  generator
+    .FeedInputArgs({InferInfoParam{ShapeVector{-1, -1}, kNumberTypeInt32},
+                    InferInfoParam{ShapeVector{}, kNumberTypeFloat32, CreateScalar<float>(1.)}})
+    .FeedExpectedOutput({{-1, -1}}, {kNumberTypeFloat32});
+  generator
+    .FeedInputArgs({InferInfoParam{ShapeVector{-2}, kNumberTypeComplex128},
+                    InferInfoParam{ShapeVector{}, kNumberTypeFloat32, CreateScalar<float>(1.)}})
+    .FeedExpectedOutput({{-2}}, {kNumberTypeComplex128});
+  return generator.Generate();
+}
+}  // namespace
+
+INSTANTIATE_TEST_CASE_P(Muls, GeneralInferTest, testing::ValuesIn(prepare_params()));
+}  // namespace mindspore::ops
