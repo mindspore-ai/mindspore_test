98dcb92398e5b6876901eca141dded2ee82a1d57
qiuleilei <qiuleilei1@huawei.com>
2024-12-28 09:25:23 +0800

    修复当conv算子对接为acl::DepthwiseConv2dNative算子且group=128时导致的精度问题

--- mindspore/lite/tools/converter/adapter/acl/mapper/conv2d_fusion_mapper.cc
+++ mindspore/lite/tools/converter/adapter/acl/mapper/conv2d_fusion_mapper.cc
@@ -32,16 +32,6 @@ STATUS Conv2DFusionMapper::Mapper(const CNodePtr &cnode) {
   }
   ops::Conv2D conv2d_op;
   PrimitivePtr dst_prim = conv2d_op.GetPrim();
-#ifndef SUPPORT_SD3403_DAVINCI
-  bool is_depth_wise = false;
-  auto depth_wise_ptr = src_prim->GetAttr(ops::kIsDepthWise);
-  if (depth_wise_ptr != nullptr) {
-    is_depth_wise = GetValue<bool>(depth_wise_ptr);
-  }
-  if (is_depth_wise) {
-    dst_prim = std::make_shared<acl::DepthwiseConv2dNative>();
-  }
-#endif
   CHECK_NULL_RETURN(dst_prim);
   dst_prim->SetAttrs(src_prim->attrs());
   auto status = AttrAdjust(dst_prim, ops::kStride);
