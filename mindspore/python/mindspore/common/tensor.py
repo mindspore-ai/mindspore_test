9700e6a46487a529f98f899b5d5d4af9dfeb067a
baishanyang <yangbaifeng1@huawei.com>
2025-01-09 14:09:33 +0800

    fix Tensor.clamp_ doc bug
    Signed-off-by: baishanyang <yangbaifeng1@huawei.com>

--- mindspore/python/mindspore/common/tensor.py
+++ mindspore/python/mindspore/common/tensor.py
@@ -2274,7 +2274,7 @@ class Tensor(Tensor_, metaclass=_TensorMeta):
         r"""
         Clamps tensor values between the specified minimum value and maximum value.
 
-        Limits the value of :math:`input` to a range, whose lower limit is `min` and upper limit is `max` .
+        Limits the value of :math:`self` to a range, whose lower limit is `min` and upper limit is `max` .
 
         .. warning::
 
@@ -2284,9 +2284,9 @@ class Tensor(Tensor_, metaclass=_TensorMeta):
 
             out_i= \left\{
             \begin{array}{align}
-                max & \text{ if } input_i\ge max \\
-                input_i & \text{ if } min \lt input_i \lt max \\
-                min & \text{ if } input_i \le min \\
+                max & \text{ if } self_i\ge max \\
+                self_i & \text{ if } min \lt self_i \lt max \\
+                min & \text{ if } self_i \le min \\
             \end{array}\right.
 
         Note:
@@ -2294,20 +2294,19 @@ class Tensor(Tensor_, metaclass=_TensorMeta):
             - When `min` is None and `max` is not None, the elements in Tensor larger than `max` will become `max`;
             - When `min` is not None and `max` is None, the elements in Tensor smaller than `min` will become `min`;
             - If `min` is greater than `max`, the value of all elements in Tensor will be set to `max`;
-            - The data type of `input`, `min` and `max` should support implicit type conversion and cannot be bool type.
+            - The data type of `self`, `min` and `max` should support implicit type conversion and cannot be bool type.
 
         Args:
-            input (Tensor): Input data, which type is Tensor. Tensors of arbitrary dimensions are supported.
             min (Union(Tensor, float, int), optional): The minimum value. Default: ``None`` .
             max (Union(Tensor, float, int), optional): The maximum value. Default: ``None`` .
 
         Returns:
             Tensor, a clipped Tensor.
-            The data type and shape are the same as input.
+            The data type and shape are the same as self.
 
         Raises:
             ValueError: If both `min` and `max` are None.
-            TypeError: If the type of `input` is not in Tensor.
+            TypeError: If the type of `self` is not in Tensor.
             TypeError: If the type of `min` is not in None, Tensor, float or int.
             TypeError: If the type of `max` is not in None, Tensor, float or int.
 
@@ -2315,9 +2314,8 @@ class Tensor(Tensor_, metaclass=_TensorMeta):
             ``Ascend``
 
         Examples:
-            >>> # case 1: the data type of input is Tensor
             >>> import mindspore
-            >>> from mindspore import Tensor, ops
+            >>> from mindspore import Tensor
             >>> import numpy as np
             >>> min_value = Tensor(5, mindspore.float32)
             >>> max_value = Tensor(20, mindspore.float32)
