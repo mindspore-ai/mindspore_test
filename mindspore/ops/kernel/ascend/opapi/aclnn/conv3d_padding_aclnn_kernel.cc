ea0b3290dcf23c813676c2c3a150882e0dd5ba93
wujueying <wujueying1@huawei.com>
2025-01-18 20:05:19 +0800

    fix conv2d/3d

--- mindspore/ops/kernel/ascend/opapi/aclnn/conv3d_padding_aclnn_kernel.cc
+++ mindspore/ops/kernel/ascend/opapi/aclnn/conv3d_padding_aclnn_kernel.cc
@@ -218,6 +218,8 @@ void Conv3DPaddingAscend::GetWorkSpaceInfo(const std::vector<KernelTensor *> &in
   }
 
   if (!is_batchfy_) {
+    input_sizes.erase(input_sizes.begin());
+    SetTensorStorageInfo<KernelTensor *>(inputs[kIndex0], input_sizes);
     output_sizes.erase(output_sizes.begin());
     SetTensorStorageInfo<KernelTensor *>(outputs[kIndex0], output_sizes);
   }
