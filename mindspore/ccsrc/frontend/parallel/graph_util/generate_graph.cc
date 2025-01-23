ded5067a9b962835a1c93d6d82453e2e0cd07cad
liangcanli <liangcanli@huawei.com>
2025-01-23 20:37:21 +0800

    disable view operator process under ge mode

--- mindspore/ccsrc/frontend/parallel/graph_util/generate_graph.cc
+++ mindspore/ccsrc/frontend/parallel/graph_util/generate_graph.cc
@@ -81,7 +81,8 @@ std::vector<AnfNodePtr> RectifyInputsForNewCNode(const std::vector<AnfNodePtr> &
   auto op_inputs_num = op_def->indexes_.size();
   auto graph_view_prim = op_def->is_graph_view_;
   static const bool close_view_op = (common::GetEnv("MS_DEV_JIT_ENABLE_VIEW_OP") == "0");
-  if (graph_view_prim == true && !close_view_op) {
+  auto ge_mode = MsContext::GetInstance()->GetJitLevel() == kAttrJitLevelO2;
+  if (graph_view_prim == true && !close_view_op && !ge_mode) {
     op_inputs_num = op_inputs_num + 1;  // for umonad input
     auto monad_input = NewValueNode(kUMonad);
     monad_input->set_abstract(kUMonad->ToAbstract());
