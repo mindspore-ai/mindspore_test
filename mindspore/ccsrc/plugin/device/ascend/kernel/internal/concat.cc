6b7fe91ab1c0d0238059d120d5e88294d708fef0
zhengzuohe <zhengzuohe@huawei.com>
2025-02-10 18:07:52 +0800

    concat div equal less

---
+++ mindspore/ccsrc/plugin/device/ascend/kernel/internal/concat.cc
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "plugin/device/ascend/kernel/internal/concat.h"
+
+#include <memory>
+#include "kernel/kernel.h"
+
+namespace mindspore {
+namespace kernel {
+constexpr auto kInputNum = 3;
+
+internal::InternalOpPtr InternalConcat::CreateKernel(const internal::InputsImmutableInfoList &inputs_ii,
+                                                     const internal::OutputsImmutableInfoList &outputs_ii,
+                                                     const std::vector<KernelTensor *> &ms_inputs,
+                                                     const std::vector<KernelTensor *> &ms_outputs) {
+  if (ms_inputs.size() != kInputNum) {
+    MS_LOG(EXCEPTION) << "Internal Op '" << kernel_name_ << "' only supports to concatenate 2 input tensors, but got "
+                      << ms_inputs.size();
+  }
+  internal::ConcatParam param;
+  auto concat_dim_idx = ms_inputs.size() - 1;
+  param.concat_dim = ms_inputs[concat_dim_idx]->GetValueWithCheck<int64_t>();
+  return internal::CreateConcatOp(inputs_ii, outputs_ii, param, internal::kInternalConcatOpName);
+}
+
+MS_INTERNAL_KERNEL_FACTORY_REG(Concat, internal::kInternalConcatOpName, InternalConcat);
+REG_MS_TO_INTERNAL_IN_TENSOR_IDX_MAP(Concat, INPUT_NUM_2, INDEX_0, INDEX_1);
+REG_MS_TO_INTERNAL_OUT_TENSOR_IDX_MAP(Concat, OUTPUT_NUM_1, INDEX_0);
+}  // namespace kernel
+}  // namespace mindspore
