1102d67d8a199c8e0fd3bfdfb40838252f107f4c
limingqi107 <limingqi@huawei.com>
2025-02-13 15:14:07 +0800

    add the pynative adapter for new backend

--- mindspore/ccsrc/backend/ms_backend/ms_backend.h
+++ mindspore/ccsrc/backend/ms_backend/ms_backend.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2025 Huawei Technologies Co., Ltd
+ * Copyright 2019-2024 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,147 +13,112 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef MINDSPORE_CCSRC_BACKEND_MS_BACKEND_MSBACKEND_H_
-#define MINDSPORE_CCSRC_BACKEND_MS_BACKEND_MSBACKEND_H_
+#ifndef MINDSPORE_CCSRC_VM_BACKEND_H_
+#define MINDSPORE_CCSRC_VM_BACKEND_H_
 
-#include <memory>
 #include <list>
+#include <memory>
 #include <string>
 #include <map>
 #include <set>
 #include <utility>
 #include <vector>
-#include <unordered_set>
-#include "utils/hash_map.h"
-#include "ir/anf.h"
-#include "backend/common/session/session_basic.h"
-#include "backend/backend_manager/backend_manager.h"
-#include "runtime/hardware/device_context.h"
-#include "backend/graph_compiler/segment_runner.h"
-#include "runtime/graph_scheduler/actor/actor_set.h"
-#include "include/common/profiler.h"
 
+#include "utils/hash_map.h"
+#include "include/common/utils/contract.h"
+#include "ir/anf.h"
+#include "base/base_ref.h"
+#include "backend/graph_compiler/segment_runner.h"
+#include "backend/graph_compiler/graph_partition.h"
+#include "backend/graph_compiler/vm.h"
+#include "backend/graph_compiler/op_backend.h"
+#include "backend/common/session/session_basic.h"
+#include "runtime/hardware/device_context.h"
+#include "runtime/graph_scheduler/graph_scheduler.h"
+#include "runtime/pipeline/task/device_task.h"
+#include "runtime/pynative/op_compiler.h"
+#include "runtime/pynative/graph_adapter.h"
+#include "include/backend/visible.h"
+#include "backend/ms_backend/ms_backend_base.h"
+#include "runtime/pynative/op_runner.h"
 namespace mindspore {
 namespace backend {
 namespace ms_backend {
-using GraphOutputInfo = session::GraphOutputInfo;
-using DeviceContext = device::DeviceContext;
-using ActorInfo = runtime::ActorInfo;
-using GraphCompiler = runtime::GraphCompiler;
-using GraphCompilerInfo = runtime::GraphCompilerInfo;
-using ControlNodeParser = runtime::ControlNodeParser;
-using FuncGraphToKernelGraphGroup = runtime::FuncGraphToKernelGraphGroup;
-using ControlNodeParserPtr = runtime::ControlNodeParserPtr;
-using KernelWithIndex = session::KernelWithIndex;
-using GraphPartition = compile::GraphPartition;
-using GraphPartitionPtr = compile::GraphPartitionPtr;
-
-// The base class of all supported backend.
-class BACKEND_EXPORT MSBackend : public BackendBase {
+class BACKEND_EXPORT MSBackend : public MSBackendBase {
  public:
-  MSBackend();
-  ~MSBackend() = default;
+  MSBackend() : MSBackendBase() {}
+  ~MSBackend() override;
 
-  // The backend graph Build interface, the return value is the built graph id.
-  BackendGraphId Build(const FuncGraphPtr &func_graph) override;
+  // Execute all tasks in queue when lazy build is enabled in PyNative mode.
+  void WaitTaskFinish() const override;
+  // Clear resource when python exit.
+  void ClearOpExecutorResource() const;
 
-  // The backend graph Run interface by the graph_id which are generated through the graph Build interface above.
-  RunningStatus Run(BackendGraphId graph_id, const VectorRef &inputs, VectorRef *outputs) override;
+  // Sync default stream in PyNative mode.
+  void SyncStream();
 
-  virtual void WaitTaskFinish() const {}
-  virtual void RunGraphByCondition(BackendGraphId graph_id, const GraphCompilerInfo &graph_compiler_info,
-                                   const VectorRef &args, VectorRef *outputs) {}
-#ifdef ENABLE_DEBUGGER
-  void SetDebuggerInit() const;
-#endif
+  KernelGraphPtr GetGraphById(GraphId graph_id);
 
- protected:
-  // Convert the nodes which are not supported in the backend.
-  void UnifyMindIR(const FuncGraphPtr &func_graph) const;
+ private:
+  void RunGraphByCondition(BackendGraphId graph_id, const GraphCompilerInfo &graph_compiler_info, const VectorRef &args,
+                           VectorRef *outputs) override;
+  // Split complete kernel graph to single op graph in PyNative back
+  // propagation, then compile and run single op graph or pyboost op(if op registered).
+  void RunGraphBySingleOp(const GraphCompilerInfo &graph_compiler_info, const VectorRef &args, VectorRef *outputs);
 
-  // The parameter func_graph is a graph, it can be either a root graph or a sub graph,
-  // The result of graph compiler is stored in graph_id_to_device_context_ and control_nodes_.
-  void CompileGraph(const FuncGraphPtr &func_graph);
+  runtime::ActorSet *RealCompileGraphBeforeRunActor(BackendGraphId graph_id,
+                                                    const GraphCompilerInfo &graph_compiler_info, const VectorRef &args,
+                                                    bool no_multi_graph);
+  void RunGraphByActors(BackendGraphId graph_id, const GraphCompilerInfo &graph_compiler_info, const VectorRef &args,
+                        VectorRef *outputs);
 
-  // Compile the kernel graph by the segment which is from the function graph partition.
-  void CompileGraphFromSegment(const GraphSegmentPtr &segment);
+  void RunMsGradGraph(const CNodePtr &kernel, const VectorRef &args, VectorRef *outputs) const;
 
-  // Compile the kernel graph which generated directly from front end(PyNative), and no need do graph partition.
-  void CompileKernelGraph(const KernelGraphPtr &kernel_graph, const std::pair<AnfNodePtrList, AnfNodePtrList> &io_nodes,
-                          DeviceContext *device_context);
+  // Clean the compilation cache to avoid memory leakage in dynamic shape scenarios.
+  void ClearResource();
 
-  void CacheFuncGraphWithKernelGraphId(const FuncGraphPtr &func_graph, const GraphId &graph_id,
-                                       DeviceContext *device_context);
+  void RunActorSet(BackendGraphId graph_id, runtime::ActorSet *actor_set, const GraphCompilerInfo &graph_compiler_info,
+                   const VectorRef &args, bool no_multi_graph, VectorRef *outputs);
 
-  void ConstructOutputs(runtime::ActorSet *actor_set, VectorRef *outputs, const FuncGraphPtr &root_graph);
+  // Cache output tensor ref count of kernels for back propagation graph in PyNative mode.
+  std::map<GraphId, std::map<KernelWithIndex, size_t>> cnode_ref_counts_;
 
-  // Restore the outputs tuple by the origin funcGraph output node and output tensors.
-  void ConstructOutputs(const AnfNodePtr &output_node, const std::vector<tensor::TensorPtr> &output_tensors,
-                        size_t *output_position, VectorRef *outputs, std::vector<tensor::TensorPtr> *tuple_tensors);
-  // Spit the tuple tensor to multi tensors for restoring the tuple output.
-  void ConstructOutputByTupleTensor(tensor::TensorPtr output_tensor, const abstract::SequenceShapePtr &tensor_shape,
-                                    VectorRef *outputs, std::vector<tensor::TensorPtr> *tuple_tensors) const;
-  // In the control flow, the output of the call node needs to be created by abstract.
-  BaseRef ConstructOutputByAbstract(const abstract::AbstractBasePtr &abstract,
-                                    const std::vector<tensor::TensorPtr> &output_tensors, size_t *output_position,
-                                    std::vector<tensor::TensorPtr> *tuple_tensors);
-  // Construct the GraphCompilerInfo by the compilation results of graph, used in Graph mode.
-  std::shared_ptr<GraphCompilerInfo> ConstructGraphCompilerInfo(const FuncGraphPtr &root_graph);
+  // Cache forward op output value node tensor ref count of kernels for back propagation graph in PyNative mode.
+  std::map<std::string, size_t> forward_op_output_tensor_id_;
 
-  void ParseControlNodes(const GraphCompilerInfo &graph_compile_info);
+  mindspore::compile::OpBackend op_backend_;
+  pynative::GraphAdapter graph_adapter_;
 
-  void ContiguousArgs(const VectorRef &args, const GraphCompilerInfo &);
+  bool first_step_{true};
+};
+class BACKEND_EXPORT PyBoostAdapter {
+ public:
+  PyBoostAdapter() = default;
+  ~PyBoostAdapter() = default;
 
-  // Wait multi stream finish.
-  void WaitMultiStream(const GraphCompilerInfo &graph_compiler_info);
+  using IsPyBoostRegisteredFunc = std::function<bool(const std::string &device_target, const std::string &op_name)>;
+  using RunPyBoostCallFunc = std::function<void(runtime::OpRunnerInfo *, VectorRef *)>;
 
-  // Backend compile cache interface, handle the control node and graph id in this class.
-  bool DumpBackendInfo();
-  bool LoadBackendInfo();
+  static bool IsPyBoostRegistered(const std::string &device_target, const std::string &op_name) {
+    MS_EXCEPTION_IF_NULL(is_pyboost_registered_func_);
+    return is_pyboost_registered_func_(device_target, op_name);
+  }
+  static void RunPyBoostCall(runtime::OpRunnerInfo *op_runner_info, VectorRef *op_outputs) {
+    MS_EXCEPTION_IF_NULL(run_pyboost_call_func_);
+    run_pyboost_call_func_(op_runner_info, op_outputs);
+  }
 
-  // Check whether this root_graph can enable single op and graph pipeline or not.
-  bool CheckEnableGraphPipeline(const std::shared_ptr<GraphCompilerInfo> &graph_compiler_info);
+  static void SetIsPyBoostRegistered(const IsPyBoostRegisteredFunc &func) { is_pyboost_registered_func_ = func; }
+  static void SetRunPyBoostCallFunc(const RunPyBoostCallFunc &func) { run_pyboost_call_func_ = func; }
 
-  // Bind a specific core to the main thread.
-  void BindCoreForMainThread();
-
-  void TransformGraphToActorDAG(const GraphCompilerInfo &graph_compiler_info);
-
-  // When compiling FuncGraph, it is divided according to the control nodes, and obtain the control nodes and several
-  // node segments. Node segments will be compiled into kernelGraphs which are expressed as GraphId and bound to
-  // the corresponding device_context.
-  std::map<GraphId, DeviceContext *> graph_id_to_device_context_;
-  // Funcgraph will be cut into multiple kernel graphs, and the map is used to save the correspondence.
-  // The kernel graphs which not cut by control flow are placed in the same group.
-  std::map<FuncGraphPtr, std::vector<std::vector<GraphId>>> func_graph_to_kernel_graph_ids_;
-  std::map<GraphInfo, DeviceContext *> graph_info_to_device_context_;
-  std::vector<AnfNodePtr> control_nodes_;
-
-  mindspore::HashMap<BackendGraphId, std::shared_ptr<GraphCompilerInfo>> actor_to_graph_compiler_info_;
-
-  // Save the mapping between cell id and actor info.
-  FuncGraphPtr root_graph_;
-  AnfNodePtr output_node_;
-  GraphPartitionPtr graph_partition_;
-  std::shared_ptr<GraphCompiler> graph_compiler_;
-  std::string device_name_;
-  uint32_t device_id_;
-  int ms_execution_mode_{kGraphMode};
-  bool has_pre_build_comm_{false};
-  void CompileSubGraph(const FuncGraphPtr &func_graph);
-  void ProcessNotSupportCnode(const FuncGraphPtr &func_graph, const device::DeviceType &old_target,
-                              const device::DeviceType &new_target) const;
-  bool CompileGraphsByKbkCache(const FuncGraphPtr &func_graph, DeviceContext *device_context);
-  bool CacheCompileGraphs();
-
-  // Whether this root_graph can enable single op and graph pipeline or not.
-  bool enable_graph_pipeline_{false};
-
-  static uint32_t backend_graph_id_;
+ private:
+  inline static IsPyBoostRegisteredFunc is_pyboost_registered_func_;
+  inline static RunPyBoostCallFunc run_pyboost_call_func_;
 };
 
-using MSBackendPtr = std::shared_ptr<MSBackend>;
+using MsBackendPtr = std::shared_ptr<MSBackend>;
 }  // namespace ms_backend
 }  // namespace backend
+using BackendOpRunInfoPtr = std::shared_ptr<session::BackendOpRunInfo>;
 }  // namespace mindspore
-#endif  // MINDSPORE_CCSRC_BACKEND_MS_BACKEND_MSBACKEND_H_
+#endif
