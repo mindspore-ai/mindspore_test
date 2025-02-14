e9e720a2f1623af28197516115f5ffc90d9b0876
ckey_Dou <ckey.chengbin@huawei.com>
2025-02-14 22:28:01 +0800

    support rmsnorm_quant branch: master commit_id: 77f659c35c4daee7639ce08720cae6cc14b001c7

--- mindspore/ccsrc/plugin/device/ascend/optimizer/backend_common_unify_mindir.cc
+++ mindspore/ccsrc/plugin/device/ascend/optimizer/backend_common_unify_mindir.cc
@@ -171,12 +171,12 @@ PassManagerPtr GetBackendFusionGroupPassManager() {
   pm->AddFusionPass(std::make_shared<opt::InferenceSwiGLUFusion>(), infer_boost);
   pm->AddFusionPass(std::make_shared<opt::InferenceMatmulSplitFusion>(), infer_boost);
   pm->AddFusionPass(std::make_shared<opt::AddRmsNormDynamicQuantFusion>(), infer_boost);
-  pm->AddFusionPass(std::make_shared<opt::ShapeReshapeFusion>(), infer_boost);
   pm->AddFusionPass(std::make_shared<opt::AddRmsNormQuantFusion>(), infer_boost);
   pm->AddFusionPass(std::make_shared<opt::AddCastRmsNormCastQuantFusion>(), infer_boost);
   pm->AddFusionPass(std::make_shared<opt::RmsNormQuantFusion>(), infer_boost);
   pm->AddFusionPass(std::make_shared<opt::AddRmsNormFusion>(), infer_boost);
   pm->AddFusionPass(std::make_shared<opt::AddCastRmsNormCastFusion>(), infer_boost);
+  pm->AddFusionPass(std::make_shared<opt::ShapeReshapeFusion>(), infer_boost);
   pm->AddFusionPass(std::make_shared<opt::SplitConcatFusion>());
   pm->AddFusionPass(std::make_shared<opt::MatmulElemFusion>(), infer_boost);
   pm->AddFusionPass(std::make_shared<opt::QbmmAllReduceAddFusion>());
