cd139776bfd16068638a94ffea51822d709604cd
ttudu <dusijia@huawei.com>
2025-02-17 19:00:09 +0800

    support lazy_inline and pipeline when disable_graph_op

--- mindspore/ccsrc/backend/graph_compiler/backend_base.cc
+++ mindspore/ccsrc/backend/graph_compiler/backend_base.cc
@@ -1007,7 +1007,54 @@ bool ExportCompileCacheKBK(const FuncGraphPtr &func_graph, const device::DeviceT
   return true;
 }
 
+bool IsCellReuseAndPipeline(const FuncGraphPtr &func_graph) {
+  // cell reuse + pipeline parallel
+  // only O2
+  if (func_graph == nullptr) {
+    return false;
+  }
+  auto context = MsContext::GetInstance();
+  MS_EXCEPTION_IF_NULL(context);
+  auto nodes = TopoSort(func_graph->get_return(), SuccDeeperSimple);
+  bool has_cell_reuse = std::any_of(nodes.begin(), nodes.end(), [](const AnfNodePtr &node) {
+    if (node == nullptr || !node->isa<CNode>()) {
+      return false;
+    }
+    auto cnode = node->cast<CNodePtr>();
+    const auto &inputs = cnode->inputs();
+
+    // for func graph
+    AnfNodePtr fn = inputs[0];
+    FuncGraphPtr child_graph = common::AnfAlgo::GetValueNodeFuncGraph(fn);
+    bool func_graph_has_cell_reuse = child_graph != nullptr && child_graph->has_flag(FUNC_GRAPH_FLAG_CELL_REUSE);
+
+    // for kernel graph
+    bool kernel_graph_has_cell_reuse = false;
+    if (IsPrimitiveCNode(cnode, prim::kPrimCall)) {
+      auto call_graph = cnode->input(kIndex1);
+      auto sub_kernel_graph = session::AnfRuntimeAlgorithm::GetValueNodeKernelGraph(call_graph);
+      kernel_graph_has_cell_reuse = sub_kernel_graph != nullptr && sub_kernel_graph->need_inline();
+    }
+    return func_graph_has_cell_reuse || kernel_graph_has_cell_reuse;
+  });
+
+  auto parallel_context = parallel::ParallelContext::GetInstance();
+  MS_EXCEPTION_IF_NULL(parallel_context);
+  auto stages = parallel_context->pipeline_stage_split_num();
+  auto grad_accu_step = parallel_context->grad_accumulation_step();
+  MS_LOG(INFO) << "graph: " << func_graph->ToString() << "stages: " << stages << ", grad_accu_step: " << grad_accu_step;
+  if (stages <= 1 && grad_accu_step <= 1) {
+    if (has_cell_reuse) {
+      // no pipeline + cell reuse + O2
+      context->SetCellReuseLevel(CellReuseLevel::kNoInline);
+    }
+    return false;
+  }
+  return has_cell_reuse;
+}
+
 void CheckRunMode(device::RunMode run_mode, const FuncGraphPtr &graph) {
+  MS_EXCEPTION_IF_NULL(graph);
   auto context = MsContext::GetInstance();
   MS_EXCEPTION_IF_NULL(context);
   // for some graph is hybrid, and some graph is graph_mode but not MS_CTX_IS_MULTI_GRAPH_SINK(AllReduce)
@@ -1018,6 +1065,11 @@ void CheckRunMode(device::RunMode run_mode, const FuncGraphPtr &graph) {
   if (run_mode != device::RunMode::kGraphMode) {
     return;
   }
+  // IsDisableGeKernel & pipeline+lazyinlnine
+  if (IsDisableGeKernel() && IsCellReuseAndPipeline(graph)) {
+    return;
+  }
+
   if (graph->exist_multi_target() || !is_hybrid_mode) {
     MS_LOG(EXCEPTION)
       << "The GE backend does not support subgraph sink and heterogeneous scenarios, please use the ms backend.";
