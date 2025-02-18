64426ce5aa07c18142f17486a2fce2ad8dbf66a1
huangyong <huangyong92@huawei.com>
2025-02-18 09:21:56 +0800

    topprouter ops support

--- mindspore/ops/kernel/ascend/aicpu/aicpu_ops/cpu_kernel/ms_kernel/topkrouter.cc
+++ mindspore/ops/kernel/ascend/aicpu/aicpu_ops/cpu_kernel/ms_kernel/topkrouter.cc
@@ -22,7 +22,7 @@
 
 namespace {
 const uint32_t kOutputNum = 2;
-const uint32_t kInputNum = 4;
+const uint32_t kInputNum = 6;
 const char *const kTopKRouter = "TopKRouter";
 #define TOPKROUTER_COMPUTE_CASE(DTYPE, TYPE, CTX)                      \
   case (DTYPE): {                                                      \
@@ -58,15 +58,20 @@ uint32_t TopKRouterCpuKernel::TopKRouterCompute(const CpuKernelContext &ctx) {
   auto capacity_ptr = static_cast<int64_t *>(ctx.Input(1)->GetData());
   auto expert_num_ptr = static_cast<int64_t *>(ctx.Input(2)->GetData());
   auto drop_type_ptr = static_cast<int64_t *>(ctx.Input(3)->GetData());
+  auto threshold_ptr = static_cast<float *>(ctx.Input(4)->GetData());
+  auto router_prob = static_cast<float *>(ctx.Input(5)->GetData());
+
   auto dispatch_index = static_cast<T *>(ctx.Output(0)->GetData());
   auto combine_index = static_cast<T *>(ctx.Output(1)->GetData());
+
   auto input_shape = ctx.Input(0)->GetTensorShape();
   auto batch = input_shape->GetDimSize(0);
   auto length = input_shape->GetDimSize(1);
   auto k = input_shape->GetDimSize(2);
-  auto capacity = *capacity_ptr;
   auto expert_num = *expert_num_ptr;
   auto drop_type = *drop_type_ptr;
+  auto capacity = *capacity_ptr;
+  auto threshold = *threshold_ptr;
 
   // init dispatch index
   auto dispatch_shape = ctx.Output(0)->GetTensorShape();
@@ -76,22 +81,16 @@ uint32_t TopKRouterCpuKernel::TopKRouterCompute(const CpuKernelContext &ctx) {
   }
   // init counter
   std::vector<int64_t> expert_counter(batch * expert_num, 0);
+  std::vector<float> token_accu_weight(batch * length, 0);
+
+  RouterInfo<T> routerinfo(input_data, capacity, expert_num, threshold, router_prob, length, k, expert_counter,
+                           token_accu_weight, dispatch_index, combine_index);
+
   if (drop_type == 0) {
     for (int bs = 0; bs < batch; bs++) {
       for (int i = 0; i < length; i++) {
         for (int j = 0; j < k; j++) {
-          auto token_index = i;
-          auto expert_id = input_data[bs * length * k + i * k + j];
-          auto position_in_expert = expert_counter[bs * expert_num + expert_id];
-          if (position_in_expert < capacity) {
-            dispatch_index[bs * expert_num * capacity + expert_id * capacity + position_in_expert] =
-              static_cast<T>(token_index + 1);
-            combine_index[bs * length * k + i * k + j] =
-              static_cast<T>(expert_id * (capacity + 1) + position_in_expert + 1);
-            expert_counter[bs * expert_num + expert_id] = static_cast<T>(position_in_expert + 1);
-          } else {
-            combine_index[bs * length * k + i * k + j] = static_cast<T>(expert_id * (capacity + 1));
-          }
+          DoCompute(ctx, i, bs, j, routerinfo);
         }
       }
     }
@@ -99,23 +98,54 @@ uint32_t TopKRouterCpuKernel::TopKRouterCompute(const CpuKernelContext &ctx) {
     for (int bs = 0; bs < batch; bs++) {
       for (int j = 0; j < k; j++) {
         for (int i = 0; i < length; i++) {
-          auto token_index = i;
-          auto expert_id = input_data[bs * length * k + i * k + j];
-          auto position_in_expert = expert_counter[bs * expert_num + expert_id];
-          if (position_in_expert < capacity) {
-            dispatch_index[bs * expert_num * capacity + expert_id * capacity + position_in_expert] =
-              static_cast<T>(token_index + 1);
-            combine_index[bs * length * k + i * k + j] =
-              static_cast<T>(expert_id * (capacity + 1) + position_in_expert + 1);
-            expert_counter[bs * expert_num + expert_id] = static_cast<T>(position_in_expert + 1);
-          } else {
-            combine_index[bs * length * k + i * k + j] = static_cast<T>(expert_id * (capacity + 1));
-          }
+          DoCompute(ctx, i, bs, j, routerinfo);
         }
       }
     }
   }
   return KERNEL_STATUS_OK;
+}
+
+template <typename T>
+void TopKRouterCpuKernel::DoCompute(const CpuKernelContext &ctx, const int i, const int bs, const int j,
+                                    RouterInfo<T> &routerinfo) {
+  const T *input_data = routerinfo.input_data;
+  const int64_t capacity = routerinfo.capacity;
+  const int64_t expert_num = routerinfo.expert_num;
+  const float threshold = routerinfo.threshold;
+  const float *router_prob = routerinfo.router_prob;
+  const int64_t length = routerinfo.length;
+  const int64_t k = routerinfo.k;
+
+  std::vector<int64_t> &expert_counter = routerinfo.expert_counter;
+  std::vector<float> &token_accu_weight = routerinfo.token_accu_weight;
+
+  T *dispatch_index = routerinfo.dispatch_index;
+  T *combine_index = routerinfo.combine_index;
+
+  // add
+  auto token_index = i;
+  auto expert_id = input_data[bs * length * k + i * k + j];
+  auto position_in_expert = expert_counter[bs * expert_num + expert_id];
+  bool condition;
+  if (threshold > 0) {
+    condition = (position_in_expert < capacity &&
+                 token_accu_weight[bs * length + i] + router_prob[bs * length * k + i * k + j] < threshold);
+  } else {
+    condition = (position_in_expert < capacity);
+  }
+  if (condition) {
+    dispatch_index[bs * expert_num * capacity + expert_id * capacity + position_in_expert] =
+      static_cast<T>(token_index + 1);
+    combine_index[bs * length * k + i * k + j] = static_cast<T>(expert_id * (capacity + 1) + position_in_expert + 1);
+    expert_counter[bs * expert_num + expert_id] = static_cast<T>(position_in_expert + 1);
+  } else {
+    combine_index[bs * length * k + i * k + j] = static_cast<T>(expert_id * (capacity + 1));
+    if (threshold > 0) {
+      token_accu_weight[bs * length + i] =
+        token_accu_weight[bs * length + i] + router_prob[bs * length * k + i * k + j];
+    }
+  }
 }
 
 REGISTER_MS_CPU_KERNEL(kTopKRouter, TopKRouterCpuKernel);
