5ad66a807245c0804099e393752eadc3376bcee3
戴仁杰 <dairenjie@huawei.com>
2025-03-03 11:59:02 +0800

    GMMV4 supports multi-tensor and add st cases

--- mindspore/ops/infer/ops_func_impl/grouped_matmul_v4.cc
+++ mindspore/ops/infer/ops_func_impl/grouped_matmul_v4.cc
@@ -60,6 +60,52 @@ int64_t GroupedMatmulV4FuncImpl::FetchGroupListSize(const PrimitivePtr &primitiv
   return input_infos[idxes_.group_list]->IsDynamic() ? abstract::Shape::kShapeDimAny : group_list_shape[kIndex0];
 }
 
+int32_t GroupedMatmulV4FuncImpl::PrivateCheckValidation(const PrimitivePtr &primitive,
+                                                        const InferInfoPtrList &input_infos, int64_t group_type) const {
+  if (group_type == -1) {
+    return OP_CHECK_SUCCESS;
+  }
+  // check the value of group_list
+  ShapeVector x_shape;
+  if (input_infos[idxes_.x]->IsSequence()) {
+    x_shape = input_infos[idxes_.x]->GetSequenceElements().at(kIndex0)->GetShape();
+  } else {
+    x_shape = input_infos[idxes_.x]->GetShape();
+  }
+  auto group_list_type_opt = input_infos[group_list_type_idx_]->GetScalarValue<int64_t>();
+  auto group_list_opt = input_infos[idxes_.group_list]->GetArrayValue<int64_t>();
+  if (MS_UNLIKELY(!group_list_type_opt.has_value() || !group_list_opt.has_value() || IsDynamic(x_shape))) {
+    return OP_CHECK_RETRY;
+  }
+
+  const auto &group_list = group_list_opt.value().ToVector();
+  auto group_list_type = group_list_type_opt.value();
+  if (MS_UNLIKELY(group_list_type != 0 && group_list_type != 1)) {
+    MS_EXCEPTION(ValueError) << "For '" << primitive->name() << "', group_list_type should be 0 or 1, but got "
+                             << group_list_type;
+  }
+  if (group_list_type == 0) {
+    for (size_t i = 0; i < group_list.size(); ++i) {
+      if (i == kIndex0) {
+        MS_CHECK_VALUE(group_list[i] >= 0, CheckAndConvertUtils::FormatCheckIntegerMsg(
+                                             "element of group_list", group_list[i], kGreaterEqual, 0, primitive));
+      } else {
+        if (MS_UNLIKELY(group_list[i] < group_list[i - 1])) {
+          MS_EXCEPTION(ValueError) << "For '" << primitive->name()
+                                   << "', the group_list must be an incrementing sequence, but got " << group_list;
+        }
+      }
+    }
+  } else {
+    for (auto &e : group_list) {
+      MS_CHECK_VALUE(
+        e >= 0, CheckAndConvertUtils::FormatCheckIntegerMsg("element of group_list", e, kGreaterEqual, 0, primitive));
+    }
+  }
+
+  return OP_CHECK_SUCCESS;
+}
+
 TypeIdList GroupedMatmulV4FuncImpl::InferType(const PrimitivePtr &primitive,
                                               const InferInfoPtrList &input_infos) const {
   auto ms_context = MsContext::GetInstance();
