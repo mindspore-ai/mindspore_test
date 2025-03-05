fa71c435562f487e69a3fcc35f1e3142cb506184
fandawei <fandawei2@huawei.com>
2025-03-05 16:10:33 +0800

    dump support initial iter config

--- mindspore/ccsrc/pybind_api/utils/dump_control_py.cc
+++ mindspore/ccsrc/pybind_api/utils/dump_control_py.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Huawei Technologies Co., Ltd
+ * Copyright 2024-2025 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,6 +28,7 @@ void RegDumpControl(py::module *m) {
   m->def("_dump_set_dynamic", []() { DumpControl::GetInstance().SetDynamicDump(); })
     .def("_dump_start", []() { DumpControl::GetInstance().DynamicDumpStart(); })
     .def("_dump_stop", []() { DumpControl::GetInstance().DynamicDumpStop(); })
+    .def("_set_init_iter", [](std::uint32_t v) { DumpControl::GetInstance().SetInitialIteration(v); })
     .def("_tensordump_set_step",
          [](const std::vector<size_t> &v) { TensorDumpStepManager::GetInstance().SetDumpStep(v); })
     .def("_tensordump_process_file", [](const std::string &filename, const std::string &dtype) -> std::string {
