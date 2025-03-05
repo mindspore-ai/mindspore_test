8b6e08c394521f0a7a44308521fb5e3fc7dc1de9
xiaopeng <xiaopeng52@huawei.com>
2025-03-05 16:46:55 +0800

    fix narrow infershape

--- mindspore/ops/infer/ops_func_impl/narrow.cc
+++ mindspore/ops/infer/ops_func_impl/narrow.cc
@@ -36,12 +36,23 @@ BaseShapePtr NarrowFuncImpl::InferShape(const PrimitivePtr &primitive,
   auto axis_value_opt = GetScalarValue<int64_t>(input_args[kInputIndex1]->GetValue());
   auto begin_value_opt = GetScalarValue<int64_t>(input_args[kInputIndex2]->GetValue());
   auto length_value_opt = GetScalarValue<int64_t>(input_args[kInputIndex3]->GetValue());
-  if (!axis_value_opt.has_value() || !begin_value_opt.has_value() || !length_value_opt.has_value()) {
-    return std::make_shared<abstract::TensorShape>(ShapeVector{abstract::TensorShape::kShapeRankAny});
-  }
 
   auto axis_value = axis_value_opt.value();
   auto x_rank = SizeToLong(input_x_shape.size());
+
+  if (!axis_value_opt.has_value()) {
+    auto out_shape = input_x_shape;
+    for (int dim = 0; dim < x_rank; ++dim) {
+      out_shape[dim] = abstract::Shape::kShapeDimAny;
+    }
+    return std::make_shared<abstract::TensorShape>(out_shape);
+  }
+
+  if (!begin_value_opt.has_value() || !length_value_opt.has_value()) {
+    auto out_shape = input_x_shape;
+    out_shape[axis_value] = abstract::Shape::kShapeDimAny;
+    return std::make_shared<abstract::TensorShape>(out_shape);
+  }
 
   MS_CHECK_VALUE(axis_value >= -x_rank && axis_value < x_rank, "dim value error. dim:" + std::to_string(axis_value) +
                                                                  ", dim should be in [" + std::to_string(-x_rank) +
