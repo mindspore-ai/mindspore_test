4a6627c603f7d67a97f0e9c80fb7b0f93057f340
qiuyufeng <qiuyufeng3@huawei.com>
2025-03-05 17:56:38 +0800

    add state_dict and buffer doc

--- mindspore/python/mindspore/nn/utils/hooks.py
+++ mindspore/python/mindspore/nn/utils/hooks.py
@@ -25,10 +25,10 @@ class RemovableHandle:
     A handle which provides the capability to remove a hook.
 
     Args:
-        hooks_dict (dict): A dictionary of hooks, indexed by hook ``id``.
+        hooks_dict (dict): A dictionary of hooks, indexed by hook `id`.
         extra_dict (Union[dict, List[dict]]): An additional dictionary or list of
             dictionaries whose keys will be deleted when the same keys are
-            removed from ``hooks_dict``.
+            removed from `hooks_dict`.
     """
 
     id: int
