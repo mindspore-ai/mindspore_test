e554b09444527562221882bf6f7f00a9e614eae3
ckey_Dou <ckey.chengbin@huawei.com>
2025-03-11 00:58:08 +0800

    update lib for lccl

--- tests/st/networks/large_models/test_parallel_infer.py
+++ tests/st/networks/large_models/test_parallel_infer.py
@@ -62,7 +62,7 @@ class TestInferParallel:
         if not ascend_home_path:
             os.environ['ASCEND_HOME_PATH'] = "/usr/local/Ascend/latest"
 
-    @arg_mark(plat_marks=['platform_ascend910b'], level_mark='level1', card_mark='allcards', essential_mark='essential')
+    @arg_mark(plat_marks=['platform_ascend910b'], level_mark='level0', card_mark='allcards', essential_mark='essential')
     def test_base_cases(self):
         """
         Feature: Infer interface
