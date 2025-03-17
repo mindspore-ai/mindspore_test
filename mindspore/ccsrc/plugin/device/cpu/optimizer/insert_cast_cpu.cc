3ef0fee6ee5b775bec55fb64421cf8f9c3bccdbd
gaoyong10 <gaoyong10@huawei.com>
2025-03-17 12:35:47 +0800

    Fix cast ref on cpu.

--- mindspore/ccsrc/plugin/device/cpu/optimizer/insert_cast_cpu.cc
+++ mindspore/ccsrc/plugin/device/cpu/optimizer/insert_cast_cpu.cc
@@ -31,6 +31,7 @@
 #include "plugin/device/cpu/kernel/cpu_kernel.h"
 #include "plugin/device/cpu/optimizer/cpu_pass_utils.h"
 #include "utils/ms_context.h"
+#include "mindspore/ops/op_def/auto_generate/gen_ops_primitive_a.h"
 #include "mindspore/ops/op_def/auto_generate/gen_ops_primitive_d.h"
 #include "mindspore/ops/op_def/auto_generate/gen_ops_primitive_m.h"
 
@@ -53,6 +54,53 @@ std::shared_ptr<std::vector<std::pair<AnfNodePtr, int>>> GetNodeUserList(const F
   return output_node_list;
 }
 
+AnfNodePtr AddAssignNodeToGraph(const FuncGraphPtr &func_graph, const AnfNodePtr &input1, const AnfNodePtr &input2) {
+  MS_EXCEPTION_IF_NULL(func_graph);
+  MS_EXCEPTION_IF_NULL(input1);
+  MS_EXCEPTION_IF_NULL(input2);
+  if (input1->kernel_info() == nullptr || input2->kernel_info() == nullptr || input1->abstract() == nullptr) {
+    MS_LOG(DEBUG) << "Invalid kernel info, input1:" << input1->kernel_info() << " input2:" << input2->kernel_info()
+                  << " abstract:" << input1->abstract() << " for graph:" << func_graph->ToString();
+    return input2;
+  }
+  const auto &kernel_info1 = dynamic_cast<device::KernelInfo *>(input1->kernel_info());
+  const auto &kernel_info2 = dynamic_cast<device::KernelInfo *>(input2->kernel_info());
+  MS_EXCEPTION_IF_NULL(kernel_info1);
+  MS_EXCEPTION_IF_NULL(kernel_info2);
+  if (!kernel_info1->has_build_info() || !kernel_info2->has_build_info()) {
+    MS_LOG(DEBUG) << "Invalid build info, input1:" << kernel_info1->has_build_info()
+                  << " input2:" << kernel_info2->has_build_info() << " for graph:" << func_graph->ToString();
+    return input2;
+  }
+
+  CNodePtr assign =
+    func_graph->NewCNode({NewValueNode(std::make_shared<Primitive>(prim::kPrimAssign->name())), input1, input2});
+  MS_EXCEPTION_IF_NULL(assign);
+  assign->set_abstract(input1->abstract()->Clone());
+
+  // set kernel build info
+  kernel::KernelBuildInfo::KernelBuildInfoBuilder builder;
+  builder.SetInputsFormat({kernel_info1->select_kernel_build_info()->GetOutputFormat(0),
+                           kernel_info2->select_kernel_build_info()->GetOutputFormat(0)});
+  builder.SetOutputsFormat({kernel_info1->select_kernel_build_info()->GetOutputFormat(0)});
+  builder.SetInputsDeviceType({kernel_info1->select_kernel_build_info()->GetOutputDeviceType(0),
+                               kernel_info2->select_kernel_build_info()->GetOutputDeviceType(0)});
+  builder.SetOutputsDeviceType({kernel_info1->select_kernel_build_info()->GetOutputDeviceType(0)});
+  builder.SetInputsKernelObjectType({kernel_info1->select_kernel_build_info()->GetOutputKernelObjectType(0),
+                                     kernel_info2->select_kernel_build_info()->GetOutputKernelObjectType(0)});
+  builder.SetOutputsKernelObjectType({kernel_info1->select_kernel_build_info()->GetOutputKernelObjectType(0)});
+  auto kernel_info = std::make_shared<device::KernelInfo>();
+  assign->set_kernel_info(kernel_info);
+  AnfAlgo::SetSelectKernelBuildInfo(builder.Build(), assign.get());
+
+  auto depend_node =
+    func_graph->NewCNode({NewValueNode(std::make_shared<Primitive>(prim::kPrimDepend->name())), input1, assign});
+  MS_EXCEPTION_IF_NULL(depend_node);
+  depend_node->set_abstract(assign->abstract());
+  MS_LOG(DEBUG) << "Add depend node:" << depend_node->fullname_with_scope() << " for graph:" << func_graph->ToString();
+  return depend_node;
+}
+
 void SyncWeightNodeWithCast(const FuncGraphPtr &func_graph, const CNodePtr &cnode, const AnfNodePtr &cur_input,
                             const AnfNodePtr &cast, const std::string &format, const TypeId &device_type,
                             const TypeId &origin_type, const abstract::BaseShapePtr &origin_shape,
@@ -65,10 +113,22 @@ void SyncWeightNodeWithCast(const FuncGraphPtr &func_graph, const CNodePtr &cnod
   MS_EXCEPTION_IF_NULL(first_depend_node);
   first_depend_node->set_abstract(cast->abstract());
   auto post_cast = AddCastOpNodeToGraph(func_graph, first_depend_node, format, device_type, origin_type, origin_shape);
-  auto kernel_graph = func_graph->cast<KernelGraphPtr>();
-  MS_EXCEPTION_IF_NULL(kernel_graph);
-  kernel_graph->AddRefCorrespondPairs(std::make_pair(post_cast, 0), common::AnfAlgo::VisitKernel(cur_input, 0));
-  make_tuple_inputs->push_back(post_cast);
+  // As some cpu optimizer does not support fp16, the graph will be like:
+  // para(fp16)
+  // %1 = cast(para, fp32)
+  // %2 = opt(%1)
+  // %3 = depend(%1, %2)
+  // %4 = cast(%3)
+  // and the parameter should be update by %4, and should be add an assign op.
+  auto assign = AddAssignNodeToGraph(func_graph, cur_input, post_cast);
+  if (!common::AnfAlgo::CheckPrimitiveType(assign, prim::kPrimDepend)) {
+    auto kernel_graph = func_graph->cast<KernelGraphPtr>();
+    MS_EXCEPTION_IF_NULL(kernel_graph);
+    kernel_graph->AddRefCorrespondPairs(std::make_pair(post_cast, 0), common::AnfAlgo::VisitKernel(cur_input, 0));
+    MS_LOG(DEBUG) << "Add ref relation for cur node:" << cur_input->fullname_with_scope()
+                  << " and post node:" << post_cast->fullname_with_scope() << " for graph:" << func_graph->ToString();
+  }
+  make_tuple_inputs->push_back(assign);
 }
 
 void InsertCast(const FuncGraphPtr &func_graph, const CNodePtr &cnode) {
