Fix ops.rand_like/ops.randint/ops.randint_like/ops.randn/ops.randn_like/ops.random_gamma/ops.random_poisson/ops.randperm/ops.scatter_update/ops.derivative/ops.print_/ops.tensordump/ops.argwhere/ops.bincount/ops.block_diag/ops.jet/ops.batch_to_space_nd docs

mindspore/python/mindspore/ops/function/array_func.py
@@ -2615,9 +2615,13 @@ def scatter_div(input_x, indices, updates):
 
 def scatter_update(input_x, indices, updates):
     r"""
-    Updates tensor values by using input indices and value.
+    Updates the input tensor values using the given input indices and update values.
 
-    Using given values to update tensor value, along with the input indices.
+    .. note::
+        - Support implicit type conversion and type promotion.
+        - Since Parameter objects do not support type conversion,
+          an exception will be thrown when input_x is of a low-precision data type.
+        - The `updates` with a shape of `indices.shape + input_x.shape[1:]` .
 
     for each `i, ..., j` in `indices.shape`:
 
@@ -2625,39 +2629,26 @@ def scatter_update(input_x, indices, updates):
 
         \text{input_x}[\text{indices}[i, ..., j], :] = \text{updates}[i, ..., j, :]
 
-    Inputs of `input_x` and `updates` comply with the implicit type conversion rules to make the data types consistent.
-    If they have different data types, the lower priority data type will be converted to
-    the relatively highest priority data type.
-
     Args:
-        input_x (Union[Parameter, Tensor]): The target tensor, with data type of Parameter or Tensor.
-        indices (Tensor): The index of input tensor. With int32 or int64 data type.
+        input_x (Union[Parameter, Tensor]): The input parameter or tensor.
+        indices (Tensor): Specify the indices for update operation.
             If there are duplicates in indices, the order for updating is undefined.
-        updates (Tensor): The tensor to update the input tensor, has the same type as input,
-            and updates.shape = indices.shape + input_x.shape[1:].
+        updates (Tensor): The values to update.
 
     Returns:
-        Tensor, has the same shape and type as `input_x`.
-
-    Raises:
-        TypeError: If `indices` is not an int32 or an int64.
-        ValueError: If the shape of `updates` is not equal to `indices.shape + input_x.shape[1:]`.
-        RuntimeError: If the data type of `input_x` and `updates` conversion is required when data type conversion
-                      is not supported.
+        Tensor
 
     Supported Platforms:
         ``Ascend`` ``GPU`` ``CPU``
 
     Examples:
         >>> import mindspore
-        >>> import numpy as np
-        >>> from mindspore import Tensor, ops
-        >>> np_x = np.array([[-0.1, 0.3, 3.6], [0.4, 0.5, -3.2]])
-        >>> input_x = mindspore.Parameter(Tensor(np_x, mindspore.float32), name="x")
-        >>> indices = Tensor(np.array([0, 1]), mindspore.int32)
-        >>> np_updates = np.array([[2.0, 1.2, 1.0], [3.0, 1.2, 1.0]])
-        >>> updates = Tensor(np_updates, mindspore.float32)
-        >>> output = ops.scatter_update(input_x, indices, updates)
+        >>> np_x = mindspore.tensor([[-0.1, 0.3, 3.6], [0.4, 0.5, -3.2]], mindspore.float32)
+        >>> input_x = mindspore.Parameter(np_x, name="x")
+        >>> indices = mindspore.tensor([0, 1], mindspore.int32)
+        >>> np_updates =  mindspore.tensor([[2.0, 1.2, 1.0], [3.0, 1.2, 1.0]])
+        >>> updates =  mindspore.tensor(np_updates, mindspore.float32)
+        >>> output = mindspore.ops.scatter_update(input_x, indices, updates)
         >>> print(output)
         [[2. 1.2  1.]
          [3. 1.2  1.]]
@@ -3977,28 +3968,17 @@ def batch_to_space_nd(input_x, block_shape, crops):
             :math:`input\_shape[i+offset]*block\_shape[i] > crops[i][0]+crops[i][1]`
 
     Returns:
-        Tensor, the output tensor with the same type as input.
-
-    Raises:
-        TypeError: If `block_shape` is not one of list, tuple, int.
-        TypeError: If `crops` is neither list nor tuple.
-        ValueError: If `block_shape` is not one dimensional when `block_shape` is a list or tuple.
-        ValueError: If the length of `block_shape` is not 2 on Ascend.
-        ValueError: If the element of `block_shape` is not an integer larger than or euqal to 1.
-        ValueError: If shape of `crops` is not (M, 2), where M is the length of `block_shape`.
-        ValueError: If the element of `crops` is not an integer larger than or euqal to 0.
+        Tensor
 
     Supported Platforms:
         ``Ascend`` ``GPU`` ``CPU``
 
     Examples:
         >>> import mindspore
-        >>> import numpy as np
-        >>> from mindspore import Tensor, ops
         >>> block_shape = [2, 2]
         >>> crops = [[0, 0], [0, 0]]
-        >>> input_x = Tensor(np.array([[[[1]]], [[[2]]], [[[3]]], [[[4]]]]), mindspore.float32)
-        >>> output = ops.batch_to_space_nd(input_x, block_shape, crops)
+        >>> input_x = mindspore.tensor([[[[1]]], [[[2]]], [[[3]]], [[[4]]]], mindspore.float32)
+        >>> output = mindspore.ops.batch_to_space_nd(input_x, block_shape, crops)
         >>> print(output)
         [[[[1.  2.]
            [3.  4.]]]]
@@ -6684,30 +6664,25 @@ def nonzero(input, *, as_tuple=False):
 
 def argwhere(input):
     """
-    Return a Tensor of the positions of all non-zero values.
+    Return a tensor of containing the positions of all non-zero elements in the input tensor.
 
     Args:
-        input (Tensor): The input tensor. The data type is Number or Bool.
+        input (Tensor): The input tensor.
 
     Returns:
-        Tensor, a 2-D Tensor whose data type is int64, containing the positions of all non-zero values of the input.
-
-    Raises:
-        TypeError: If `input` is not Tensor.
-        ValueError: If dim of `input` equals to 0.
+        2-D Tensor
 
     Supported Platforms:
         ``Ascend`` ``GPU`` ``CPU``
 
     Examples:
         >>> import mindspore
-        >>> from mindspore import Tensor, ops
-        >>> import numpy as np
-        >>> x = Tensor(np.array([[[1,  0], [-5, 0]]]), mindspore.int32)
-        >>> output = ops.argwhere(x)
+        >>> x = mindspore.tensor([[[1,  0], [-5, 0]]], mindspore.int32)
+        >>> output = mindspore.ops.argwhere(x)
         >>> print(output)
-        [[0 0 0]
-         [0 1 0]]
+        Tensor(shape=[2, 3], dtype=Int64, value=
+        [[0, 0, 0],
+        [0, 1, 0]])
     """
     return nonzero(input)
 

mindspore/python/mindspore/ops/function/debug_func.py
@@ -23,9 +23,9 @@ from .._primitive_cache import _get_cache_prim
 
 def print_(*input_x):
     """
-    Outputs the inputs to stdout. The outputs are printed to screen by default.
+    Outputs the inputs to stdout.
+    The outputs are printed to screen by default.
     It can also be saved in a file by setting the parameter  `print_file_path` in `context`.
-    Once set, the output will be saved in the file specified by print_file_path.
     :func:`mindspore.parse_print` can be employed to reload the data.
     For more information, please refer to :func:`mindspore.set_context` and :func:`mindspore.parse_print`.
     In Ascend platform with graph mode, the environment variables `MS_DUMP_SLICE_SIZE` and `MS_DUMP_WAIT_TIME`
@@ -33,7 +33,7 @@ def print_(*input_x):
 
     Note:
         In pynative mode, please use python print function.
-        In Ascend platform with graph mode, the bool, int and float would be converted into Tensor to print, and
+        In Ascend platform with graph mode, the bool, int and float would be converted into tensor to print, and
         str remains unchanged.
         This function is used for debugging.
 
@@ -44,17 +44,13 @@ def print_(*input_x):
     Returns:
         Invalid value, should be ignored.
 
-    Raises:
-        TypeError: If `input_x` is not one of the following: Tensor, bool, int, float, str, tuple or list.
-
     Supported Platforms:
         ``Ascend`` ``GPU`` ``CPU``
 
     Examples:
-        >>> import numpy as np
-        >>> from mindspore import Tensor, ops
-        >>> x = Tensor(np.ones([2, 1]).astype(np.int32))
-        >>> y = Tensor(np.ones([2, 2]).astype(np.int32))
+        >>> import mindspore
+        >>> x = mindspore.tensor(mindspore.ops.ones([2, 1], mindspore.int32))
+        >>> y = mindspore.tensor(mindspore.ops.ones([2, 2], mindspore.int32))
         >>> result = ops.print_('Print Tensor x and Tensor y:', x, y)
         Print Tensor x and Tensor y:
         Tensor(shape=[2, 1], dtype=Int32, value=
@@ -71,10 +67,10 @@ def print_(*input_x):
 
 def tensordump(file_name, tensor, mode='out'):
     """
-    Save Tensor in numpy's npy format.
+    Save tensor in npy format.
 
     .. warning::
-        - The parameter mode will no longer support the value 'all'.
+        - The parameter `mode` will no longer support the value 'all'.
 
     In Parallel situation, tensordump will dump slice of data at each rank.
     In Ascend platform with graph mode, Your code OpA --> OpB may compiled as OpA --> RedistributionOps --> OpB.
@@ -89,18 +85,18 @@ def tensordump(file_name, tensor, mode='out'):
     So the parameter mode is to handle this situation.
 
     Assuming OpA's output is used as both tensordump's input parameter and OpB's input parameter.
-    Different requirements of saving dump data can be achieved by configuring parameter mode:
+    Different requirements of saving dump data can be achieved by configuring parameter `mode` :
 
-    - If the mode is 'out', the dump data contains only OpA's output slice.
-    - If the mode is 'in', the dump data contains only OpB's input slice.
+    - If the `mode` is 'out', the dump data contains only OpA's output slice.
+    - If the `mode` is 'in', the dump data contains only OpB's input slice.
 
-    For mode 'in', the input slice npy file format is: fileName_dumpMode_dtype_id.npy.
+    For `mode` 'in', the input slice npy file format is: fileName_dumpMode_dtype_id.npy.
 
-    For mode 'out', the output slice npy file format is: filename_dtype_id.npy.
+    For `mode` 'out', the output slice npy file format is: fileName_dtype_id.npy.
 
     - fileName: Value of the parameter file_name
-      (if parameter file_name is a user-specified path, the value of fileName is the last level of the path).
-    - dumpMode: Value of the parameter mode.
+      (if parameter `file_name` is a user-specified path, the value of fileName is the last level of the path).
+    - dumpMode: Value of the parameter `mode`.
     - dtype: The original data type. Data of type bfloat16 stored in the .npy file will be converted to float32.
     - id: An auto increment ID.
 
@@ -108,21 +104,14 @@ def tensordump(file_name, tensor, mode='out'):
         - In Ascend platform with graph mode, the environment variables `MS_DUMP_SLICE_SIZE` and `MS_DUMP_WAIT_TIME`
           can be set to solve operator execution failure when outputting big tensor or outputting tensor intensively.
         - The operator of tensordump doesn't support in control flow.
-        - If current parallel mode is STAND_ALONE, mode should only be 'out'.
-        - Parameter mode will be set to 'out' if user doesn't configure it.
+        - If current parallel mode is STAND_ALONE, `mode` should only be 'out'.
         - This function is used for debugging.
 
     Args:
         file_name (str): The path of the npy file saves.
         tensor (Tensor): The tensor that user want to dump.
         mode (str, optional): Used to control tensordump behavior, available value is one of ['in', 'out'].
-            Default value is ``out``.
-
-    Raises:
-        TypeError: If `file_name` is not str.
-        TypeError: If `tensor` is not Tensor.
-        TypeError: If `mode` is not str.
-        ValueError: If `mode` is not in one of ['in', 'out'].
+            Default ``out`` .
 
     Supported Platforms:
         ``Ascend``
@@ -134,38 +123,37 @@ def tensordump(file_name, tensor, mode='out'):
 
         >>> import os
         >>> import numpy as np
-        >>> import mindspore as ms
-        >>> from mindspore import nn, Tensor, ops, context
-        >>> from mindspore.ops import operations as P
+        >>> import mindspore
+        >>> from mindspore import nn, context
         >>> from mindspore.communication import init, get_rank
         >>> init()
         >>> rank_id = get_rank()
-        >>> dump_path = f'dumps/rank_{rank_id}/mul1_mul2.npy'
+        >>> dump_path = f'rank_{rank_id}_mul1_mul2.npy'
         >>> class Net(nn.Cell):
         ...     def __init__(self, strategy1, strategy2):
         ...         super(Net, self).__init__()
-        ...         self.matmul1 = P.MatMul().shard(strategy1)
-        ...         self.matmul2 = P.MatMul().shard(strategy2)
+        ...         self.matmul1 = mindspore.ops.MatMul().shard(strategy1)
+        ...         self.matmul2 = mindspore.ops.MatMul().shard(strategy2)
         ...
         ...     def construct(self, x, y, b):
         ...         out1 = self.matmul1(x, y)
-        ...         ops.tensordump(dump_path, out1, 'out')
+        ...         mindspore.ops.tensordump(dump_path, out1, 'out')
         ...         out2 = self.matmul2(out1, b)
         ...         return out2
         ...
-        >>> ms.set_context(mode=ms.GRAPH_MODE)
+        >>> mindspore.set_context(mode=mindspore.GRAPH_MODE)
         >>> os.environ["MS_DEV_SAVE_GRAPHS"] = "2"
         >>> context.set_auto_parallel_context(parallel_mode='semi_auto_parallel', full_batch=True)
         >>> strategy1 = ((1, 2), (2, 1))
         >>> strategy2 = ((1, 2), (2, 1))
         >>> net = Net(strategy1, strategy2)
-        >>> x = Tensor(0.1 * np.random.randn(64, 64).astype(np.float32))
-        >>> y = Tensor(0.1 * np.random.randn(64, 64).astype(np.float32))
-        >>> b = Tensor(0.1 * np.random.randn(64, 64).astype(np.float32))
+        >>> x = mindspore.tensor(0.1 * mindspore.ops.randn(64, 64), mindspore.float32)
+        >>> y = mindspore.tensor(0.1 * mindspore.ops.randn(64, 64), mindspore.float32)
+        >>> b = mindspore.tensor(0.1 * mindspore.ops.randn(64, 64), mindspore.float32)
         >>> out = net(x, y, b)
         >>> print(f"out shape is: {out.shape}")
         >>> # out shape is (64, 64)
-        >>> matmul1_output_slice = np.load('dumps/rank_0/mul1_mul2_float32_0.npy')      # load matmul1's output slice
+        >>> matmul1_output_slice = np.load(f'rank_{rank_id}_mul1_mul2_float32_0.npy')      # load matmul1's output slice
         >>> print(f"matmul1_output_slice is loaded, shape is: {matmul1_output_slice.shape}")
         >>> # matmul1_output_slice is loaded, shape is: (64, 64)
     """

mindspore/python/mindspore/ops/function/grad/grad_func.py
@@ -467,49 +467,42 @@ def jet(fn, primals, series):
     while the other to 0, which is like the derivative of origin input with respect to itself.
 
     Note:
-        If `primals` is Tensor of int type, it will be converted to Tensor of float type.
+        If `primals` is tensor of int type, it will be converted to Tensor of float type.
 
     Args:
         fn (Union[Cell, function]): Function to do TaylorOperation.
         primals (Union[Tensor, tuple[Tensor]]): The inputs to `fn`.
-        series (Union[Tensor, tuple[Tensor]]): If tuple, the length and type of series should be the same as inputs.
-            For each Tensor, the length of first dimension `i` represents the `1` to `i+1`-th order of derivative of
-            output with respect to the inputs will be figured out.
+        series (Union[Tensor, tuple[Tensor]]): The original 1st to nth order derivatives of the input.
+            The index `i` of the zeroth dimension of the tensor corresponds to the `i+1` -th order derivative of the
+            output with respect to the input.
 
     Returns:
-        Tuple, tuple of out_primals and out_series.
+        Tuple(out_primals, out_series)
 
         - **out_primals** (Union[Tensor, list[Tensor]]) - The output of `fn(primals)`.
-        - **out_series** (Union[Tensor, list[Tensor]]) - The `1` to `i+1`-th order of derivative of output with respect
+        - **out_series** (Union[Tensor, list[Tensor]]) - The `1` to `i+1` -th order of derivative of output with respect
           to the inputs.
-
-    Raises:
-        TypeError: If `primals` is not a tensor or tuple of tensors.
-        TypeError: If type of `primals` is not the same as type of `series`.
 
     Supported Platforms:
         ``Ascend`` ``GPU`` ``CPU``
 
     Examples:
-        >>> import numpy as np
-        >>> import mindspore.nn as nn
-        >>> import mindspore as ms
-        >>> from mindspore import ops
-        >>> from mindspore import Tensor
-        >>> ms.set_context(mode=ms.GRAPH_MODE)
+        >>> import mindspore
+        >>> from mindspore import nn
+        >>> mindspore.set_context(mode=mindspore.GRAPH_MODE)
         >>> class Net(nn.Cell):
         ...     def __init__(self):
         ...         super().__init__()
-        ...         self.sin = ops.Sin()
-        ...         self.exp = ops.Exp()
+        ...         self.sin = mindspore.ops.Sin()
+        ...         self.exp = mindspore.ops.Exp()
         ...     def construct(self, x):
         ...         out1 = self.sin(x)
         ...         out2 = self.exp(out1)
         ...         return out2
-        >>> primals = Tensor(np.array([[1, 2], [3, 4]]).astype(np.float32))
-        >>> series = Tensor(np.array([[[1, 1], [1, 1]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]]).astype(np.float32))
+        >>> primals = mindspore.tensor([[1, 2], [3, 4]], mindspore.float32)
+        >>> series = mindspore.tensor([[[1, 1], [1, 1]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]], mindspore.float32)
         >>> net = Net()
-        >>> out_primals, out_series = ops.jet(net, primals, series)
+        >>> out_primals, out_series = mindspore.ops.jet(net, primals, series)
         >>> print(out_primals, out_series)
         [[2.319777  2.4825778]
          [1.1515628 0.4691642]] [[[ 1.2533808  -1.0331168 ]
@@ -577,49 +570,41 @@ def derivative(fn, primals, order):
     input first order derivative is set to 1, while the other to 0.
 
     Note:
-        If `primals` is Tensor of int type, it will be converted to Tensor of float type.
+        If `primals` is tensor of int type, it will be converted to tensor of float type.
 
     Args:
         fn (Union[Cell, function]): Function to do TaylorOperation.
         primals (Union[Tensor, tuple[Tensor]]): The inputs to `fn`.
-        order (int): For each Tensor, the `order`-th order of derivative of output with respect to the inputs will be
-            figured out.
+        order (int): The order of differentiation.
 
     Returns:
-        Tuple, tuple of out_primals and out_series.
+        Tuple(out_primals, out_series)
 
         - **out_primals** (Union[Tensor, list[Tensor]]) - The output of `fn(primals)`.
         - **out_series** (Union[Tensor, list[Tensor]]) - The `order`-th order of derivative of output with respect
           to the inputs.
 
-    Raises:
-        TypeError: If `primals` is not a tensor or tuple of tensors.
-        TypeError: If `order` is not int.
-        ValueError: If `order` is less than 1.
-
     Supported Platforms:
         ``Ascend`` ``GPU`` ``CPU``
 
     Examples:
-        >>> import numpy as np
-        >>> import mindspore as ms
-        >>> import mindspore.nn as nn
-        >>> from mindspore import ops
-        >>> from mindspore import Tensor
-        >>> ms.set_context(mode=ms.GRAPH_MODE)
+        >>> import mindspore
+        >>> from mindspore import nn
+        >>> mindspore.set_context(mode=mindspore.GRAPH_MODE)
         >>> class Net(nn.Cell):
         ...     def __init__(self):
         ...         super().__init__()
-        ...         self.sin = ops.Sin()
-        ...         self.exp = ops.Exp()
+        ...         self.sin = mindspore.ops.Sin()
+        ...         self.exp = mindspore.ops.Exp()
         ...     def construct(self, x):
         ...         out1 = self.sin(x)
         ...         out2 = self.exp(out1)
         ...         return out2
-        >>> primals = Tensor(np.array([[1, 2], [3, 4]]).astype(np.float32))
+        >>>
+        >>> primals = mindspore.tensor([[1, 2], [3, 4]], mindspore.float32)
         >>> order = 3
         >>> net = Net()
-        >>> out_primals, out_series = ops.derivative(net, primals, order)
+        >>> out_primals, out_series = mindspore.ops.derivative(net, primals, order)
         >>> print(out_primals, out_series)
         [[2.319777  2.4825778]
          [1.1515628 0.4691642]] [[-4.0515366   3.6724353 ]