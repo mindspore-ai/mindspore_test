ce013523063db0b695f87f90ba4771b783b11cf5
lizhenyu <lizhenyu13@huawei.com>
2025-03-19 14:35:05 +0800

    support convert dynamic shape to static shape for llm infer in pynative mode+jit

--- mindspore/ccsrc/runtime/graph_scheduler/actor/data_prepare_actor.cc
+++ mindspore/ccsrc/runtime/graph_scheduler/actor/data_prepare_actor.cc
@@ -505,7 +505,6 @@ void DataPrepareActor::UpdateDeviceAddressForDataNode(const AnfNodePtr &input_no
   // Assign tensor address to input data node and set `ref_count` to `SIZE_MAX` for avoiding clean.
   (void)address_modified_input_nodes_.insert(input_node.get());
   tensor_address->set_flag(device_address->flag());
-  DeviceAddressUtils::UpdateDeviceAddressHostInfoByNode(tensor_address, input_node, 0);
   AnfAlgo::SetOutputAddr(tensor_address, 0, input_node.get());
   MS_LOG(DEBUG) << "Update device address of " << input_node->DebugString() << " to " << tensor_address.get()
                 << ", kernel tensor addr:" << tensor_address->kernel_tensor().get()
