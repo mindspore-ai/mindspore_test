4252afb54e592785f3495130945698b12175d37f
fengyihang <fengyihang1@huawei.com>
2025-03-25 17:22:37 +0800

    modify unifiy docs

--- mindspore/python/mindspore/parallel/transform_safetensors.py
+++ mindspore/python/mindspore/parallel/transform_safetensors.py
@@ -1055,9 +1055,10 @@ def _cal_param_name_map_and_param_list(file_list, total_safetensors_dir, json_fi
             values = len(keys) * [file_list[0]]
             param_name_map = dict(zip(keys, values))
     else:
-        if len(json_files) != 1:
-            raise ValueError(f"For 'load_parallel_checkpoint', the number of json files in 'total_safetensors_dir' "
-                             f"must be 1, but got {len(json_files)}.")
+        if not json_files:
+            raise ValueError(
+                f"For 'load_parallel_checkpoint', there must be a JSON file named 'param_name_map.json' in "
+                f"the 'total_safetensors_dir'.")
         param_name_json = os.path.join(total_safetensors_dir, json_files[0])
         with open(param_name_json, 'r') as f:
             param_name_map = json.load(f)
