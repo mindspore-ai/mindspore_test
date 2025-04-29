4a62b5b61d04b8810062289ddb7af9197ca89fd4
ZPac <zhoupeichen@huawei.com>
2025-04-29 21:57:49 +0800

    Fix event and stream case

--- tests/st/hal/test_hal_stream.py
+++ tests/st/hal/test_hal_stream.py
@@ -1,3 +1,4 @@
+
 # Copyright 2023 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -121,12 +122,12 @@ def test_runtime_wait_event():
     with ms.runtime.StreamCtx(s1):
         b = ops.matmul(a, a)
         ev.record(s1)
-        assert ev.query() is False
 
     with ms.runtime.StreamCtx(s2):
         s2.wait_event(ev)
         c = ops.matmul(b, b)
     ms.runtime.synchronize()
+    assert ev.query() is True
     assert np.allclose(ops.mm(a, a).asnumpy(), b.asnumpy())
     assert np.allclose(ops.mm(b, b).asnumpy(), c.asnumpy())
 
