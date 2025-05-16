0cb26ec3a4eaeb07ad953cffeeb7f28a52852a9f
熊攀 <xiongpan17@huawei.com>
2025-05-16 10:10:48 +0800

    适配resized算子

--- mindspore/lite/tools/optimizer/graph/attr_to_args_pass.cc
+++ mindspore/lite/tools/optimizer/graph/attr_to_args_pass.cc
@@ -109,8 +109,8 @@ static const std::map<std::string, std::vector<std::pair<std::string, size_t>>> 
   {"Triu", {{"diagonal", 2}}},
   {"SoftMarginLoss", {{"reduction", 3}}},
   {"SmoothL1Loss", {{"beta", 3}, {"reduction", 4}}},
-  {"TensorScatterElements", {{"axis", 4}, {"reduction", 5}, {"reduce", 5}}}  // reduce OR reduction is passed
-};
+  {"TensorScatterElements", {{"axis", 4}, {"reduction", 5}, {"reduce", 5}}},  // reduce OR reduction is passed
+  {"ResizeD", {{"sizes", 2}, {"scales", 3}, {"coordinate_transformation_mode", 4}}}};
 
 constexpr size_t kMatMulInputSizeWithBias = 6;  // primitive, x1, x2, bias, transpose_a, transpose_b
 constexpr size_t kInputSizeTwo = 2;
