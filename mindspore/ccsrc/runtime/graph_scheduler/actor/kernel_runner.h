6a1a35aef8c6f459c054ab83f326bd58a6b5c27a
jiaxueyu <jiaxueyu@huawei.com>
2025-05-29 18:48:50 +0800

    use synccopy for copy parameter

--- mindspore/ccsrc/runtime/graph_scheduler/actor/kernel_runner.h
+++ mindspore/ccsrc/runtime/graph_scheduler/actor/kernel_runner.h
@@ -321,10 +321,8 @@ class KernelRunner {
   void FetchOutputDeviceTensor(OpContext<KernelTensor> *const context);
   void FetchWorkspaceDeviceTensor();
   // Need copy when the data type or format between real parameters and formal parameters are inconsistent.
-  void CopyInputDeviceTensor(KernelTensorPtr device_tensor, size_t input_index, OpContext<KernelTensor> *const context);
-  // Use for graph parameter.
-  void CopyParameterDeviceTensor(KernelTensorPtr kernel_tensor, size_t input_index,
-                                 OpContext<KernelTensor> *const context, size_t stream_id);
+  void CopyInputDeviceTensor(KernelTensorPtr device_tensor, size_t input_index, bool is_infer_boost_parameter,
+                             OpContext<KernelTensor> *const context);
   // The processing before kernel launch: update the info of kernel launch.
   void PreLaunchKernel(OpContext<KernelTensor> *const context);
   // Back refresh the dynamic device tensor stores that have been triggered copy.
