fb64b8dfe90277bc21f9e904e561426ac8fca2c4
yanghaoran <yanghaoran2@huawei.com>
2025-06-09 17:20:34 +0800

    unregistered network testcases

--- tests/st/dataset/test_read_video_dvpp.py
+++ tests/st/dataset/test_read_video_dvpp.py
@@ -95,7 +95,7 @@ def test_read_video_with_h264_pts():
     check_mindspore_data(mindspore_output, expected_output)
 
 
-@arg_mark(plat_marks=['platform_ascend910b'], level_mark='level0', card_mark='onecard', essential_mark='essential')
+@arg_mark(plat_marks=['platform_ascend910b'], level_mark='level1', card_mark='onecard', essential_mark='essential')
 def test_read_video_with_h264_sec():
     """
     Feature: read_video
@@ -145,7 +145,7 @@ def test_read_video_with_h265_pts():
     check_mindspore_data(mindspore_output, expected_output)
 
 
-@arg_mark(plat_marks=['platform_ascend910b'], level_mark='level0', card_mark='onecard', essential_mark='essential')
+@arg_mark(plat_marks=['platform_ascend910b'], level_mark='level1', card_mark='onecard', essential_mark='essential')
 def test_read_video_with_h265_sec():
     """
     Feature: read_video
