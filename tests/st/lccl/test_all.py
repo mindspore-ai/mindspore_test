5350e13991220571a315b5db85a7340f0c3fc72b
yanghaoran <yanghaoran2@huawei.com>
2025-06-09 17:20:34 +0800

    optimize unregistered network testcases

--- tests/st/lccl/test_all.py
+++ tests/st/lccl/test_all.py
@@ -18,7 +18,7 @@ from mindspore import context
 from tests.mark_utils import arg_mark
 
 
-@arg_mark(plat_marks=['platform_ascend910b'], level_mark='level0', card_mark='allcards', essential_mark='essential')
+@arg_mark(plat_marks=['platform_ascend910b'], level_mark='level1', card_mark='allcards', essential_mark='essential')
 def test_lccl_allreduce():
     """
     Feature: lccl operator test.
