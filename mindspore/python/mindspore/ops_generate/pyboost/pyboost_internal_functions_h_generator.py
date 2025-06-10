7809bf4da92158f654ea42268991e9029dca7cba
linux <noodles.liuxu@huawei.com>
2025-06-10 10:37:05 +0800

    fix ops generate for internal op in pyboost

--- mindspore/python/mindspore/ops_generate/pyboost/pyboost_internal_functions_h_generator.py
+++ mindspore/python/mindspore/ops_generate/pyboost/pyboost_internal_functions_h_generator.py
@@ -28,7 +28,6 @@ from common.template import Template
 from common.gen_utils import save_file
 from common.base_generator import BaseGenerator
 
-from .pyboost_utils import is_optional_param
 from .op_template_parser import OpTemplateParser
 
 
@@ -63,7 +62,8 @@ class PyboostInternalFunctionsHeaderGenerator(BaseGenerator):
             if getattr(op_proto.op_dispatch, 'internal_op_ascend') == 'None':
                 continue
             operator_name = op_proto.op_name
-            call_args_with_types = self.get_call_args_with_type(op_proto)
+            op_parser = OpTemplateParser(op_proto)
+            call_args_with_types = op_parser.parse_call_args_with_types(is_convert=True)
             func_list.append(self.pyboost_internal_func_template.replace(operator_name=operator_name,
                                                                          call_args_with_type=call_args_with_types))
 
@@ -74,56 +74,3 @@ class PyboostInternalFunctionsHeaderGenerator(BaseGenerator):
         save_path = os.path.join(work_path, K.MS_PYBOOST_INTERNAL_FUNCTIONS_AUTO_GEN_PATH)
         file_name = "functions.h"
         save_file(save_path, file_name, pyboost_internal_func_h_str)
-
-
-    def _parse_call_args_types(self, op_args):
-        call_args_types = []
-        for op_arg in op_args:
-            is_optional = is_optional_param(op_arg)
-            if op_arg.is_type_id:
-                call_args_types.append('TypeId')
-                continue
-            call_args_types.append(self._get_convert_dtype(op_arg.arg_dtype, is_optional))
-        return call_args_types
-
-    def _get_convert_dtype(self, arg_dtype, is_optional=False):
-        """convert dtype to mindspore type"""
-        type_convert = {
-            'int': 'int64_t',
-            'float': 'float',
-            'bool': 'bool',
-            'number': 'mindspore::ScalarPtr',
-            'str': 'string',
-            'tensor': 'mindspore::tensor::BaseTensorPtr',
-            'tuple[int]': 'std::vector<int64_t>',
-            'tuple[float]': 'std::vector<float>',
-            'tuple[bool]': 'std::vector<bool>',
-            'tuple[tensor]': 'std::vector<BaseTensorPtr>',
-            'list[int]': 'std::vector<int64_t>',
-            'list[float]': 'std::vector<float>',
-            'list[bool]': 'std::vector<bool>',
-            'list[tensor]': 'std::vector<BaseTensorPtr>',
-        }
-
-        optional_tensor_type_convert = {
-            'tensor': 'std::optional<mindspore::tensor::BaseTensorPtr>',
-            'tuple[tensor]': 'std::optional<mindspore::tensor::BaseTensorPtr>',
-            'list[tensor]': 'std::optional<mindspore::tensor::BaseTensorPtr>'
-        }
-
-        if is_optional and arg_dtype in optional_tensor_type_convert:
-            return optional_tensor_type_convert[arg_dtype]
-
-        if arg_dtype in type_convert:
-            return type_convert[arg_dtype]
-        raise TypeError(f"""Unsupported dtype {arg_dtype} for args.""")
-
-    def get_call_args_with_type(self, op_proto):
-        """Get call args with cpp type according to op proto"""
-        op_parser = OpTemplateParser(op_proto)
-        call_args_after_convert, _, _ = op_parser.op_args_converter()
-        call_args_type = self._parse_call_args_types(op_proto.op_args)
-        call_args_with_types = []
-        for call_arg, arg_dtype in zip(call_args_after_convert, call_args_type):
-            call_args_with_types.append("const " + arg_dtype + " &" + call_arg)
-        return call_args_with_types
