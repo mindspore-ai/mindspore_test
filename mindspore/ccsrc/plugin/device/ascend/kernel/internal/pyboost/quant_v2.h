1b814e4874c185aa572c3e71d577b8b1023f64d9
fanjibin <fanjibin@huawei.com>
2025-06-11 11:20:28 +0800

    internal quant_v2 support pyboost

---
+++ mindspore/ccsrc/plugin/device/ascend/kernel/internal/pyboost/quant_v2.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2025 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_INTERNAL_PYBOOST_QUANT_V2_H_
+#define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_INTERNAL_PYBOOST_QUANT_V2_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <utility>
+
+#include "plugin/device/ascend/kernel/internal/pyboost/internal_kernel_info.h"
+
+namespace mindspore {
+namespace kernel {
+class QuantV2 : public InternalKernelInfo {
+ public:
+  QuantV2() : InternalKernelInfo(std::move("QuantPerChannel")) {}
+  ~QuantV2() = default;
+
+  void Call(const std::shared_ptr<pyboost::OpRunner> &op, const BaseTensorPtr &x, const BaseTensorPtr &scale,
+            const BaseTensorPtr &offset, const bool sqrt_mode, const int64_t rounding_mode, const int64_t dst_type);
+
+ protected:
+  internal::InternalOpPtr CreateKernel(const internal::InputsImmutableInfoList &inputs,
+                                       const internal::OutputsImmutableInfoList &outputs) override;
+};
+}  // namespace kernel
+}  // namespace mindspore
+#endif  // MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_INTERNAL_PYBOOST_QUANT_V2_H_
