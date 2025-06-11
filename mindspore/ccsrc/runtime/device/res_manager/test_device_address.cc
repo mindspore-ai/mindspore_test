ebafccbeb20e64a2861d73086cd5e98ba2a26d72
jiaxueyu <jiaxueyu@huawei.com>
2025-06-11 18:41:35 +0800

    fix ut not register device address maker[Fix Conflict]

---
+++ mindspore/ccsrc/runtime/device/res_manager/test_device_address.cc
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2025 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "runtime/device/res_manager/test_device_address.h"
+#include "ir/device_address_maker.h"
+
+namespace mindspore {
+namespace runtime {
+namespace test {
+namespace {
+DeviceSyncPtr MakeTestDeviceAddress(TypeId data_type, const ShapeVector &shape, void *data_ptr,
+                                    DeviceAddressDeleter &&deleter) {
+  auto context = MsContext::GetInstance();
+  MS_EXCEPTION_IF_NULL(context);
+  auto device_id = context->get_param<uint32_t>(MS_CTX_DEVICE_ID);
+  auto data_size = SizeOf(shape) * abstract::TypeIdSize(data_type);
+  auto device_context = device::DeviceContextManager::GetInstance().GetOrCreateDeviceContext({"CPU", device_id});
+  MS_EXCEPTION_IF_NULL(device_context);
+  MS_EXCEPTION_IF_NULL(device_context->device_res_manager_);
+  auto device_address = device_context->device_res_manager_->CreateDeviceAddress(
+    data_ptr, data_size, shape, Format::DEFAULT_FORMAT, data_type, "CPU", device_id, 0);
+  device_address->SetPointerRefCountDeleter(std::move(deleter));
+  return device_address;
+}
+
+const char device_name[] = "CPU";
+MS_REGISTER_DEVICE(device_name, TestDeviceContext);
+REGISTER_DEVICE_ADDRESS_MAKER(device::DeviceType::kCPU, [](TypeId data_type, const ShapeVector &shape, void *data_ptr,
+                                                           DeviceAddressDeleter &&deleter) {
+  return MakeTestDeviceAddress(data_type, shape, data_ptr, std::move(deleter));
+});
+}  // namespace
+
+MS_REGISTER_HAL_COPY_FUNC(
+  DeviceType::kCPU, ([](const DeviceSyncPtr &dst_device_sync, const DeviceSyncPtr &src_device_sync, size_t stream_id) {
+    auto context = MsContext::GetInstance();
+    MS_EXCEPTION_IF_NULL(context);
+    auto device_id = context->get_param<uint32_t>(MS_CTX_DEVICE_ID);
+    device::ResKey res_key{DeviceType::kCPU, device_id};
+    auto res_manager = device::HalResManager::GetInstance().GetOrCreateResManager(res_key);
+    MS_EXCEPTION_IF_NULL(res_manager);
+    return res_manager->SyncCopy(dst_device_sync, src_device_sync, stream_id);
+  }),
+  ([](const DeviceSyncPtr &dst_device_sync, const DeviceSyncPtr &src_device_sync, size_t stream_id) {
+    auto context = MsContext::GetInstance();
+    MS_EXCEPTION_IF_NULL(context);
+    auto device_id = context->get_param<uint32_t>(MS_CTX_DEVICE_ID);
+    device::ResKey res_key{DeviceType::kCPU, device_id};
+    auto res_manager = device::HalResManager::GetInstance().GetOrCreateResManager(res_key);
+    MS_EXCEPTION_IF_NULL(res_manager);
+    return res_manager->SyncCopy(dst_device_sync, src_device_sync, stream_id);
+  }));
+
+MS_REGISTER_HAL_RES_MANAGER(kCPUDevice, DeviceType::kCPU, TestResManager);
+}  // namespace test
+}  // namespace runtime
+}  // namespace mindspore
