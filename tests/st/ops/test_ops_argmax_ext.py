5654c5384260834e8559f90ab7c5ffaca2be6d84
zhunaipan <zhunaipan@huawei.com>
2025-06-12 14:31:39 +0800

    change st level0 to level1 according to the success rate for optimizing the CI execution performance

--- tests/st/ops/test_ops_argmax_ext.py
+++ tests/st/ops/test_ops_argmax_ext.py
@@ -53,7 +53,7 @@ def GenInputData(np_data_type, shape=(3, 4, 5)):
     return Tensor(data)
 
 
-@arg_mark(plat_marks=['platform_ascend'], level_mark='level0', card_mark='onecard', essential_mark='essential')
+@arg_mark(plat_marks=['platform_ascend'], level_mark='level1', card_mark='onecard', essential_mark='essential')
 @pytest.mark.parametrize("context_mode", [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
 def test_ops_argmax_ext_normal(context_mode):
     """
