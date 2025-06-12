9aee7d6cfc08f12be62fbcade346e7e21f928ae1
fanjibin <fanjibin@huawei.com>
2025-06-12 09:58:01 +0800

    internal quant_batchmatmul support pyboost

--- mindspore/ccsrc/plugin/device/ascend/kernel/internal/pyboost/quant_batch_matmul.cc
+++ mindspore/ccsrc/plugin/device/ascend/kernel/internal/pyboost/quant_batch_matmul.cc
@@ -17,6 +17,7 @@
 #include "plugin/device/ascend/kernel/internal/pyboost/quant_batch_matmul.h"
 #include "include/runtime/hardware_abstract/kernel_base/kernel.h"
 
+
 namespace mindspore {
 namespace kernel {
 internal::InternalOpPtr QuantBatchMatmul::CreateKernel(const internal::InputsImmutableInfoList &inputs,
