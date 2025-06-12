57d184f6957c77f656e9bde743d022b0eadb7e60
fanjibin <fanjibin@huawei.com>
2025-06-12 09:58:01 +0800

    internal quant_batchmatmul support pyboost

---
+++ mindspore/ccsrc/plugin/device/ascend/kernel/internal/pyboost/quant_batch_matmul.cc
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2025 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "plugin/device/ascend/kernel/internal/pyboost/quant_batch_matmul.h"
+#include "include/runtime/hardware_abstract/kernel_base/kernel.h"
+
+namespace mindspore {
+namespace kernel {
+internal::InternalOpPtr QuantBatchMatmul::CreateKernel(const internal::InputsImmutableInfoList &inputs,
+                                                       const internal::OutputsImmutableInfoList &outputs) {
+  output_format_ = outputs[0].GetFormat();
+  return internal::CreateMatmulOp(inputs, outputs, param_, internal::kInternalMatMulOpName);
+}
+
+uint64_t QuantBatchMatmul::GetOrGenerateOpTilingKey(const uint64_t &tiling_key) const {
+  return CalcInternalOpTilingHash(kernel_name_, tiling_key, output_format_, param_.transpose_a, param_.transpose_b);
+}
+
+void QuantBatchMatmul::Call(const std::shared_ptr<pyboost::OpRunner> &op, const uint64_t &op_key,
+                            const uint64_t &tiling_key, const TensorPtr &x, const TensorPtr &y, const TensorPtr &scale,
+                            const std::optional<TensorPtr> &offset, const std::optional<TensorPtr> &bias,
+                            const std::optional<TensorPtr> &pertoken_scale, const bool transpose_a,
+                            const bool transpose_b, const int64_t dtype) {
+  TensorPtrList inputs = {x, y, bias.has_value() ? bias.value() : nullptr, scale};
+  TensorPtrList outputs = op->outputs();
+  param_.transpose_a = transpose_a;
+  param_.transpose_b = transpose_b;
+  param_.with_bias = bias.has_value();
+  param_.enable_shuffle = false;
+  param_.enable_dequant = true;
+  TransInternalShapes(inputs, outputs);
+
+  GetOrCreateKernel(op, op_key, tiling_key, inputs, outputs);
+  LAUNCH_INTERNAL(kernel_name_, op, internal_op_, inputs, outputs, tiling_info_);
+}
+MS_INTERNAL_KERNEL_INFO_FACTORY_REG(QuantBatchMatmul, QuantBatchMatmul);
+}  // namespace kernel
+}  // namespace mindspore
