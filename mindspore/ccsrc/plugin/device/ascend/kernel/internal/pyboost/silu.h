1ef9dd8d41423d6e20bbc5435c4a3366367b8f06
fanjibin <fanjibin@huawei.com>
2025-06-12 15:40:23 +0800

    support internal silu pyboost

---
+++ mindspore/ccsrc/plugin/device/ascend/kernel/internal/pyboost/silu.h
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2025 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_INTERNAL_PYBOOST_SILU_H_
+#define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_INTERNAL_PYBOOST_SILU_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <utility>
+
+#include "plugin/device/ascend/kernel/internal/pyboost/internal_kernel_info.h"
+
+namespace mindspore {
+namespace kernel {
+class SiLU : public InternalKernelInfo {
+ public:
+  SiLU() : InternalKernelInfo(std::move(internal::kInternalSwishOpName)) {}
+  ~SiLU() = default;
+
+  void Call(const std::shared_ptr<pyboost::OpRunner> &op, const BaseTensorPtr &x);
+
+ protected:
+  internal::InternalOpPtr CreateKernel(const internal::InputsImmutableInfoList &inputs,
+                                       const internal::OutputsImmutableInfoList &outputs) override;
+};
+}  // namespace kernel
+}  // namespace mindspore
+#endif  // MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_INTERNAL_PYBOOST_SILU_H_
