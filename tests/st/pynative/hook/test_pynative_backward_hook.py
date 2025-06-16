ef69a4d0cee5798692b3222812cf63bd281d6c4d
shuqian0 <154421897@qq.com>
2025-06-16 14:20:49 +0800

    fix test case

--- tests/st/pynative/hook/test_pynative_backward_hook.py
+++ tests/st/pynative/hook/test_pynative_backward_hook.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+import pytest
 import numpy as np
 import mindspore as ms
 from mindspore import nn, ops, Tensor, context
