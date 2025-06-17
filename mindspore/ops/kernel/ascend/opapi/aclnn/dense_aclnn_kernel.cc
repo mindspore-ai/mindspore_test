69662eeda063f5ef993c77c4a0cd92e297b4f865
hw_hz <zhanghao491@huawei.com>
2025-06-17 15:57:56 +0800

    refactor of dense aclnn kernel mod

--- mindspore/ops/kernel/ascend/opapi/aclnn/dense_aclnn_kernel.cc
+++ mindspore/ops/kernel/ascend/opapi/aclnn/dense_aclnn_kernel.cc
@@ -19,6 +19,8 @@
 #include <functional>
 #include "ir/tensor.h"
 #include "runtime/device/kernel_runtime.h"
+#include "mindspore/ops/view/transpose_ext_view_strides_calc.h"
+#include "kernel/ascend/opapi/aclnn/view/view_utils.h"
 
 namespace mindspore {
 namespace kernel {
@@ -41,55 +43,57 @@ void DenseAclnnKernelMod::SetFlatternNdLinearTensorStorageInfo(const KernelTenso
   new_tensor->set_tensor_storage_info(tensor_storage_info);
 }
 
+std::vector<int64_t> DenseAclnnKernelMod::TransposeWeight(const KernelTensor *w_tensor,
+                                                          const std::vector<int64_t> &w_shape,
+                                                          const KernelTensorPtr &w_t_tensor) {
+  ShapeVector w_t_shape{w_shape};
+  if (w_t_shape.size() >= kIndex2) {
+    // set shape
+    auto w_rank = w_shape.size();
+    w_t_shape[w_rank - kIndex1] = w_shape[w_rank - kIndex2];
+    w_t_shape[w_rank - kIndex2] = w_shape[w_rank - kIndex1];
+    w_t_tensor->SetShape(std::make_shared<abstract::TensorShape>(w_t_shape));
+    // calculate new storage infoF
+    ops::OldTensorInfoPtr old_info = GetOldTensorInfo(w_tensor);
+    auto new_info = ops::TransposeExtViewStridesCalc(old_info, -1, -2);
+    w_t_tensor->set_tensor_storage_info(new_info[0]);
+  }
+  return w_t_shape;
+}
+
 void DenseAclnnKernelMod::GetWorkSpaceInfo(const std::vector<KernelTensor *> &inputs,
                                            const std::vector<KernelTensor *> &outputs) {
   ClearOpsWorkSpaceList();
-  auto x_shape = inputs[kIndex0]->GetShapeVector();
-  auto w_shape = inputs[kIndex1]->GetShapeVector();
-
+  const auto &x_shape = inputs[kIndex0]->GetShapeVector();
+  const auto &w_shape = inputs[kIndex1]->GetShapeVector();
   auto w_rank = w_shape.size();
-  auto x_rank = x_shape.size();
-  // 1. Generate Transpose weight, the weight'rank greater than 0
-  ShapeVector w_t_shape = w_shape;
-  if (w_rank < kDim2) {
-    t_perm_.resize(kDim1);
-    t_perm_[0] = 0;
-  } else {
-    t_perm_.resize(w_rank);
-    // w_t modifies the last 2 dimensions, leaving the rest intact.
-    t_perm_[w_rank - kDim1] = w_rank - kDim2;
-    t_perm_[w_rank - kDim2] = w_rank - kDim1;
-    w_t_shape[w_rank - kDim1] = w_shape[w_rank - kDim2];
-    w_t_shape[w_rank - kDim2] = w_shape[w_rank - kDim1];
+  x_rank_ = x_shape.size();
 
-    for (size_t i = 0; i < w_rank - kDim2; ++i) {
-      t_perm_[i] = i;
-    }
-  }
+  // 1. Generate Transpose weight, the weight'rank greater than 1
+  w_t_tensor_ = inputs[kIndex1]->CloneKernelTensor();
+  auto w_t_shape = TransposeWeight(inputs[kIndex1], w_shape, w_t_tensor_);
+  auto w_t = w_t_tensor_.get();
 
-  auto w_t_ = &w_t_tensor_;
-  w_t_tensor_.SetType(inputs[kIndex1]->GetType());
-  w_t_tensor_.SetShape(std::make_shared<abstract::TensorShape>(w_t_shape));
-  GetWorkspaceForResizeTransform(inputs[kIndex1], t_perm_, &w_t_tensor_);
   // 2. Go through Different scenarios based on input'rank and bias'rank.
   auto bias = inputs[kIndex2];
+  is_bias_none_ = bias->GetType()->type_id() == kMetaTypeNone;
+
   cube_math_type_ = OpApiUtil::GetCubeMathType(OpApiUtil::IsAllowMatmulHF32());
 
-  MAKE_SCALAR(1, kNumberTypeInt64, one_);
-  if (bias->GetType()->type_id() == kMetaTypeNone) {
-    GetWorkspaceForResizeMatmul(inputs[kIndex0], w_t_, outputs[kIndex0], cube_math_type_);
+  if (is_bias_none_) {
+    GetWorkspaceForResizeMatmul(inputs[kIndex0], w_t, outputs[kIndex0], cube_math_type_);
   } else {
-    auto bias_shape = bias->GetShapeVector();
-    auto bias_rank = bias_shape.size();
+    const auto &bias_shape = bias->GetShapeVector();
+    bias_rank_ = bias_shape.size();
 
-    if (x_rank == kDim2) {
-      GetWorkspaceForResizeAddmm(inputs[kIndex2], inputs[kIndex0], w_t_, one_, one_, outputs[kIndex0], cube_math_type_);
-    } else if (bias_rank == kDim1 || x_rank == kDim3) {
+    if (x_rank_ == kIndex2) {
+      GetWorkspaceForResizeAddmm(inputs[kIndex2], inputs[kIndex0], w_t, one_, one_, outputs[kIndex0], cube_math_type_);
+    } else if (bias_rank_ == kIndex1 || x_rank_ == kIndex3) {
       input_kernel_tensor_ = inputs[kIndex0]->CloneKernelTensor();
       output_kernel_tensor_ = outputs[kIndex0]->CloneKernelTensor();
 
       int input_reshape_size = 1;
-      if (x_rank > kDim1) {
+      if (x_rank_ > kIndex1) {
         input_reshape_size = std::accumulate(x_shape.begin(), x_shape.end() - 1, 1, std::multiplies<int64_t>());
       }
       // Generate reshape Input 2D(Input_shape_i0 * Input_shape_i1 * ... Input_shape_ix-1, Input_shape_ix)
@@ -97,69 +101,56 @@ void DenseAclnnKernelMod::GetWorkSpaceInfo(const std::vector<KernelTensor *> &in
       // Generate reshape Output 2D(Input_shape_i0 * Input_shape_i1 * ... Input_shape_ix-1, W_t_shape_last)
       SetFlatternNdLinearTensorStorageInfo(output_kernel_tensor_, input_reshape_size, w_t_shape);
 
-      GetWorkspaceForResizeAddmm(inputs[kIndex2], input_kernel_tensor_.get(), w_t_, one_, one_,
+      GetWorkspaceForResizeAddmm(inputs[kIndex2], input_kernel_tensor_.get(), w_t, one_, one_,
                                  output_kernel_tensor_.get(), cube_math_type_);
     } else {
-      auto matmul_res_ = &matmul_tensor_;
-      matmul_tensor_.SetType(inputs[kIndex0]->GetType());
-      ShapeVector matmul_shape = x_shape;
-      matmul_shape[x_rank - 1] = w_t_shape[w_rank - 1];
+      ShapeVector matmul_shape{x_shape};
+      matmul_shape[x_rank_ - 1] = w_t_shape[w_rank - 1];
       // When x and w are 1D, a scalar tensor(0D) is generated.
-      if (x_rank == 1 && w_rank == 1) {
+      if (x_rank_ == 1 && w_rank == 1) {
         matmul_shape = {};
       }
+
+      matmul_tensor_.SetType(inputs[kIndex0]->GetType());
       matmul_tensor_.SetShape(std::make_shared<abstract::TensorShape>(matmul_shape));
 
-      GetWorkspaceForResizeMatmul(inputs[kIndex0], w_t_, &matmul_tensor_, cube_math_type_);
-      GetWorkspaceForResizeAdd(matmul_res_, bias, one_, outputs[kIndex0]);
+      GetWorkspaceForResizeMatmul(inputs[kIndex0], w_t, &matmul_tensor_, cube_math_type_);
+      GetWorkspaceForResizeAdd(&matmul_tensor_, bias, one_, outputs[kIndex0]);
 
       const auto &matmul_output_size =
         ops::CalOutputSize(matmul_tensor_.GetShapeVector(), mindspore::abstract::TypeIdSize(matmul_tensor_.dtype_id()));
       workspace_size_list_.emplace_back(matmul_output_size);
     }
   }
-  const auto &w_t_output_size =
-    ops::CalOutputSize(w_t_tensor_.GetShapeVector(), mindspore::abstract::TypeIdSize(w_t_tensor_.dtype_id()));
-
-  workspace_size_list_.emplace_back(w_t_output_size);
 }
 
 bool DenseAclnnKernelMod::Launch(const std::vector<KernelTensor *> &inputs,
                                  const std::vector<KernelTensor *> &workspace,
                                  const std::vector<KernelTensor *> &outputs, void *stream_ptr) {
   MS_EXCEPTION_IF_NULL(stream_ptr);
-  auto x_shape = inputs[kIndex0]->GetShapeVector();
-  auto x_rank = x_shape.size();
-  // 1. Generate Transpose weight, the weight'rank greater than 0
-  // The w_t_tensor always the last one
-  size_t workspace_offset = LongToSize(SizeToLong(workspace.size()) - SizeToLong(kIndex1));
-  w_t_tensor_.set_device_ptr(workspace[workspace_offset]->device_ptr());
-  auto w_t_ = &w_t_tensor_;
 
-  RunOpTransform(stream_ptr, workspace, inputs[kIndex1], t_perm_, &w_t_tensor_);
+  // 1. Generate Transpose weight, the weight'rank greater than 1
+  w_t_tensor_->set_device_ptr(inputs[kIndex1]->device_ptr());
+  auto w_t = w_t_tensor_.get();
+
   // 2. Go through Different scenarios based on input'rank and bias'rank.
-  auto bias = inputs[kIndex2];
-  if (bias->GetType()->type_id() == kMetaTypeNone) {
-    RunOpMatmul(stream_ptr, workspace, inputs[kIndex0], w_t_, outputs[kIndex0], cube_math_type_);
+  if (is_bias_none_) {
+    RunOpMatmul(stream_ptr, workspace, inputs[kIndex0], w_t, outputs[kIndex0], cube_math_type_);
   } else {
-    auto bias_shape = bias->GetShapeVector();
-    auto bias_rank = bias_shape.size();
-    if (x_rank == kDim2) {
-      RunOpAddmm(stream_ptr, workspace, inputs[kIndex2], inputs[kIndex0], w_t_, one_, one_, outputs[kIndex0],
+    if (x_rank_ == kIndex2) {
+      RunOpAddmm(stream_ptr, workspace, inputs[kIndex2], inputs[kIndex0], w_t, one_, one_, outputs[kIndex0],
                  cube_math_type_);
-    } else if (bias_rank == kDim1 || x_rank == kDim3) {
+    } else if (bias_rank_ == kIndex1 || x_rank_ == kIndex3) {
       input_kernel_tensor_->set_device_ptr(inputs[kIndex0]->device_ptr());
       output_kernel_tensor_->set_device_ptr(outputs[kIndex0]->device_ptr());
-
-      RunOpAddmm(stream_ptr, workspace, inputs[kIndex2], input_kernel_tensor_.get(), w_t_, one_, one_,
+      RunOpAddmm(stream_ptr, workspace, inputs[kIndex2], input_kernel_tensor_.get(), w_t, one_, one_,
                  output_kernel_tensor_.get(), cube_math_type_);
     } else {
       // If matmul_tensor_ has value, it's the penultimate one.
-      matmul_tensor_.set_device_ptr(workspace[workspace_offset - kIndex1]->device_ptr());
-      auto matmul_res_ = &matmul_tensor_;
-
-      RunOpMatmul(stream_ptr, workspace, inputs[kIndex0], w_t_, &matmul_tensor_, cube_math_type_);
-      RunOpAdd(stream_ptr, workspace, matmul_res_, bias, one_, outputs[kIndex0]);
+      auto bias = inputs[kIndex2];
+      matmul_tensor_.set_device_ptr(workspace[workspace.size() - kIndex1]->device_ptr());
+      RunOpMatmul(stream_ptr, workspace, inputs[kIndex0], w_t, &matmul_tensor_, cube_math_type_);
+      RunOpAdd(stream_ptr, workspace, &matmul_tensor_, bias, one_, outputs[kIndex0]);
     }
   }
   return true;
