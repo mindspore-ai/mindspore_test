fa9e245b474784d49ee239eafc5cc20fbc31dbb7
Zhichao <linzhichao2@huawei.com>
2025-06-18 20:42:29 +0800

    internal add asdop gather_pre_rms_norm

---
+++ mindspore/ops/infer/ops_func_impl/gather_pre_rms_norm.h
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2025 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_CORE_OPS_OPS_FUNC_IMPL_GATHER_PRE_RMS_NORM_H_
+#define MINDSPORE_CORE_OPS_OPS_FUNC_IMPL_GATHER_PRE_RMS_NORM_H_
+
+#include "ops/ops_func_impl/op_func_impl.h"
+
+namespace mindspore {
+namespace ops {
+enum GatherPreRmsNormInputIndex : size_t {
+  kGatherPreRmsNormInputXIndex = 0,
+  kGatherPreRmsNormInputResInIndex,
+  kGatherPreRmsNormInputIndicesIndex,
+  kGatherPreRmsNormInputGammaIndex,
+};
+
+class OPS_API GatherPreRmsNormFuncImpl : public OpFuncImpl {
+ public:
+  ShapeArray InferShape(const PrimitivePtr &primitive, const InferInfoPtrList &input_infos) const override;
+  TypeIdList InferType(const PrimitivePtr &primitive, const InferInfoPtrList &input_infos) const override;
+  bool GeneralInferRegistered() const override { return true; }
+
+ private:
+  void CheckInputs(const PrimitivePtr &primitive, const InferInfoPtrList &input_infos) const;
+  static constexpr size_t kInputRankSize1 = 1;
+  static constexpr size_t kInputRankSize2 = 2;
+};
+}  // namespace ops
+}  // namespace mindspore
+
+#endif  // MINDSPORE_CORE_OPS_OPS_FUNC_IMPL_GATHER_PRE_RMS_NORM_H_
