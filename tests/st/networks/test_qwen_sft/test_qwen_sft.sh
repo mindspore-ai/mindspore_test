19bcce3f0b5b6fb7fb801c01a1c08cf78edebf33
lby <liubuyu1@huawei.com>
2025-06-19 19:48:53 +0800

    Revert "update msadapter st"
    This reverts commit c5087b0d5b62854656e2a69ccc40b38c1df2882c.

--- tests/st/networks/test_qwen_sft/test_qwen_sft.sh
+++ tests/st/networks/test_qwen_sft/test_qwen_sft.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 export CUDA_DEVICE_MAX_CONNECTIONS=1
-export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
-export HCCL_CONNECT_TIMEOUT=3600
+export HCCL_IF_BASE_PORT=60050
 
-MindSpeed_LLM_PATH=../scripts/LLM/MindSpeed-LLM
+MindSpeed_LLM_PATH=../MindSpeed-Core-MS/MindSpeed-LLM
 
 NPUS_PER_NODE=8
 MASTER_PORT=6015
@@ -121,5 +120,4 @@ msrun $DISTRIBUTED_ARGS ${MindSpeed_LLM_PATH}/posttrain_gpt.py \
     $OUTPUT_ARGS \
     $TUNE_ARGS \
     --distributed-backend nccl \
-    --ai-framework mindspore \
     | tee qwen_sft.txt
