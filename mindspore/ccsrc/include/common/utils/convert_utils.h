072632996e466a8183edc98f21cc56e387648204
caifubi <caifubi1@huawei.com>
2025-06-21 14:48:25 +0800

    Fix ascend level1 st

--- mindspore/ccsrc/include/common/utils/convert_utils.h
+++ mindspore/ccsrc/include/common/utils/convert_utils.h
@@ -72,7 +72,8 @@ std::vector<T> TensorValueToVector(const tensor::TensorPtr &tensor) {
   MS_EXCEPTION_IF_NULL(tensor);
   std::vector<T> value;
   size_t element_size = tensor->DataSize();
-  auto *data = static_cast<T *>(tensor->data_c());
+  auto cpu_tensor= tensor->cpu();
+  auto *data = static_cast<T *>(cpu_tensor->data_c());
   for (size_t i = 0; i < element_size; i++) {
     value.push_back(data[i]);
   }
