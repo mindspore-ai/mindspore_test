06dc0fb425d64cccb730cdd38e662a95304ae6f5
熊攀 <xiongpan17@huawei.com>
2025-06-27 02:53:31 +0800

    添加copy right

--- mindspore/lite/tools/kernel_builder/ascend/ascendc/cmake/util/parse_ini_to_json.py
+++ mindspore/lite/tools/kernel_builder/ascend/ascendc/cmake/util/parse_ini_to_json.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# copy from https://gitee.com/ascend/samples/blob/master/best_practices/contrib/blast-code/blast_tik/cmake/util/parse_ini_to_json.py
 
 """
 parser ini to json
