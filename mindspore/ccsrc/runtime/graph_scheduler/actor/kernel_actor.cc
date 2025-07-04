6065adf547ffa0ce9be86d66c4ff2593eca1b133
jiaxueyu <jiaxueyu@huawei.com>
2025-07-04 14:54:33 +0800

    copy input exception for inference
    add st parallel_paged_attention_mgr patch

--- mindspore/ccsrc/runtime/graph_scheduler/actor/kernel_actor.cc
+++ mindspore/ccsrc/runtime/graph_scheduler/actor/kernel_actor.cc
@@ -1051,7 +1051,7 @@ void KernelActor::SetMemInfoForRdr() {
 }
 
 void KernelActor::CopyInputDeviceTensor(KernelTensorPtr kernel_tensor, size_t input_index,
-                                        OpContext<KernelTensor> *const context, bool parallel_dispatch_param) {
+                                        OpContext<KernelTensor> *const context, bool inference_param) {
   // The ignored input address that is not used in the kernel launch and no need copy.
   MS_EXCEPTION_IF_NULL(kernel_tensor);
   auto device_tensor = kernel_tensor->device_address().get();
@@ -1076,7 +1076,7 @@ void KernelActor::CopyInputDeviceTensor(KernelTensorPtr kernel_tensor, size_t in
       device_tensor->type_id() == real_input_info->type_id_) {
     return;
   }
-  if (parallel_dispatch_param) {
+  if (inference_param) {
     MS_LOG(EXCEPTION) << GetAID().Name()
                       << " not support copy parameter input for parallel dispatch, input index: " << input_index;
   }
--- mindspore/ccsrc/runtime/graph_scheduler/actor/kernel_actor.h
+++ mindspore/ccsrc/runtime/graph_scheduler/actor/kernel_actor.h
@@ -291,7 +291,7 @@ class KernelActor : public DebugAwareActor {
   void FetchWorkspaceDeviceTensor();
   // Need copy when the data type or format between real parameters and formal parameters are inconsistent.
   void CopyInputDeviceTensor(KernelTensorPtr kernel_tensor, size_t input_index, OpContext<KernelTensor> *const context,
-                             bool parallel_dispatch_param);
+                             bool inference_param);
   // The processing before kernel launch: update the info of kernel launch.
   void PreLaunchKernel(OpContext<KernelTensor> *const context);
   // The processing after kernel launch: 1.erase input, 2.free memory, 3.send output.
