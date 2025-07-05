49a8833d8df19b816802ecaf4bcf28ccae7f138e
shenwei41 <shenwei41@huawei.com>
2025-07-05 11:18:29 +0800

    open source code rectification

--- mindspore/ccsrc/minddata/dataset/kernels/image/dvpp/utils/VdecHelper.h
+++ mindspore/ccsrc/minddata/dataset/kernels/image/dvpp/utils/VdecHelper.h
@@ -1,17 +1,14 @@
 /**
-* Copyright 2022 Huawei Technologies Co., Ltd
+# Adapted from
+# https://gitee.com/ascend/samples/blob/master/cplusplus/common/acllite/include/VdecHelper.h
 *
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-
-* http://www.apache.org/licenses/LICENSE-2.0
-
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+* @file VdecHelper.h
+*
+* Copyright (c) Huawei Technologies Co., Ltd. 2020-2022. All rights reserved.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 */
 #ifndef MINDSPORE_CCSRC_MINDDATA_DATASET_KERNELS_IMAGE_DVPP_UTILS_VDEC_HELPER_H_
 #define MINDSPORE_CCSRC_MINDDATA_DATASET_KERNELS_IMAGE_DVPP_UTILS_VDEC_HELPER_H_
