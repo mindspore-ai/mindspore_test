152c90a7366da72d1ef5e649f87601e87994d7e0
xiaopeng <xiaopeng52@huawei.com>
2025-07-14 18:07:29 +0800

    remainder_ testcase remove cpu

--- tests/st/tensor/overload/test_remainder_.py
+++ tests/st/tensor/overload/test_remainder_.py
@@ -153,7 +153,7 @@ def test_tensor_remainder__normal(mode):
 
 
 @arg_mark(
-    plat_marks=['cpu_linux', 'cpu_windows', 'cpu_macos', 'platform_gpu', 'platform_ascend', 'platform_ascend910b'],
+    plat_marks=['platform_gpu', 'platform_ascend', 'platform_ascend910b'],
     level_mark='level1',
     card_mark='onecard',
     essential_mark='unessential')
