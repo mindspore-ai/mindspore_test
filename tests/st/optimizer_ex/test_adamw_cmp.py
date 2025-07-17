739ca0a18ae0846a13f15af095ebf538e3308e2c
yide12 <wangyide10@huawei.com>
2025-07-17 17:17:39 +0800

    r2.7 remove_redundancy st port rectification

--- tests/st/optimizer_ex/test_adamw_cmp.py
+++ tests/st/optimizer_ex/test_adamw_cmp.py
@@ -173,7 +173,7 @@ def allclose_nparray(data_expected, data_me, rtol, atol, equal_nan=True):
         assert np.array(data_expected).shape == np.array(data_me).shape
 
 
-@arg_mark(plat_marks=['platform_gpu', 'platform_ascend'],
+@arg_mark(plat_marks=['platform_ascend'],
           level_mark='level0',
           card_mark='onecard',
           essential_mark='essential')
@@ -189,7 +189,7 @@ def test_adamw_basic(mode):
     fact.result_cmp()
 
 
-@arg_mark(plat_marks=['platform_gpu', 'platform_ascend'],
+@arg_mark(plat_marks=['platform_ascend'],
           level_mark='level1',
           card_mark='onecard',
           essential_mark='unessential')
@@ -205,7 +205,7 @@ def test_adamw_group(mode):
     fact.result_cmp()
 
 
-@arg_mark(plat_marks=['platform_gpu', 'platform_ascend'],
+@arg_mark(plat_marks=['platform_ascend'],
           level_mark='level1',
           card_mark='onecard',
           essential_mark='unessential')
@@ -221,7 +221,7 @@ def test_adamw_lr_dynamic(mode):
     fact.result_cmp()
 
 
-@arg_mark(plat_marks=['platform_gpu', 'platform_ascend'],
+@arg_mark(plat_marks=['platform_ascend'],
           level_mark='level1',
           card_mark='onecard',
           essential_mark='unessential')
@@ -237,7 +237,7 @@ def test_adamw_group_lr_dynamic(mode):
     fact.result_cmp()
 
 
-@arg_mark(plat_marks=['platform_gpu', 'platform_ascend'],
+@arg_mark(plat_marks=['platform_ascend'],
           level_mark='level1',
           card_mark='onecard',
           essential_mark='unessential')
