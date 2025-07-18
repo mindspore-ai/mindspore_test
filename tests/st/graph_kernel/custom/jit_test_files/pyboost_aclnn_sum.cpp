fa0ef0921705ee57849697480a3eba4834d745ce
dayschan <chendeshi@huawei.com>
2025-07-18 11:25:07 +0800

    aclnn op runner

--- tests/st/graph_kernel/custom/jit_test_files/pyboost_aclnn_sum.cpp
+++ tests/st/graph_kernel/custom/jit_test_files/pyboost_aclnn_sum.cpp
@@ -15,37 +15,12 @@
  */
 
 #include <set>
-#include <functional>
 #include <optional>
-#include "ms_extension/all.h"
+#include "ms_extension/api.h"
+#include "mindspore/ccsrc/pyboost/functions/auto_generate/functions.h"
 
-namespace ms::pynative {
-using AclnnLaunchFunc = std::function<void(mindspore::device::DeviceContext *, size_t)>;
-
-class InnerAclnnOpRunner : public PyboostRunner {
- public:
-  using PyboostRunner::PyboostRunner;
-  void SetLaunchFunc(AclnnLaunchFunc func) { launch_func_ = func; }
-
- protected:
-  using PyboostRunner::CalcWorkspace;
-  void LaunchKernel() override {
-    if (launch_func_ != nullptr) {
-      launch_func_(_device_context_, _stream_id_);
-    }
-  }
-  void _DispatchLaunchTask() override { LaunchKernel(); }
-  AclnnLaunchFunc launch_func_{nullptr};
-};
-
-#define LAUNCH_ACLNN_FUNC(aclnn_api, ...)                                \
-  [__VA_ARGS__](auto __device_context, auto __stream_id) {               \
-    LAUNCH_ACLNN(aclnn_api, __device_context, __stream_id, __VA_ARGS__); \
-  }
-}  // namespace ms::pynative
-
-namespace mindspore {
-ms::Tensor GenResultTensor(const ms::Tensor &t, const std::vector<int64_t> &axis, bool keepdims, TypeId type_id) {
+namespace custom {
+ms::Tensor GenResultTensor(const ms::Tensor &t, const std::vector<int64_t> &axis, bool keepdims, ms::TypeId type_id) {
   auto s = t.shape();
   size_t n = s.size();
   ShapeVector out_shape;
@@ -71,27 +46,19 @@ ms::Tensor GenResultTensor(const ms::Tensor &t, const std::vector<int64_t> &axis
   return ms::Tensor(type_id, out_shape);
 }
 
+using namespace mindspore;
+using namespace mindspore::device::ascend;
 ms::Tensor npu_abs_reduce_sum(const ms::Tensor &x, std::optional<std::vector<int64_t>> axis, bool keepdims,
                               std::optional<int64_t> dtype) {
-  auto type_id = dtype.has_value() ? static_cast<TypeId>(dtype.value()) : x.data_type();
+  auto type_id = dtype.has_value() ? static_cast<ms::TypeId>(dtype.value()) : x.data_type();
   std::vector<int64_t> axis_vec = axis.has_value() ? axis.value() : std::vector<int64_t>();
   auto result = GenResultTensor(x, axis_vec, keepdims, type_id);
-  auto result_t = result.tensor();
-
-  auto y = kernel::pyboost::abs(x.tensor());
-  auto runner = std::make_shared<ms::pynative::InnerAclnnOpRunner>("ReduceSum");
-  runner->SetLaunchFunc(LAUNCH_ACLNN_FUNC(aclnnReduceSum, y, axis_vec, keepdims, type_id, result_t));
-  runner->Run({ms::Tensor(y)}, {result});
+  auto y = ms::Tensor(mindspore::kernel::pyboost::abs(x.tensor()));
+  auto runner = std::make_shared<ms::pynative::AclnnOpRunner>("ReduceSum");
+  runner->SetLaunchFunc(LAUNCH_ACLNN_FUNC(aclnnReduceSum, y, axis_vec, keepdims, type_id, result));
+  runner->Run({y}, {result});
   return result;
 }
-}  // namespace mindspore
+}  // namespace custom
 
-auto pyboost_npu_abs_reduce_sum(const ms::Tensor &x, std::optional<std::vector<int64_t>> axis, bool keepdims,
-                                std::optional<int64_t> dtype) {
-  return ms::pynative::PyboostRunner::Call<1>(mindspore::npu_abs_reduce_sum, x, axis, keepdims, dtype);
-}
-
-PYBIND11_MODULE(MS_EXTENSION_NAME, m) {
-  m.def("npu_abs_reduce_sum", &pyboost_npu_abs_reduce_sum, "Abs and aclnnReduceSum", pybind11::arg("x"),
-        pybind11::arg("axis") = std::nullopt, pybind11::arg("keepdims") = false, pybind11::arg("dtype") = std::nullopt);
-}
+PYBIND11_MODULE(MS_EXTENSION_NAME, m) { m.def("npu_abs_reduce_sum", PYBOOST_CALLER(1, custom::npu_abs_reduce_sum)); }
