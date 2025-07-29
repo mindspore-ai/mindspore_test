5ba251d0d8070aac95e317fc4cf87860da0682ba
panzhihui <panzhihui3@huawei.com>
2025-07-29 16:39:06 +0800

    add op_plugin_utils.h

--- mindspore/ccsrc/plugin/device/cpu/kernel/custom/custom_op_plugin_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/custom/custom_op_plugin_kernel.h
@@ -28,8 +28,8 @@ namespace kernel {
 
 class CustomOpPluginCpuKernelMod : public NativeCpuKernelMod {
  public:
-  CustomOpPluginCpuKernelMod() : handle_(nullptr), reg_func_(nullptr), aot_func_(nullptr) {}
-  ~CustomOpPluginCpuKernelMod();
+  CustomOpPluginCpuKernelMod() = default;
+  ~CustomOpPluginCpuKernelMod() = default;
 
   bool Init(const std::vector<KernelTensor *> &inputs, const std::vector<KernelTensor *> &outputs) override;
   bool Launch(const std::vector<KernelTensor *> &inputs, const std::vector<KernelTensor *> &workspace,
@@ -46,9 +46,6 @@ class CustomOpPluginCpuKernelMod : public NativeCpuKernelMod {
 
   std::string file_path_;
   std::string func_name_;
-  void *handle_{nullptr};
-  bool (*reg_func_)(const char *);
-  int (*aot_func_)(int, void **, int *, int64_t **, const char **, void *, void *);
 
   KernelInputInfoImpl kernel_info_;
 
---
+++ mindspore/ccsrc/plugin/device/cpu/kernel/custom/op_plugin_utils.h
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2025 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_CUSTOM_OP_PLUGIN_UTILS_H_
+#define MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_CUSTOM_OP_PLUGIN_UTILS_H_
+
+#include <string>
+#include <vector>
+#include "plugin/device/cpu/kernel/custom/custom_kernel_input_info.h"
+
+namespace mindspore::kernel {
+struct OpPluginKernelParam {
+  std::vector<void *> params;
+  std::vector<int> ndims;
+  std::vector<int64_t *> shapes;
+  std::vector<std::string> dtype_strings;
+  std::vector<const char *> dtypes;
+  KernelInputInfoImpl kernel_info;
+  void *stream{nullptr};
+};
+
+void *GetOpPluginHandle();
+BACKEND_EXPORT bool IsOpPluginKernel(const std::string &op_name);
+BACKEND_EXPORT int LaunchOpPluginKernel(const std::string &op_name, size_t nparam, void **params, int *ndims,
+                                        int64_t **shapes, const char **type_pointer_list, void *kernel_info,
+                                        void *stream = nullptr);
+BACKEND_EXPORT int LaunchOpPluginKernel(const std::string &op_name, OpPluginKernelParam *param);
+BACKEND_EXPORT OpPluginKernelParam CreateOpPluginParam(const std::vector<KernelTensor *> &inputs,
+                                                       const std::vector<KernelTensor *> &outputs,
+                                                       const std::vector<KernelTensor *> &workspace);
+}  // namespace mindspore::kernel
+
+#endif  // MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_CUSTOM_OP_PLUGIN_UTILS_H_
