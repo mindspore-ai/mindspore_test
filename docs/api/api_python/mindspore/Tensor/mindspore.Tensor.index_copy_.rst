89541641e11f676772b03489d9fdfae90851720f
wang_ziqi <wangziqi4@huawei.com>
2025-08-04 11:10:20 +0800

    Add Tensor.index_copy_
    ------
    
    Signed-off-by: wang_ziqi <wangziqi4@huawei.com>

---
+++ docs/api/api_python/mindspore/Tensor/mindspore.Tensor.index_copy_.rst
@@ -0,0 +1,19 @@
+mindspore.Tensor.index_copy\_
+=============================
+
+.. py:method:: mindspore.Tensor.index_copy_(dim, index, tensor) -> Tensor
+
+    根据 `index` 中的索引顺序，将 `tensor` 的元素复制到 `self` 中。
+
+    .. note::
+        `index` 的值必须在 `[0, self.shape[dim])` 范围内，如果超出该范围，结果未定义。
+
+        如果 `index` 的值包含重复的索引，则结果是不确定的，因为它取决于最后发生的拷贝操作。
+
+    参数：
+        - **dim** (int) - 指定 `index` 属于哪个维度。
+        - **index** (Tensor) - 一维Tensor，其值为在 `self` 中沿指定的 `dim` 的索引。
+        - **tensor** (Tensor) - 包含待复制元素的tensor。
+
+    返回：
+        Tensor `self` 。
--- mindspore/ops/kernel/ascend/pyboost/customize/index_fill_tensor.cc
+++ mindspore/ops/kernel/ascend/pyboost/customize/index_fill_tensor.cc
@@ -79,12 +79,15 @@ tensor::TensorPtr IndexFillTensorAscendCustomize(const std::shared_ptr<OpRunner>
       PyBoostUtils::MallocOpInputs(device_context, input);
       // Malloc for output tensors
       PyBoostUtils::MallocOpOutputs(device_context, outputs);
-      MS_LOG(DEBUG) << "Run device task "
-                    << " start";
-      LAUNCH_ACLNN(aclnnIndexFillTensor, device_context, op->stream_id(), input, dim_imm, index_vector, value_scalar,
-                   outputs[0]);
-      MS_LOG(DEBUG) << "Run device task "
-                    << " end";
+      if (index_vector.empty() && input->DataSize() == 0) {
+        MS_LOG(DEBUG) << "aclnnIndexFillTensor throws 'CopyNpuToNpuOp' error when 'self' and 'index' are empty tensors,"
+                      << "because we allocated NPU memory for empty 'self' tensor. So we skip launching in this case.";
+      } else {
+        MS_LOG(DEBUG) << "Run IndexFillScalar device task start";
+        LAUNCH_ACLNN(aclnnIndexFillTensor, device_context, op->stream_id(), input, dim_imm, index_vector, value_scalar,
+                     outputs[0]);
+        MS_LOG(DEBUG) << "Run IndexFillScalardevice task end";
+      }
     }));
   return op->output(0);
 }
