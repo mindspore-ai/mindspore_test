9489c289164927e607c8c54c024571af7826fc3a
huilan li <lihuilan1@huawei.com>
2025-08-13 16:56:13 +0800

    Union hccl create_group

--- tests/st/auto_parallel/hccl_config.py
+++ tests/st/auto_parallel/hccl_config.py
@@ -13,16 +13,15 @@
 # limitations under the License.
 # ============================================================================
 
-import os
-
 import mindspore.nn as nn
 from mindspore import context
+from mindspore._c_expression import GroupOptions
 from mindspore.common import set_seed, lazy_inline
 from mindspore.nn.utils import no_init_parameters
 from mindspore.train import Model, CheckpointConfig, ModelCheckpoint, LossMonitor
-from mindspore.communication.management import init, get_rank
+from mindspore.communication.management import init, get_rank, create_group, destroy_group
 from tests.st.auto_parallel.utils.dataset_utils import FakeData
-from tests.st.auto_parallel.utils._utils import set_parallel_mode, clean_all_ckpt_files, clear_files_in_directory
+from tests.st.auto_parallel.utils._utils import set_parallel_mode, clean_all_ckpt_files
 
 context.set_context(mode=context.GRAPH_MODE)
 init()
@@ -84,12 +83,10 @@ def model_train(model, epoch, dataset, ckpt_path, ckpt_prefix, integral_save, re
     model.train(epoch=epoch, train_dataset=dataset, callbacks=[ckpt_callback, loss_cb], dataset_sink_mode=False)
 
 
-def semi_auto_dp2_mp2_pp2(cur_root_dir, remove_redundancy=False):
+def semi_auto_dp2_mp2_pp2(cur_root_dir, remove_redundancy=False, opt_config=None):
     # save ir graph
     ir_graph_path = f"{cur_root_dir}/graphs"
-    clear_files_in_directory(f"{ir_graph_path}/rank_{get_rank()}")
-    os.environ['MS_DEV_SAVE_GRAPHS'] = "2"
-    os.environ['MS_DEV_SAVE_GRAPHS_PATH'] = ir_graph_path
+    save_ir_graphs(ir_graph_path)
 
     # dataset
     set_seed(1)
@@ -105,7 +102,7 @@ def semi_auto_dp2_mp2_pp2(cur_root_dir, remove_redundancy=False):
     stra_ckpt_file = f"{cur_root_dir}/train_strategy.ckpt"
     pp_config = {"stages": 2}
     parallel_config = {"parallel_mode": "semi_auto", "data_strategy": "data_parallel", "pipeline_config": pp_config,
-                       "save_strategy_file": stra_ckpt_file}
+                       "save_strategy_file": stra_ckpt_file, "parallel_optimizer_config": opt_config}
     stage_config = {"_backbone.flatten": 0, "_backbone.layer1": 0, "_backbone.relu1": 0, "_backbone.layer2": 0,
                     "_backbone.relu2": 1, "_backbone.layer3": 1}
     parallel_model = create_pp_train_model(parallel_net, 4, stage_config, parallel_config, net_optim)
@@ -114,12 +111,58 @@ def semi_auto_dp2_mp2_pp2(cur_root_dir, remove_redundancy=False):
     ckpt_path = f"{cur_root_dir}/rank_{get_rank()}_ckpt"
     model_train(model=parallel_model, epoch=2, dataset=parallel_dataset, ckpt_path=ckpt_path,
                 ckpt_prefix="ckpt_parallel_wd", integral_save=False, remove_redundancy=remove_redundancy)
+    return parallel_model
 
 
-def test_hccl_config():
+def test_union_hccl_groups():
     """
-    Feature: hccl buffle size.
-    Description: customized value of specific groups.
+    Feature: Union hccl groups creation.
+    Description: If hccl groups has been created in python by create_group, it will not be created repeatedly in C++.
     Expectation: run success.
     """
-    semi_auto_dp2_mp2_pp2(cur_root_dir="./test_hccl_config")
+    group0 = "customed groups 0-1, 0"
+    options0 = GroupOptions()
+    options0.hccl_config = {}
+
+    group1 = "customed groups 0-1, 1"
+    options1 = GroupOptions()
+    options1.hccl_config = {"hccl_buffer_size": 400}
+
+    group2 = "customed groups 0-1, 2"
+    options2 = GroupOptions()
+    options2.hccl_config = {"hccl_buffer_size": 100}
+
+    group3 = "customed groups 0-1, 3"
+    options3 = GroupOptions()
+    options3.hccl_config = {"hccl_buffer_size": 200}
+
+    # case 1, create_group, destroy before parallel, create default
+    epoch = 0
+    rank_ids = [0, 1]
+    if get_rank() in rank_ids:
+        create_group(group0, rank_ids, options0)
+        destroy_group(group0)
+    semi_auto_dp2_mp2_pp2(cur_root_dir=f"./test_union_hccl_groups/run_{epoch}", opt_config=None)
+
+    # case 2, create_group, reused in parallel
+    epoch += 1
+    rank_ids = [0, 1]
+    if get_rank() in rank_ids:
+        create_group(group1, rank_ids, options1)
+        create_group(group2, rank_ids, options2)
+    semi_auto_dp2_mp2_pp2(cur_root_dir=f"./test_union_hccl_groups/run_{epoch}", opt_config=None)
+
+    # case 3, destroy group, create default
+    epoch += 1
+    rank_ids = [0, 1]
+    if get_rank() in rank_ids:
+        destroy_group(group1)
+        destroy_group(group2)
+    semi_auto_dp2_mp2_pp2(cur_root_dir=f"./test_union_hccl_groups/run_{epoch}", opt_config=None)
+
+    # case 4, create group again, reused in parallel
+    epoch += 1
+    rank_ids = [0, 1]
+    if get_rank() in rank_ids:
+        create_group(group3, rank_ids, options3)
+    semi_auto_dp2_mp2_pp2(cur_root_dir=f"./test_union_hccl_groups/run_{epoch}", opt_config=None)
