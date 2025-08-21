5ea411af959316ad030fd54fe97b5d8d3d0e10cb
yuanqi <yuanqi13@huawei.com>
2025-08-21 19:53:23 +0800

    fix for lite

--- mindspore/core/include/ir/anf.h
+++ mindspore/core/include/ir/anf.h
@@ -32,7 +32,7 @@
 #include "base/base.h"
 #include "base/effect_info.h"
 #include "ir/kernel_info_dev.h"
-
+#include "ir/scope.h"
 #include "ir/primal_attr.h"
 #include "ir/primal_debug_info.h"
 #include "utils/hashing.h"
