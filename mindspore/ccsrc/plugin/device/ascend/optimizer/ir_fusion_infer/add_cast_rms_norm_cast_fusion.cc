4f25e4c15417694698dee06ab9dc9000456cacef
liudongxu <liudongxu5@huawei.com>
2025-08-22 11:03:05 +0800

    support matmul-split/matmul-add-split/matmul-addext-split/addext-rms-norm fusion ir

--- mindspore/ccsrc/plugin/device/ascend/optimizer/ir_fusion_infer/add_cast_rms_norm_cast_fusion.cc
+++ mindspore/ccsrc/plugin/device/ascend/optimizer/ir_fusion_infer/add_cast_rms_norm_cast_fusion.cc
@@ -33,7 +33,7 @@
 namespace mindspore {
 namespace opt {
 std::vector<std::string> AddCastRmsNormCastFusion::MustExistPrimitiveName() const {
-  std::vector<std::string> ret{prim::kPrimRmsNorm->name(), prim::kPrimCast->name(), prim::kPrimAdd->name()};
+  std::vector<std::string> ret = {prim::kPrimRmsNorm->name(), prim::kPrimCast->name()};
   return ret;
 }
 
