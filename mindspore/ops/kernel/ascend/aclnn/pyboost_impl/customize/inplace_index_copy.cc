f048172fd6a04f64a5514e6eaa869659e9860e76
wang_ziqi <wangziqi4@huawei.com>
2025-08-25 14:42:54 +0800

    rebase master
    ------
    
    Signed-off-by: wang_ziqi <wangziqi4@huawei.com>

--- mindspore/ops/kernel/ascend/aclnn/pyboost_impl/customize/inplace_index_copy.cc
+++ mindspore/ops/kernel/ascend/aclnn/pyboost_impl/customize/inplace_index_copy.cc
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-#include "kernel/ascend/pyboost/customize/inplace_index_copy.h"
-#include "plugin/res_manager/ascend/stream_manager/ascend_stream_manager.h"
+#include "kernel/ascend/aclnn/pyboost_impl/customize/inplace_index_copy.h"
+#include "plugin/ascend/res_manager/stream_manager/ascend_stream_manager.h"
 #include "mindspore/ccsrc/pyboost/pyboost_utils.h"
-#include "kernel/ascend/pyboost/aclnn_utils.h"
+#include "kernel/ascend/aclnn/pyboost_impl/aclnn_utils.h"
 
 namespace mindspore {
 namespace kernel {
