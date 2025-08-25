27cfc43d6f1010035e41b3cc6de1b091c9175d66
fuqiaolong <fuqiaolong1@huawei.com>
2025-08-25 16:21:47 +0800

    1. enable JIT_ENABLE_AUGASSIGN_INPLACE 2. fix fallback error

--- tests/st/compiler/augassign_inplace/test_augassign_backend.py
+++ tests/st/compiler/augassign_inplace/test_augassign_backend.py
@@ -16,26 +16,10 @@
 import numpy as np
 import mindspore as ms
 import mindspore.nn as nn
-from mindspore import context, Tensor, jit
+from mindspore import Tensor, jit
 from mindspore import Parameter
 from mindspore.common import dtype as mstype
-from mindspore._extends.parse import compile_config
 from tests.mark_utils import arg_mark
-
-
-class Net(nn.Cell):
-    def __init__(self):
-        super().__init__()
-        self.param_zero = Parameter(Tensor(0, mstype.float32), name='zero')
-        self.param_a = Parameter(Tensor(15, mstype.float32), name='a')
-
-    def construct(self):
-        out0 = self.param_zero
-        out1 = self.param_a
-
-        out1 += self.param_a
-        out0 += self.param_a
-        return out0, out1
 
 
 @arg_mark(plat_marks=['platform_ascend'], level_mark='level0', card_mark='onecard', essential_mark='essential')
@@ -45,25 +29,33 @@ def test_augassign_backend():
     Description: Support augassign inplace in kbk mode.
     Expectation: Run success.
     """
-    compile_config.JIT_ENABLE_AUGASSIGN_INPLACE = '1'
+
+    class Net(nn.Cell):
+        def __init__(self):
+            super().__init__()
+            self.param_zero = Parameter(Tensor(0, mstype.float32), name='zero')
+            self.param_a = Parameter(Tensor(15, mstype.float32), name='a')
+
+        def construct(self):
+            out0 = self.param_zero
+            out1 = self.param_a
+
+            out1 += self.param_a
+            out0 += self.param_a
+            return out0, out1
+
+    pynative_output = Net()()
 
     net0 = Net()
     net0.construct = jit(net0.construct, backend='GE')
     graph_output_ge = net0()
-    assert graph_output_ge[0] == Tensor(15, mstype.float32)
-    assert graph_output_ge[1] == Tensor(30, mstype.float32)
 
     net1 = Net()
     net1.construct = jit(net1.construct, backend='ms_backend')
     graph_output = net1()
-    assert graph_output[0] == Tensor(30, mstype.float32)
-    assert graph_output[1] == Tensor(30, mstype.float32)
 
-    context.set_context(mode=context.PYNATIVE_MODE)
-    pynative_output = Net()()
+    assert graph_output_ge == pynative_output
     assert graph_output == pynative_output
-
-    compile_config.JIT_ENABLE_AUGASSIGN_INPLACE = '0'
 
 
 @arg_mark(plat_marks=['platform_ascend'], level_mark='level0', card_mark='onecard', essential_mark='essential')
@@ -73,8 +65,6 @@ def test_initial_scalar_body_tensor1():
     Description: Test scalar arg when first entry of while and set to tensor in body.
     Expectation: No exception in infer process.
     """
-
-    compile_config.JIT_ENABLE_AUGASSIGN_INPLACE = '1'
 
     def func(x, a, b):
         y = 1
@@ -100,4 +90,3 @@ def test_initial_scalar_body_tensor1():
     input_me_a = Tensor(2, ms.float32)
     input_me_b = Tensor(6, ms.float32)
     test_net(input_me_x, input_me_a, input_me_b)
-    compile_config.JIT_ENABLE_AUGASSIGN_INPLACE = '0'
