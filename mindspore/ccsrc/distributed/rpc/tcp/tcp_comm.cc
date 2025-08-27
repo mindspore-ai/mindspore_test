10dee7d74337b5b796c8484517ee785cacc74239
ZPaC <zhoupeichen@huawei.com>
2025-08-27 18:01:55 +0800

    Delete warning log for sglang

--- mindspore/ccsrc/distributed/rpc/tcp/tcp_comm.cc
+++ mindspore/ccsrc/distributed/rpc/tcp/tcp_comm.cc
@@ -76,8 +76,8 @@ void ConnectedEventHandler(int fd, uint32_t events, void *context) {
   }
 
   if (!conn->enable_ssl) {
-    MS_LOG(WARNING) << "Connection from " << conn->source << " to " << conn->destination
-                    << " is successfully created. System errno: " << strerror(errno);
+    MS_LOG(INFO) << "Connection from " << conn->source << " to " << conn->destination
+                 << " is successfully created. System errno: " << strerror(errno);
     conn->socket_operation->ConnEstablishedEventHandler(fd, events, context);
   }
   return;
--- mindspore/ccsrc/plugin/res_manager/ascend/hccl_adapter/hccl_adapter.cc
+++ mindspore/ccsrc/plugin/res_manager/ascend/hccl_adapter/hccl_adapter.cc
@@ -261,12 +261,7 @@ bool HcclAdapter::InitHccl(uint32_t device_id, std::string_view rank_id) {
     return true;
   }
   InitPlugin();
-  auto options = GenHcclOptions(device_id, rank_id);
-  bool ret = InitKernelInfoStore(options);
-  if (!ret) {
-    return false;
-  }
-  ret = InitHcclExec();
+  bool ret = InitHcclExec();
   if (!ret) {
     return false;
   }
