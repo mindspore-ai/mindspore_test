357c805454073875da2060c0d7663ce2b52557ce
gaoyong10 <gaoyong10@huawei.com>
2025-08-29 11:31:44 +0800

    Fix ref output for any type graph.

--- mindspore/ccsrc/runtime/core/graph_scheduler/control_flow/control_node_parser.cc
+++ mindspore/ccsrc/runtime/core/graph_scheduler/control_flow/control_node_parser.cc
@@ -1964,7 +1964,7 @@ void CollectDeviceContextByDynamicLen(const CNodePtr &cnode, const FuncGraphPtr 
     MS_LOG(EXCEPTION) << "Invalid parameter context size:" << parameter_contexts.size()
                       << " parameter size:" << para_num;
   }
-  for (size_t i = para_num - arg_num; i < para_num; ++i) {
+  for (size_t i = 0; i < arg_num; ++i) {
     size_t output_num = common::AnfAlgo::GetOutputNumByAbstract(cnode->input(i + 1)->abstract());
     for (size_t j = 0; j < output_num; ++j) {
       arg_context->emplace_back(parameter_contexts[0]);
