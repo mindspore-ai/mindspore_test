1903e1ada389ef6758c3e26d77aa4173351cc2e1
changzherui <changzherui1@huawei.com>
2025-09-12 11:11:12 +0800

    fix log

--- mindspore/core/utils/log_adapter.cc
+++ mindspore/core/utils/log_adapter.cc
@@ -834,6 +834,7 @@ const std::string GetSubModuleName(SubModuleId module_id) {
     "KERNEL",             // SM_KERNEL
     "MD",                 // SM_MD
     "ME",                 // SM_ME
+    "EXPRESS",            // SM_EXPRESS
     "OPTIMIZER",          // SM_OPTIMIZER
     "PARALLEL",           // SM_PARALLEL
     "PARSER",             // SM_PARSER
