70f9c7ba108b91118931aea5a3d627b7a8ddde26
shen_haochen <shenhaochen@huawei.com>
2025-09-26 11:18:53 +0800

    refactory thread bind core test case

--- tests/st/runtime/bind_core/run_thread_bind_core.py
+++ tests/st/runtime/bind_core/run_thread_bind_core.py
@@ -12,20 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+import os
+import json
 import numpy as np
 import mindspore as ms
 import mindspore.nn as nn
 import mindspore.ops as P
 from mindspore import Tensor, jit
+from mindspore import dtype as mstype
 from mindspore.common import Parameter
 
 steps = 30
 
 ms.set_context(mode=ms.PYNATIVE_MODE)
 
-affinity_cpu_list = ["0-10", "21-30"]
-module_to_cpu_dict = {"main": [0, 1, 2, 3], "minddata": [4, 5], "other": [6, 7],
-                      "runtime": [8, 9], "pynative": [10, 11, 21, 100]}
+def _get_env_with_json(env_name, default):
+    value = os.environ.get(env_name, default)
+    if value:
+        try:
+            return json.loads(value)
+        except json.JSONDecodeError:
+            return value
+    return default
+
+affinity_cpu_list = _get_env_with_json('AFFINITY_CPU_LIST', None)
+module_to_cpu_dict = _get_env_with_json('MODULE_TO_CPU_DICT', None)
+
 ms.runtime.set_cpu_affinity(True, affinity_cpu_list, module_to_cpu_dict)
 
 class Net(nn.Cell):
@@ -47,6 +59,8 @@ class Net(nn.Cell):
 
 base_shape = (2, 3)
 net = Net()
+dyn_input_data = Tensor(shape=[2, None], dtype=mstype.float32)
+net.set_inputs(dyn_input_data)
 
 for i in range(steps):
     input_data = Tensor(np.full(base_shape, i).astype(np.float32))
