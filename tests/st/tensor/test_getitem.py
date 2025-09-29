1947420b482c25e209f4f90e7fe35601e24e5136
lizhitong <lizhitong5@huawei.com>
2025-09-29 11:00:50 +0800

    supplementary_test_case_of_tensor_index

--- tests/st/tensor/test_getitem.py
+++ tests/st/tensor/test_getitem.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+"""test case of tensor index getitem"""
 
 import os
 import numpy as np
@@ -34,7 +35,7 @@ def assert_executed_by_graph_mode(func, x, index):
         assert has_graph(jcr), f"ms_x: {x}, index: {index}"
 
 
-class Net_index3(nn.Cell):
+class NetIndex3(nn.Cell):
     def construct(self, x, index1, index2, index3):
         y = x[index1, index2, index3]
         return y
@@ -53,7 +54,7 @@ def test_getitem_index_negative(mode):
     """
     ms.set_context(mode=mode)
     context.set_context(jit_level='O0')
-    net = Net_index3()
+    net = NetIndex3()
     x_np = np.arange(6 * 7 * 8 * 9).reshape((6, 7, 8, 9)).astype(np.float32)
     index1_np = -1
     index2_np = -1
@@ -63,7 +64,7 @@ def test_getitem_index_negative(mode):
     assert np.allclose(y_np, y.asnumpy())
 
 
-class Net_index2_slice(nn.Cell):
+class NetIndex2Slice(nn.Cell):
     def construct(self, x, index1, index2):
         y = x[index1, 0:2, index2]
         return y
@@ -82,7 +83,7 @@ def test_getitem_index_negative_with_slice(mode):
     """
     ms.set_context(mode=mode)
     context.set_context(jit_level='O0')
-    net = Net_index2_slice()
+    net = NetIndex2Slice()
     x_np = np.arange(6 * 7 * 8 * 9).reshape((6, 7, 8, 9)).astype(np.float32)
     index1_np = -1
     index2_np = -1
@@ -91,10 +92,291 @@ def test_getitem_index_negative_with_slice(mode):
     assert np.allclose(y_np, y.asnumpy())
 
 
+def previous_getitem_check_indexing(x, index, np_expected, capture_mode=None):
+    """getitem run and check"""
+    if capture_mode is None:
+        def func(ms_x, index):
+            ms_y = ms_x[index]
+            return ms_y
+    else:
+        @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+        def func(ms_x, index):
+            ms_y = ms_x[index]
+            return ms_y
+
+    ms_output = func(x, index)
+
+    if capture_mode == 'bytecode':
+        assert_executed_by_graph_mode(func, x, index)
+
+    assert np.allclose(np_expected, ms_output.asnumpy()), f"ms_x: {x}, index: {index}, " \
+                                                          f"expected:{np_expected} {np_expected.shape}, " \
+                                                          f"ms_output:{ms_output} {ms_output.shape}"
+
+
+@arg_mark(
+    plat_marks=['cpu_linux', 'cpu_windows', 'cpu_macos'],
+    level_mark='level0',
+    card_mark='onecard',
+    essential_mark='essential'
+)
+@pytest.mark.parametrize('capture_mode', [None])
+def test_previous_getitem_level0(capture_mode):
+    """
+    Feature: tensor getitem
+    Description: Verify the result of tensor getitem
+    Expectation: success
+    """
+    np_x = np.arange(2*3*4).reshape(2, 3, 4)
+    ms_x = Tensor(np_x)
+
+    # Basic index
+    basic_indices = [0, slice(0, 1), True, None, ..., (0, 2, ...), [0, 1]]
+    for index in basic_indices:
+        np_expected = np_x[index]
+        previous_getitem_check_indexing(ms_x, index, np_expected, capture_mode)
+
+    # Tensor index
+    tensor_indices = [
+        Tensor(0),
+        slice(Tensor(0), Tensor(2)),
+        Tensor([0, 1]),
+        Tensor([True, True])
+    ]
+    np_indices = [0, slice(0, 2), [0, 1], [True, True]]
+    for np_index, tensor_index in zip(np_indices, tensor_indices):
+        np_expected = np_x[np_index]
+        previous_getitem_check_indexing(ms_x, tensor_index, np_expected, capture_mode)
+
+    # Fancy index
+    fancy_indices = [([0, 1], [0, 1]),
+                     (Tensor([0, 1]), Tensor([0, 1])),
+                     ([0.0, 1.0], [0.0, 1.0]),
+                     ([0, 1], 0, [0, 1]),
+                     (Tensor([0, 1]), Tensor(0), Tensor([0, 1])),
+                     (0, [0, 1], [0, 1]),
+                     (Tensor(0), Tensor([0, 1]), Tensor([0, 1])),
+                     ([0, 1], slice(0, 2), [0, 1]),
+                     (Tensor([0, 1]), slice(0, 2), Tensor([0, 1])),
+                     ([0, 1], True, [0, 1]),
+                     ([0, 1], None, [0, 1]),
+                     (Tensor([0, 1]), None, Tensor([0, 1])),
+                     ([0, 1], ..., [0, 1]),
+                     (Tensor([0, 1]), ..., Tensor([0, 1])),
+                     (Tensor([0]), Tensor(0), slice(0, 4, 2))]
+
+    np_expecteds = [np.array([[0, 1, 2, 3], [16, 17, 18, 19]]),
+                    np.array([[0, 1, 2, 3], [16, 17, 18, 19]]),
+                    np.array([[0, 1, 2, 3], [16, 17, 18, 19]]),
+                    np.array([0, 13]),
+                    np.array([0, 13]),
+                    np.array([0, 5]),
+                    np.array([0, 5]),
+                    np.array([[0, 4], [13, 17]]),
+                    np.array([[0, 4], [13, 17]]),
+                    np.array([[0, 1, 2, 3], [16, 17, 18, 19]]),
+                    np.array([[[0, 1, 2, 3]], [[16, 17, 18, 19]]]),
+                    np.array([[[0, 1, 2, 3]], [[16, 17, 18, 19]]]),
+                    np.array([[0, 4, 8], [13, 17, 21]]),
+                    np.array([[0, 4, 8], [13, 17, 21]]),
+                    np.array([[0, 2]])]
+
+    for index, np_expected in zip(fancy_indices, np_expecteds):
+        previous_getitem_check_indexing(ms_x, index, np_expected, capture_mode)
+
+
+@arg_mark(
+    plat_marks=['cpu_linux'],
+    level_mark='level1',
+    card_mark='onecard',
+    essential_mark='essential'
+)
+@pytest.mark.parametrize('capture_mode', [None])
+def test_previous_getitem_level1(capture_mode):
+    """
+    Feature: tensor getitem
+    Description: Verify the result of tensor getitem
+    Expectation: success
+    """
+    np_x = np.arange(2*3*4).reshape(2, 3, 4)
+    ms_x = Tensor(np_x)
+
+    # Slice index with None
+    basic_indices = [slice(None, 2), slice(0, None), slice(None, None)]
+    np_expected = np_x[slice(0, 2)]
+    for index in basic_indices:
+        previous_getitem_check_indexing(ms_x, index, np_expected, capture_mode)
+
+    # List index which is normal tuple
+    index = [0, slice(0, 2), ...]
+    np_expected = np.array([[0, 1, 2, 3], [4, 5, 6, 7]])
+    previous_getitem_check_indexing(ms_x, index, np_expected, capture_mode)
+
+    # Tuple index with negative step slice
+    index = (0, slice(2, 0, -1))
+    np_expected = np_x[index]
+    previous_getitem_check_indexing(ms_x, index, np_expected, capture_mode)
+
+    # Tensor index with all False
+    index = [False, False]
+    np_expected = np_x[index]
+    previous_getitem_check_indexing(ms_x, Tensor(index), np_expected, capture_mode)
+
+
+@arg_mark(
+    plat_marks=['cpu_linux', 'cpu_windows', 'cpu_macos'],
+    level_mark='level0',
+    card_mark='onecard',
+    essential_mark='essential'
+)
+@pytest.mark.parametrize('mode, capture_mode', [(ms.GRAPH_MODE, 'bytecode'),
+                                                (ms.PYNATIVE_MODE, 'ast')])
+def test_previous_getitem_exception_index_error(mode, capture_mode):
+    """
+    Feature: previous tensor getitem
+    Description: Verify the result of previous tensor getitem exception
+    Expectation: success
+    """
+
+    np_x = np.arange(2 * 3 * 4).reshape((2, 3, 4)).astype(np.float32)
+    ms_x = Tensor(np_x)
+    tensor_index = Tensor(np.array([[[True], [True], [True]], [[True], [True], [True]]]))
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_float_index(x):
+        return x[1.1]
+    with pytest.raises(IndexError):
+        _ = ms_x[1.1] if mode == ms.PYNATIVE_MODE else func_float_index(ms_x)
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_array_index(x):
+        return x[np.arange(0)]
+    with pytest.raises(IndexError):
+        _ = ms_x[np.arange(0)] if mode == ms.PYNATIVE_MODE else func_array_index(ms_x)
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_tensor_index_dim_greater_data_dim(x):
+        return x[[tensor_index, 0]]
+    ms_x = Tensor(np_x)
+    with pytest.raises(IndexError):
+        _ = ms_x[[tensor_index, 0]] if mode == ms.PYNATIVE_MODE else func_tensor_index_dim_greater_data_dim(ms_x)
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_tensor_index_with_float(x):
+        return x[Tensor(1.1)]
+    ms_x = Tensor(np_x)
+    with pytest.raises(IndexError):
+        _ = ms_x[Tensor(1.1)] if mode == ms.PYNATIVE_MODE else func_tensor_index_with_float(ms_x)
+
+
+@arg_mark(
+    plat_marks=['cpu_windows', 'cpu_macos'],
+    level_mark='level0',
+    card_mark='onecard',
+    essential_mark='essential'
+)
+@pytest.mark.parametrize('mode, capture_mode', [(ms.GRAPH_MODE, 'bytecode'),
+                                                (ms.PYNATIVE_MODE, 'ast')])
+def test_previous_getitem_exception_index_error_without_centos(mode, capture_mode):
+    """
+    Feature: previous tensor getitem
+    Description: Verify the result of previous tensor getitem exception
+    Expectation: success
+    """
+    np_x = np.arange(2 * 3 * 4).reshape((2, 3, 4)).astype(np.float32)
+    ms_x = Tensor(np_x)
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_list_index_dim_greater_tensor_data_dim(x):
+        return x[[0, 2]]
+    ms_x = Tensor(np_x)
+    with pytest.raises(IndexError):
+        _ = ms_x[[0, 2]] if mode == ms.PYNATIVE_MODE else func_list_index_dim_greater_tensor_data_dim(ms_x)
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_tuple_index_dim_greater_tensor_data_dim(x):
+        return x[([0, 2])]
+    ms_x = Tensor(np_x)
+    with pytest.raises(IndexError):
+        _ = ms_x[([0, 2])] if mode == ms.PYNATIVE_MODE else func_tuple_index_dim_greater_tensor_data_dim(ms_x)
+
+@arg_mark(
+    plat_marks=['cpu_linux', 'cpu_windows', 'cpu_macos'],
+    level_mark='level0',
+    card_mark='onecard',
+    essential_mark='essential'
+)
+@pytest.mark.parametrize('mode, capture_mode', [(ms.GRAPH_MODE, 'bytecode'),
+                                                (ms.PYNATIVE_MODE, 'ast')])
+def test_previous_getitem_exception_type_error(mode, capture_mode):
+    """
+    Feature: previous tensor getitem
+    Description: Verify the result of previous tensor getitem exception
+    Expectation: success
+    """
+
+    np_x = np.arange(2 * 3 * 4).reshape((2, 3, 4)).astype(np.float32)
+    ms_x = Tensor(np_x)
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_scalar_tensor_with_int_index(x):
+        return x[0]
+    ms_x = Tensor(1)
+    with pytest.raises(TypeError):
+        _ = ms_x[0] if mode == ms.PYNATIVE_MODE else func_scalar_tensor_with_int_index(ms_x)
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_tuple_index_with_float(x):
+        return x[[0, 1.4]]
+    ms_x = Tensor(np_x)
+    with pytest.raises(TypeError):
+        _ = ms_x[[0, 1.4]] if mode == ms.PYNATIVE_MODE else func_tuple_index_with_float(ms_x)
+
+@arg_mark(
+    plat_marks=['cpu_linux', 'cpu_windows', 'cpu_macos'],
+    level_mark='level0',
+    card_mark='onecard',
+    essential_mark='essential'
+)
+@pytest.mark.parametrize('mode, capture_mode', [(ms.GRAPH_MODE, 'bytecode'),
+                                                (ms.PYNATIVE_MODE, 'ast')])
+def test_previous_getitem_exception_value_error(mode, capture_mode):
+    """
+    Feature: previous tensor getitem
+    Description: Verify the result of previous tensor getitem exception
+    Expectation: success
+    """
+
+    np_x = np.arange(2 * 3 * 4).reshape((2, 3, 4)).astype(np.float32)
+    ms_x = Tensor(np_x)
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_scalar_tensor_with_slice(x):
+        return x[:]
+    ms_x = Tensor(1)
+    with pytest.raises(ValueError):
+        _ = ms_x[:] if mode == ms.PYNATIVE_MODE else func_scalar_tensor_with_slice(ms_x)
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_index_none_data_dim_out_8(x):
+        return x[(None, slice(0, 2), slice(0, 2), slice(0, 2), slice(0, 2), slice(0, 2), slice(0, 2), slice(0, 2))]
+    ms_x = Tensor(np.arange(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8)).reshape(1, 2, 3, 4, 5, 6, 7, 8)
+    s = slice(0, 2)
+    indices = (None, s, s, s, s, s, s, s)
+    with pytest.raises(ValueError):
+        _ = ms_x[indices] if mode == ms.PYNATIVE_MODE else func_index_none_data_dim_out_8(ms_x)
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_data_dim_out_8(x):
+        return x[(0, 1)]
+    ms_x = Tensor(np.arange(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9)).reshape(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    with pytest.raises(ValueError):
+        _ = ms_x[(0, 1)] if mode == ms.PYNATIVE_MODE else func_data_dim_out_8(ms_x)
+
+
 def getitem_check_indexing(x, index, np_expected, capture_mode=None):
     """getitem run and check"""
     if capture_mode is None:
-
         def func(ms_x, index):
             ms_y = ms_x[index]
             return ms_y
@@ -204,6 +486,40 @@ def test_getitem(capture_mode):
 
     for index, np_expected in zip(fancy_indices, np_expecteds):
         getitem_check_indexing(ms_x, index, np_expected, capture_mode)
+
+def getitem_check_indexing_without_jit(x, index, np_expected, capture_mode=None):
+    """getitem run and check"""
+    def func(ms_x, index):
+        ms_y = ms_x[index]
+        return ms_y
+    ms_output = func(x, index)
+    assert np.allclose(np_expected, ms_output.asnumpy()), f"ms_x: {x}, index: {index}, " \
+                                                          f"expected:{np_expected} {np_expected.shape}, " \
+                                                          f"ms_output:{ms_output} {ms_output.shape}"
+
+@arg_mark(plat_marks=['platform_ascend'], level_mark='level1', card_mark='onecard', essential_mark='essential')
+@pytest.mark.parametrize('capture_mode', [None])
+def test_slice_tensor_index_getitem_without_jit(capture_mode):
+    """
+    Feature: tensor setitem
+    Description: Verify the result of tensor setitem
+    Expectation: success
+    """
+
+     # Slice Tensor index
+    np_x = np.arange(2*3*4).reshape(2, 3, 4)
+    ms_x = Tensor(np_x)
+    basic_type = [ms.int8, ms.uint8, ms.int16, ms.uint16, ms.int, ms.int32, ms.uint32, ms.int64, ms.uint64, ms.float16,
+                  ms.float, ms.float32, ms.double, ms.float64, ms.bfloat16]
+    np_expected = np.array([[[12, 13, 14, 15],
+                             [16, 17, 18, 19],
+                             [20, 21, 22, 23]]])
+    for dtype in basic_type:
+        start = Tensor(1, dtype=dtype)
+        end = Tensor(2, dtype=dtype)
+        step = Tensor(1, dtype=dtype)
+        slice_index = slice(start, end, step)
+        getitem_check_indexing_without_jit(ms_x, slice_index, np_expected, capture_mode)
 
 
 @arg_mark(plat_marks=['platform_ascend'], level_mark='level0', card_mark='onecard', essential_mark='essential')
@@ -436,38 +752,44 @@ def test_getitem_exception(mode, capture_mode):
     ms_x = Tensor(np_x)
 
     @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
-    def func1(x): return ms_x[2, 0, 0]
+    def func1(x):
+        return x[2, 0, 0]
     with pytest.raises(IndexError) as exc:
         _ = ms_x[2, 0, 0] if mode == ms.PYNATIVE_MODE else func1(ms_x)
     assert "is out of bounds" in str(exc.value)
 
     @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
-    def func2(x): return ms_x[0, 0, 0, 0]
+    def func2(x):
+        return x[0, 0, 0, 0]
     with pytest.raises(IndexError) as exc:
         _ = ms_x[0, 0, 0, 0] if mode == ms.PYNATIVE_MODE else func2(ms_x)
     assert "too many indices" in str(exc.value)
 
     @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
-    def func3(x): return ms_x[0, 't']
+    def func3(x):
+        return x[0, 't']
     with pytest.raises(IndexError) as exc:
         _ = ms_x[0, 't'] if mode == ms.PYNATIVE_MODE else func3(ms_x)
     assert "Invalid tensor index type" in str(exc.value)
 
     @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
-    def func4(x): return ms_x[0:3:-1]
+    def func4(x):
+        return x[0:3:-1]
     with pytest.raises(ValueError) as exc:
         _ = ms_x[0:3:-1] if mode == ms.PYNATIVE_MODE else func4(ms_x)
     assert "slice step must be positive" in str(exc.value)
 
     @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
-    def func5(x): return ms_x[0]
+    def func5(x):
+        return x[0]
     ms_x = Tensor(0)
     with pytest.raises(TypeError) as exc:
         _ = ms_x[0] if mode == ms.PYNATIVE_MODE else func5(ms_x)
     assert "Invalid index of a 0-dim tensor." in str(exc.value)
 
     @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
-    def func6(x): return ms_x[0:1]
+    def func6(x):
+        return x[0:1]
     with pytest.raises(TypeError) as exc:
         _ = ms_x[0:1] if mode == ms.PYNATIVE_MODE else func6(ms_x)
     assert "Invalid index of a 0-dim tensor." in str(exc.value)
@@ -477,11 +799,46 @@ def test_getitem_exception(mode, capture_mode):
     assert "Invalid index of a 0-dim tensor." in str(exc.value)
 
     @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
-    def func7(x): return ms_x[ms.Tensor(1.4), :, : ]
+    def func7(x):
+        return x[Tensor(1.4), :, : ]
     ms_x = Tensor(np_x)
     with pytest.raises(TypeError) as exc:
-        _ = ms_x[ms.Tensor(1.4), :, : ] if mode == ms.PYNATIVE_MODE else func7(ms_x)
+        _ = ms_x[Tensor(1.4), :, : ] if mode == ms.PYNATIVE_MODE else func7(ms_x)
     assert "For 'Index', tensors used as indices must be long, int, uint8, or bool tensors" in str(exc.value)
+
+
+@arg_mark(plat_marks=['platform_ascend'], level_mark='level0', card_mark='onecard', essential_mark='essential')
+@pytest.mark.parametrize('mode, capture_mode', [(ms.GRAPH_MODE, 'bytecode'),
+                                                (ms.PYNATIVE_MODE, 'ast')])
+def test_getitem_exception_without_jit_ast(mode, capture_mode):
+    """
+    Feature: tensor getitem
+    Description: Verify the result of tensor setitem exception
+    Expectation: success
+    """
+    os.environ["MS_DEV_TENSOR_INDEX_BOOST"] = '1'
+
+    np_x = np.arange(2 * 3 * 4).reshape((2, 3, 4)).astype(np.float32)
+    ms_x = Tensor(np_x)
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_tensor_as_slice_index_dim_out_1(x):
+        s = Tensor([0, 1])
+        index = slice(s, s, s)
+        return x[index]
+    with pytest.raises(ValueError):
+        _ = ms_x[Tensor([0, 1]):1:1] if mode == ms.PYNATIVE_MODE else func_tensor_as_slice_index_dim_out_1(ms_x)
+
+    @ms.jit(capture_mode=capture_mode, jit_level="O0", backend="ms_backend")
+    def func_tensor_as_slice_index_with_unsupport_type(x):
+        s = Tensor(3 + 4j, dtype=ms.complex64)
+        index = slice(s, s, s)
+        return x[index]
+    with pytest.raises(TypeError):
+        _ = (
+            ms_x[Tensor(3 + 4j, dtype=ms.complex64):1:1]
+            if mode == ms.PYNATIVE_MODE
+            else func_tensor_as_slice_index_with_unsupport_type(ms_x)
+        )
 
 
 class NetMutableSequenceIndex(nn.Cell):
