7500e1a32df99c65336942e3daa31429665795e0
huangbingjian <huangbingjian1@huawei.com>
2025-10-21 16:52:05 +0800

    Raise error when getting attribute from super.

--- tests/ut/python/mindjit/graph_syntax/python_builtin_functions/test_super.py
+++ tests/ut/python/mindjit/graph_syntax/python_builtin_functions/test_super.py
@@ -1,4 +1,4 @@
-# Copyright 2020-2023 Huawei Technologies Co., Ltd
+# Copyright 2020-2025 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,14 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+# pylint: disable=bad-super-call
 """ test super"""
+import pytest
 import numpy as np
-
-import mindspore.nn as nn
-from mindspore import Tensor
-from mindspore import context
-
-context.set_context(mode=context.GRAPH_MODE)
+import mindspore as ms
+from mindspore import nn, Tensor
 
 
 class FatherNet(nn.Cell):
@@ -54,10 +52,9 @@ class SingleSubNet(FatherNet):
     def construct(self, x, y):
         ret_father_construct = super().construct(x, y)
         ret_father_test = super(SingleSubNet, self).test_father(x)
-        ret_father_x = super(SingleSubNet, self).x
         ret_sub_z = self.z
 
-        return ret_father_construct, ret_father_test, ret_father_x, ret_sub_z
+        return ret_father_construct, ret_father_test, ret_sub_z
 
 
 class MulSubNet(FatherNet, MatherNet):
@@ -69,41 +66,53 @@ class MulSubNet(FatherNet, MatherNet):
     def construct(self, x, y):
         ret_father_construct = super().construct(x, y)
         ret_father_test = super(MulSubNet, self).test_father(x)
-        ret_father_x = super(MulSubNet, self).x
         ret_mather_construct = super(FatherNet, self).construct(x, y)
         ret_mather_test = super(FatherNet, self).test_mather(y)
-        ret_mather_y = super(FatherNet, self).y
         ret_sub_z = self.z
 
-        return ret_father_construct, ret_father_test, ret_father_x, \
-               ret_mather_construct, ret_mather_test, ret_mather_y, ret_sub_z
+        return ret_father_construct, ret_father_test, \
+               ret_mather_construct, ret_mather_test, ret_sub_z
 
 
-def test_single_super():
+@pytest.mark.parametrize('mode', [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
+def test_single_super(mode):
+    """
+    Feature: Super
+    Description: test super syntax
+    Expectation: No exception
+    """
+    ms.set_context(mode=mode)
     single_net = SingleSubNet(2, 3)
     x = Tensor(np.ones([1, 2, 3], np.int32))
     y = Tensor(np.ones([1, 2, 3], np.int32))
     single_net(x, y)
 
 
-def test_mul_super():
+@pytest.mark.parametrize('mode', [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
+def test_mul_super(mode):
+    """
+    Feature: Super
+    Description: test super syntax
+    Expectation: No exception
+    """
+    ms.set_context(mode=mode)
     mul_net = MulSubNet(2, 3, 4)
     x = Tensor(np.ones([1, 2, 3], np.int32))
     y = Tensor(np.ones([1, 2, 3], np.int32))
     mul_net(x, y)
 
 
-def test_single_super_in():
+@pytest.mark.parametrize('mode', [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
+def test_unnormal_super_attribute(mode):
+    """
+    Feature: Super
+    Description: test super syntax
+    Expectation: AttributeError exception
+    """
     class FatherNetIn(nn.Cell):
         def __init__(self, x):
             super(FatherNetIn, self).__init__(x)
             self.x = x
-
-        def construct(self, x, y):
-            return self.x * x
-
-        def test_father(self, x):
-            return self.x + x
 
     class SingleSubNetIN(FatherNetIn):
         def __init__(self, x, z):
@@ -111,13 +120,13 @@ def test_single_super_in():
             self.z = z
 
         def construct(self, x, y):
-            ret_father_construct = super().construct(x, y)
-            ret_father_test = super(SingleSubNetIN, self).test_father(x)
             ret_father_x = super(SingleSubNetIN, self).x
+            return ret_father_x
 
-            return ret_father_construct, ret_father_test, ret_father_x
-
+    ms.set_context(mode=mode)
     single_net_in = SingleSubNetIN(2, 3)
     x = Tensor(np.ones([1, 2, 3], np.int32))
     y = Tensor(np.ones([1, 2, 3], np.int32))
-    single_net_in(x, y)
+    with pytest.raises(AttributeError) as ex:
+        single_net_in(x, y)
+    assert "has no attribute 'x'" in str(ex.value)
