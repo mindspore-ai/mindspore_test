dbb3a6cbfa8ec04e1e58e5b25ae12cd454e7b911
fandawei <fandawei2@huawei.com>
2025-10-28 15:10:17 +0800

    GE backend dump test case migration from 910a to 910b

--- tests/st/tools/data_dump/ge_backend_dump/test_ge_backend_dump.py
+++ tests/st/tools/data_dump/ge_backend_dump/test_ge_backend_dump.py
@@ -12,12 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+"""GE backend dump test."""
+
 import subprocess
 from tests.mark_utils import arg_mark
 from tests.security_utils import security_off_wrap
 
 
-@arg_mark(plat_marks=['platform_ascend'], level_mark='level1', card_mark='onecard', essential_mark='essential')
+@arg_mark(plat_marks=['platform_ascend910b'], level_mark='level1', card_mark='onecard', essential_mark='essential')
 @security_off_wrap
 def test_ge_backend_dump():
     """
@@ -32,7 +34,8 @@ def test_ge_backend_dump():
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
-        timeout=120
+        timeout=120,
+        check=True
     )
 
     output = result.stdout
