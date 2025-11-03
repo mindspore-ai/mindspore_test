0334fa0626a9a1aca9f8bf5730629bbece201e2c
jiaorui <jiaorui3@huawei.com>
2025-11-03 16:46:22 +0800

    msda not support 910a

--- tests/st/custom/ops_custom/ascendc/test_custom_multi_output.py
+++ tests/st/custom/ops_custom/ascendc/test_custom_multi_output.py
@@ -1,3 +1,19 @@
+# Copyright 2024-2025 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""custom test case"""
+
 import numpy as np
 import mindspore as ms
 from mindspore import nn, ops, Tensor, context
@@ -110,7 +126,7 @@ class TestMultiOutput():
         assert np.allclose(grad_sampling_loc.asnumpy(), output[1].asnumpy())
         assert np.allclose(grad_attn_weight.asnumpy(), output[2].asnumpy())
 
-    @arg_mark(plat_marks=['platform_ascend'], level_mark='level1', card_mark='onecard', essential_mark='essential')
+    @arg_mark(plat_marks=['platform_ascend910b'], level_mark='level1', card_mark='onecard', essential_mark='essential')
     @pytest.mark.parametrize('context_mode', [ms.PYNATIVE_MODE])
     def test_cpp_infer_shape_type(self, context_mode):
         """
@@ -126,7 +142,7 @@ class TestMultiOutput():
             self.value, self.shapes, self.level_start_index, self.sampling_locations, self.attention_weights)
         self.compare_out(grad_value, grad_sampling_loc, grad_attn_weight)
 
-    @arg_mark(plat_marks=['platform_ascend'], level_mark='level1', card_mark='onecard', essential_mark='essential')
+    @arg_mark(plat_marks=['platform_ascend910b'], level_mark='level1', card_mark='onecard', essential_mark='essential')
     @pytest.mark.parametrize('context_mode', [ms.PYNATIVE_MODE])
     def test_python_infer_shape_type(self, context_mode):
         """
@@ -149,7 +165,7 @@ class TestMultiOutput():
             self.value, self.shapes, self.level_start_index, self.sampling_locations, self.attention_weights)
         self.compare_out(grad_value, grad_sampling_loc, grad_attn_weight)
 
-    @arg_mark(plat_marks=['platform_ascend'], level_mark='level1', card_mark='onecard', essential_mark='essential')
+    @arg_mark(plat_marks=['platform_ascend910b'], level_mark='level1', card_mark='onecard', essential_mark='essential')
     @pytest.mark.parametrize('context_mode', [ms.PYNATIVE_MODE])
     def test_cpp_infer_shape_python_infer_type(self, context_mode):
         """
@@ -165,7 +181,7 @@ class TestMultiOutput():
             self.value, self.shapes, self.level_start_index, self.sampling_locations, self.attention_weights)
         self.compare_out(grad_value, grad_sampling_loc, grad_attn_weight)
 
-    @arg_mark(plat_marks=['platform_ascend'], level_mark='level1', card_mark='onecard', essential_mark='essential')
+    @arg_mark(plat_marks=['platform_ascend910b'], level_mark='level1', card_mark='onecard', essential_mark='essential')
     @pytest.mark.parametrize('context_mode', [ms.PYNATIVE_MODE])
     def test_python_infer_shape_cpp_infer_type(self, context_mode):
         """
