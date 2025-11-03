b0db90709dd8ce3254533d53c40631a985b7fab5
ckey_Dou <ckey.chengbin@huawei.com>
2025-11-03 11:06:22 +0800

    rename atb namespace

--- mindspore/ops/kernel/ascend/hccl/hcom_all_reduce_scatter.cc
+++ mindspore/ops/kernel/ascend/hccl/hcom_all_reduce_scatter.cc
@@ -53,7 +53,7 @@ bool HcomAllReduceScatterKernel::Launch(const std::vector<KernelTensor *> &input
   if (use_lccl_) {
     auto lccl_result = lccl_reduce_scatter_func_(lccl_ptr_, inputs[0]->device_ptr(), outputs[0]->device_ptr(),
                                                  hccl_count_, hccl_data_type_list_[0], op_type_, stream_ptr);
-    if (lccl_result != Lcal::LCAL_SUCCESS) {
+    if (lccl_result != Lcal_ms::LCAL_SUCCESS) {
       MS_LOG(EXCEPTION) << "LCCL ReduceScatter failed.";
     }
     return true;
