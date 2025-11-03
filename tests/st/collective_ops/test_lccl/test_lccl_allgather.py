7c0afe85808286f3fb989cfe0231a8bbdf9ad7df
shen_haochen <shenhaochen@huawei.com>
2025-11-03 21:26:53 +0800

    lccl testcase only support 910B

--- tests/st/collective_ops/test_lccl/test_lccl_allgather.py
+++ tests/st/collective_ops/test_lccl/test_lccl_allgather.py
@@ -12,17 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-
-"""test lccl allgather with 8p"""
+"""
+test lccl allgather with 8p
+"""
 
 import numpy as np
 
-import mindspore.nn as nn
-from mindspore import Tensor
+import mindspore as ms
+from mindspore import Tensor, nn
 from mindspore.common.initializer import initializer
 from mindspore.common.parameter import Parameter
 from mindspore.communication.management import init, HCCL_WORLD_COMM_GROUP, get_rank, get_group_size
 from mindspore.ops import operations as P
+
+ms.set_context(mode=ms.GRAPH_MODE, device_target="Ascend", jit_config={"jit_level": "O0", "infer_boost": "on"})
 
 init()
 rank = get_rank()
@@ -32,7 +35,7 @@ x = np.ones([1, 1, 3, 3]).astype(np.float32) * 0.01 * (rank + 1)
 
 class Net(nn.Cell):
     def __init__(self):
-        super(Net, self).__init__()
+        super().__init__()
         self.all_gather = P.AllGather(group=HCCL_WORLD_COMM_GROUP)
         self.x = Parameter(initializer(Tensor(x), x.shape), name='x')
 
