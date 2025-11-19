fa5a2d601c0146fce92c5a6a8c6c271b8c1b7ca2
shaoshengqi <shaoshengqi@huawei.com>
2025-11-19 09:27:04 +0800

    [psjit] migrate test_parser_list_slice.py

--- tests/st/compiler/utils.py
+++ tests/st/compiler/utils.py
@@ -18,3 +18,29 @@ def match_array(actual, expected, error=0, err_msg=''):
             actual, expected, decimal=error, err_msg=err_msg)
     else:
         np.testing.assert_equal(actual, expected, err_msg=err_msg)
+
+
+def _count_unequal_element(data_expected, data_me, rtol, atol):
+    assert data_expected.shape == data_me.shape
+    total_count = len(data_expected.flatten())
+    error = np.abs(data_expected - data_me)
+    greater = np.greater(error, atol + np.abs(data_me) * rtol)
+    nan_diff = np.not_equal(np.isnan(data_expected), np.isnan(data_me))
+    inf_diff = np.not_equal(np.isinf(data_expected), np.isinf(data_me))
+    # ICKTGQ
+    if data_expected.dtype in ('complex64', 'complex128'):
+        greater = greater + nan_diff + inf_diff
+    else:
+        neginf_diff = np.not_equal(np.isneginf(data_expected), np.isneginf(data_me))
+        greater = greater + nan_diff + inf_diff + neginf_diff
+    loss_count = np.count_nonzero(greater)
+    assert (loss_count / total_count) < rtol, \
+        "\ndata_expected_std:{0}\ndata_me_error:{1}\nloss:{2}". \
+            format(data_expected[greater], data_me[greater], error[greater])
+
+
+def allclose_nparray(data_expected, data_me, rtol, atol, equal_nan=True):
+    if not np.allclose(data_expected, data_me, rtol, atol, equal_nan=equal_nan):
+        _count_unequal_element(data_expected, data_me, rtol, atol)
+    else:
+        assert np.array(data_expected).shape == np.array(data_me).shape
