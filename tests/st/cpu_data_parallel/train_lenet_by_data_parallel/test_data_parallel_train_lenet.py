6f49dbf28ad7c8e82b9185f6d15676c28aab07a1
zhangyinxia <zhangyinxia@huawei.com>
2025-11-25 16:24:45 +0800

    updata openssl

--- tests/st/cpu_data_parallel/train_lenet_by_data_parallel/test_data_parallel_train_lenet.py
+++ tests/st/cpu_data_parallel/train_lenet_by_data_parallel/test_data_parallel_train_lenet.py
@@ -21,7 +21,7 @@ import sys
 from tests.mark_utils import arg_mark
 
 
-@arg_mark(plat_marks=['cpu_linux', 'cpu_windows', 'cpu_macos'], level_mark='level1', card_mark='onecard',
+@arg_mark(plat_marks=['cpu_linux', 'cpu_windows', 'cpu_macos'], level_mark='level0', card_mark='onecard',
           essential_mark='unessential')
 def test_train_lenet_by_data_parallel():
     """
