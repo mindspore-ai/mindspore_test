e5a2e87e1268c6c13a7d639531bdefbddd14114d
熊攀 <xiongpan17@huawei.com>
2025-12-05 17:40:52 +0800

    修改tile算子infer value逻辑

--- tests/st/ops/ascend/test_tile.py
+++ tests/st/ops/ascend/test_tile.py
@@ -12,27 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+"""test for tile"""
 from tests.mark_utils import arg_mark
 import pytest
 import numpy as np
 
-import mindspore.context as context
-import mindspore.nn as nn
+from mindspore import context
+from mindspore import nn
 from mindspore import Tensor
-from mindspore.common import dtype as mstype
 from mindspore.ops import function as F
+import mindspore as ms
 
 context.set_context(device_target="Ascend")
 
-
 class Net(nn.Cell):
     def __init__(self):
-        super(Net, self).__init__()
+        super().__init__()
         self.tile = F.tile
+        self.x = Tensor(np.ones((1, 1, 3, 4)), dtype=ms.bfloat16)
+        self.multiples = (2, 2, 1, 1)
 
-    def construct(self, x, multiples):
-        return self.tile(x, multiples)
-
+    def construct(self):
+        return self.tile(self.x, self.multiples)
 
 @arg_mark(plat_marks=['platform_ascend910b'], level_mark='level1', card_mark='onecard', essential_mark='essential')
 @pytest.mark.parametrize('mode', [context.GRAPH_MODE, context.PYNATIVE_MODE])
@@ -44,11 +45,6 @@ def test_tile_bfloat16(mode):
     """
     context.set_context(mode=mode)
     net = Net()
-    x = Tensor(np.array([[0.1, 0.2], [0.3, 0.4]]), mstype.bfloat16)
-    multiples = (2, 2)
-    output = net(x, multiples)
-    expect_output = np.array([[0.1, 0.2, 0.1, 0.2],
-                              [0.3, 0.4, 0.3, 0.4],
-                              [0.1, 0.2, 0.1, 0.2],
-                              [0.3, 0.4, 0.3, 0.4]]).astype(np.float32)
+    output = net()
+    expect_output = np.ones((2, 2, 3, 4)).astype(np.float32)
     np.testing.assert_allclose(output.float().asnumpy(), expect_output, 1e-3, 1e-3)
