73a7f2e28c92fa4ad49985413dfa49fda84bf7b5
caifubi <caifubi1@huawei.com>
2025-12-10 14:50:31 +0800

    fix custom grad

--- mindspore/ccsrc/pynative/backward/function.cc
+++ mindspore/ccsrc/pynative/backward/function.cc
@@ -14,12 +14,12 @@
  * limitations under the License.
  */
 
+#include "include/pynative/backward/function.h"
 #include "include/utils/convert_utils_py.h"
 #include "include/utils/tensor_py.h"
 #include "include/pynative/utils/pynative_execute.h"
 #include "include/pynative/utils/pynative_utils.h"
 #include "pynative/backward/grad_utils.h"
-#include "pynative/backward/function.h"
 #include "pynative/backward/op_grad/func_grad.h"
 #include "pynative/backward/saved_tensor.h"
 
